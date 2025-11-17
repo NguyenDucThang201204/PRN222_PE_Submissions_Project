44ba1954bb4e5301697bdffaf612029819eb6782
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:41:38 +0700

    Upload Service/BearAccountService.cs

---
+++ Service/BearAccountService.cs
@@ -0,0 +1,21 @@
+﻿using Repo;
+using Repo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class BearAccountService
+    {
+        private readonly BearAccountRepo _repo;
+        public BearAccountService() => _repo = new BearAccountRepo();
+
+        public async Task<BearAccount> GetSystemUserAccountAsync(string username, string password)
+        {
+            return await _repo.GetUserAccount(username, password);
+        }
+    }
+}
