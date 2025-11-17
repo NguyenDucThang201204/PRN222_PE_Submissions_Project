b25ecef79ec14c98ba09b1d6c040c8df22f871cb
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:39 +0700

    Upload Service/IAuthService.cs

---
+++ Service/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using Repository.Requests;
+using Repository.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IAuthService
+    {
+        Task<LoginResponse> LoginAsync(LoginRequest request);
+    }
+}
