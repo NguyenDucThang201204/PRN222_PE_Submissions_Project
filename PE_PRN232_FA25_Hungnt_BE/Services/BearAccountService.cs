a153cd97f325700c5a118e82a6b68bac35e87cac
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:17 +0700

    Upload PE_PRN232_FA25_Hungnt_BE/Services/BearAccountService.cs

---
+++ PE_PRN232_FA25_Hungnt_BE/Services/BearAccountService.cs
@@ -0,0 +1,27 @@
+﻿using Repositories;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class BearAccountService
+    {
+        private readonly BearAccountRepo _repo;
+        public BearAccountService() => _repo = new BearAccountRepo();
+        public async Task<BearAccount> GetAccount(string email, string password)
+        {
+            try
+            {
+                return await _repo.GetUserAccount(email, password);
+            }
+            catch 
+            { 
+            }
+            return null;
+        }
+    }
+}
