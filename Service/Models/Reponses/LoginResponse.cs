da29c2984cf52600b7bc49a6ef90581321131b42
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:02:50 +0700

    Upload Service/Models/Reponses/LoginResponse.cs

---
+++ Service/Models/Reponses/LoginResponse.cs
@@ -0,0 +1,16 @@
+﻿using Service.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Models.Reponses
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; } = default!;
+
+        public string Role { get; set; } = default!;
+    }
+}
