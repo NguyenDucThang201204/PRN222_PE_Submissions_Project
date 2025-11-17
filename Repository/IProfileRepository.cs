2b43a6c21f2b4cb21bada56b5fa0db672b882c2e
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:47:16 +0700

    Upload Repository/IProfileRepository.cs

---
+++ Repository/IProfileRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOs.Models;
+
+namespace Repository
+{
+    public interface IProfileRepository
+    {
+        Task<List<BearProfile>> GetProfiles();
+        Task<BearProfile> GetProfile(int id);
+        Task<BearProfile> AddProfile(BearProfile profile);
+        Task<BearProfile> UpdateProfile(BearProfile profile);
+        Task<BearProfile> DeleteProfile(int id);
+        Task<List<BearType>> GetTypes();
+
+        Task<List<BearProfile>> SearchProfile(string keyword);
+
+    }
+}
