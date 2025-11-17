031b2698c0b95d8d5f23936a490499e688c3ab0f
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:46:38 +0700

    Upload Repositories/ModelExtensions/SearchRequest.cs

---
+++ Repositories/ModelExtensions/SearchRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.ModelExtensions
+{
+    public class SearchRequest
+    {
+        [Required(ErrorMessage = "Current Page is not null !")]
+        [Range(1, int.MaxValue, ErrorMessage = "Current page more than 0")]
+        [DefaultValue(2)]
+        public int? CurrentPage { get; set; }
+
+        [Required(ErrorMessage = "Page Size is not null !")]
+        [Range(1, int.MaxValue, ErrorMessage ="Page size more than 0")]
+        [DefaultValue(3)]
+        public int? PageSize { get; set; }
+    }
+
+    public class MainSearchRequest : SearchRequest
+    {
+        public string? BearName { get; set; }
+        public string? BearTypeName { get; set; }
+        public decimal? BearWeight { get; set; }
+    }
+}
