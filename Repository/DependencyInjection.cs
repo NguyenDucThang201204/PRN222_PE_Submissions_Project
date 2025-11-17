903b92e7e319c1f723494cdede646618d8103139
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:02:18 +0700

    Upload Repository/DependencyInjection.cs

---
+++ Repository/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repository.Basic;
+using Repository.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public static class DependencyInjection
+    {
+        public static void AddDataServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FA25BearDBContext>(
+                options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof(GenericRepository<>));
+            services.AddScoped<BearAccountRepository>();
+            services.AddScoped<BearProfileRepository>();
+            services.AddScoped<BearTypeRepository>();
+
+        }
+    }
+}
