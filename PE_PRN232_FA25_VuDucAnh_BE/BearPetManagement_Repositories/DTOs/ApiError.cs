33f75870028df1f96f5fa22f90c749822d9f7218
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:01:58 +0700

    Upload PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Repositories/DTOs/ApiError.cs

---
+++ PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Repositories/DTOs/ApiError.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement_Repositories.DTOs
+{
+    public class ApiError
+    {
+        public string ErrorCode { get; set; }
+        public string Message { get; set; }
+
+        public ApiError(string errorCode, string message)
+        {
+            ErrorCode = errorCode;
+            Message = message;
+        }
+
+        public static readonly Dictionary<int, ApiError> _errors = new()
+        {
+            {400, new ApiError("400", "Missing/invalid input")},
+            {401, new ApiError("401", "Token missing/invalid")},
+            {403, new ApiError("403", "Permission denied")},
+            {404, new ApiError("404", "Resource not found")},
+            {500, new ApiError("500", "Internal server error")}
+
+
+        };
+    }
+}
