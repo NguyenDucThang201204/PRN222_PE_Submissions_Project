ac84513b27a90fdf5e0229a9b06c998595c9b30f
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:20:45 +0700

    Upload Service/ModelService.cs

---
+++ Service/ModelService.cs
@@ -0,0 +1,45 @@
+﻿using Repository;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ModelService
+    {
+        private readonly ModelRepo _modelservice;
+
+        public ModelService(ModelRepo modelRepo)
+        {
+            _modelservice = modelRepo;
+        }
+
+        public async Task<List<BearProfile>> GetAllAsync()
+        {
+            return await _modelservice.GetAllAsync();
+        }
+
+        public async Task<BearProfile> GetByIdAsync(int modelId)
+        {
+            return await _modelservice.GetByIdAsync(modelId);
+        }
+
+        public async Task<bool> CreateAsync(BearProfile entity)
+        {
+
+            return await _modelservice.CreateAsync(entity);
+        }
+
+        public async Task<bool> UpdateAsync(BearProfile entity)
+        {
+            return await _modelservice.UpdateAsync(entity);
+        }
+        public async Task<bool> DeletetAsync(int id)
+        {
+            return await _modelservice.DeletetAsync(id);
+        }
+    }
+}
