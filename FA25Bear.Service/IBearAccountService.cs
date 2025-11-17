52c70dded63e2f3f5334a7877c55637cfff08c45
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:44:19 +0700

    Upload FA25Bear.Service/IBearAccountService.cs

---
+++ FA25Bear.Service/IBearAccountService.cs
@@ -0,0 +1,11 @@
+﻿using FA25Bear.DataAccess.Models;
+using FA25Bear.DataAccess.Models.DTOs;
+
+namespace FA25Bear.Service
+{
+    public interface IBearAccountService
+    {
+        BearAccount CheckLogin(LoginRequestDTO lr);
+
+    }
+}
