252faaf54762597e75eff8e24c20999ea38b900d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:00:59 +0700

    Upload DAL/Repositories/BearAccountRepository.cs

---
+++ DAL/Repositories/BearAccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Entities;
+using DAL.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class BearAccountRepository  : GenericRepository<BearAccount>, IBearAccountRepository
+    {
+        public BearAccountRepository(Fa25bearDbContext context) : base(context)
+        {
+        }
+        public async Task<BearAccount> LoginAsync(string email, string password)
+        {
+            return await _context.BearAccounts.FirstOrDefaultAsync(e => (e.Email == email) && e.Password == password);
+        }
+    }
+}
