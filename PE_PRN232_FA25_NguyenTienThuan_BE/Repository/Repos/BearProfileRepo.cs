e715d087dfad4c596e66c3d160d15b5233c163db
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:46 +0700

    Upload PE_PRN232_FA25_NguyenTienThuan_BE/Repository/Repos/BearProfileRepo.cs

---
+++ PE_PRN232_FA25_NguyenTienThuan_BE/Repository/Repos/BearProfileRepo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repos
+{
+    public class BearProfileRepo : GenericRepository<BearProfile>
+    {
+        public BearProfileRepo() : base() { }
+
+        public async Task<BearProfile?> GetById(int id)
+        {
+            return await _context.BearProfiles.Include(b => b.BearType).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<BearProfile>> GetAll()
+        {
+            return await _context.BearProfiles.Include(b => b.BearType).ToListAsync();
+        }
+    }
+}
