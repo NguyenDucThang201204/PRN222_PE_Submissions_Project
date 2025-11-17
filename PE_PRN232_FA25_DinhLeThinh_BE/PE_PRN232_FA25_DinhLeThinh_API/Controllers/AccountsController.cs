6e2b99acfacf859644d7028c1be3b760d6843baf
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:41:20 +0700

    Upload PE_PRN232_FA25_DinhLeThinh_BE/PE_PRN232_FA25_DinhLeThinh_API/Controllers/AccountsController.cs

---
+++ PE_PRN232_FA25_DinhLeThinh_BE/PE_PRN232_FA25_DinhLeThinh_API/Controllers/AccountsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.DTOs;
+using PE_PRN232_FA25_DinhLeThinh_API.Middleware;
+using Service;
+
+namespace PE_PRN232_FA25_DinhLeThinh_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountService _acc;
+
+        public AccountsController(IAccountService acc)
+        {
+            _acc = acc;
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Authenticate([FromBody] LoginRequest request)
+        {
+            var response = await _acc.LoginAsync(request);
+            if (response == null)
+                return BadRequest(ErrorResponse.FromErrorCode(ErrorCode.HB40001, "Invalid username or password"));
+            return Ok(response);
+        }
+    }
+}
