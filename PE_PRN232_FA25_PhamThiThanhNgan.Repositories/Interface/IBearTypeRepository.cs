92d810013176a521a743878b94862f31e5ca46ee
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:17 +0700

    Upload PE_PRN232_FA25_PhamThiThanhNgan.Repositories/Interface/IBearTypeRepository.cs

---
+++ PE_PRN232_FA25_PhamThiThanhNgan.Repositories/Interface/IBearTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using PE_PRN232_FA25_PhamThiThanhNgan.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_PhamThiThanhNgan.Repositories.Interface
+{
+    public interface IBearTypeRepository
+    {
+        Task<IEnumerable<BearType>> GetAllAsync();
+        Task<BearType?> GetByIdAsync(int id);
+    }
+}
