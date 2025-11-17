ca99e145ba9fab0a04669207afb221ddf4ac3712
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:35:22 +0700

    Upload Repositories/ModelExtentions/UpdateRequest.cs

---
+++ Repositories/ModelExtentions/UpdateRequest.cs
@@ -0,0 +1,23 @@
+﻿using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.ModelExtentions
+{
+    public class UpdateRequest
+    {
+        public int LeopardTypeId { get; set; }
+        public string LeopardName { get; set; }
+        public double Weight { get; set; }
+
+        public string Characteristics { get; set; }
+
+        public string CareNeeds { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
