9ebd1b9d55063626052359505079f5b81d7c0f86
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:34 +0700

    Upload PE_PRN232_FA25_TangPhat_BE/Repository/AccountRepo.cs

---
+++ PE_PRN232_FA25_TangPhat_BE/Repository/AccountRepo.cs
@@ -0,0 +1,24 @@
+﻿using BusinessObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class AccountRepo : IAccountRepo
+    {
+        private readonly Fa25bearDbContext _context;
+        public AccountRepo()
+        {
+            _context = new Fa25bearDbContext();
+        }
+
+        public async Task<BearAccount?> LoginAsync(string email, string pass)
+        {
+            return await _context.BearAccounts.FirstOrDefaultAsync(a => a.Email == email && a.Password == pass);
+        }
+    }
+}
