c45410357b26354e07e1f9c8cec3f4448cf0716f
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:37:38 +0700

    Upload Repositories/ModelExtensions/PaginationResult.cs

---
+++ Repositories/ModelExtensions/PaginationResult.cs
@@ -0,0 +1,26 @@
+﻿namespace Repositories.ModelExtensions
+{
+    public class PaginationResult<T> where T : class
+    {
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public List<T> Items { get; set; }
+    }
+    public class GroupedPaginationResult<TKey, TItem>
+    {
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public List<GroupedData<TKey, TItem>> Items { get; set; }
+    }
+
+    public class GroupedData<TKey, TItem>
+    {
+        public TKey Key { get; set; }
+        public List<TItem> Items { get; set; }
+    }
+
+}
