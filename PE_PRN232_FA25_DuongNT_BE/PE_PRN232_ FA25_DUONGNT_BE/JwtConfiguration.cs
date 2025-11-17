b746cb5ee8a4ec45afda17de042014177a163b55
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:31 +0700

    Upload PE_PRN232_FA25_DuongNT_BE/PE_PRN232_ FA25_DUONGNT_BE/JwtConfiguration.cs

---
+++ PE_PRN232_FA25_DuongNT_BE/PE_PRN232_ FA25_DUONGNT_BE/JwtConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace PE_PRN232__FA25_DUONGNT_BE
+{
+    public class JwtConfiguration
+    {
+        public static void ConfigureJwtEvents(JwtBearerOptions options)
+        {
+            options.Events = new JwtBearerEvents
+            {
+                OnChallenge = context =>
+                {
+                    context.HandleResponse();
+                    context.Response.StatusCode = 401;
+                    context.Response.ContentType = "application/json";
+
+                    var error = new
+                    {
+                        ErrorCode = "HB40101",
+                        Status = 401,
+                        Message = "Token missing/invalid"
+                    };
+
+                    return context.Response.WriteAsJsonAsync(error);
+                },
+                OnForbidden = context =>
+                {
+                    context.Response.StatusCode = 403;
+                    context.Response.ContentType = "application/json";
+
+                    var error = new
+                    {
+                        ErrorCode = "HB40301",
+                        Status = 403,
+                        Message = "Permission denied"
+                    };
+
+                    return context.Response.WriteAsJsonAsync(error);
+                }
+            };
+        }
+    }
+}
