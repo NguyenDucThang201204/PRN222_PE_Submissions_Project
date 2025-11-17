2c6ef21df7cc7194d1143d76511f62935c1474d3
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:46:59 +0700

    Upload PE_PRN232_FA25_TranSonTrieu_BE/PE_PRN232_FA25_TranSonTrieu_BE/Controllers/AuthController.cs

---
+++ PE_PRN232_FA25_TranSonTrieu_BE/PE_PRN232_FA25_TranSonTrieu_BE/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using DAO.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace PE_PRN232_FA25_TranSonTrieu_BE.Controllers
+{
+    [Route("Account/Login")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly IUserService _userService;
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginDto)
+        {
+            try
+            {
+                var response = await _userService.Login(loginDto);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
