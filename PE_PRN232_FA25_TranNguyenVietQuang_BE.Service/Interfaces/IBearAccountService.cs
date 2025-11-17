a58af5c835ea3ad8b31e097176b8cbf2c33ab37c
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:39:08 +0700

    Upload PE_PRN232_FA25_TranNguyenVietQuang_BE.Service/Interfaces/IBearAccountService.cs

---
+++ PE_PRN232_FA25_TranNguyenVietQuang_BE.Service/Interfaces/IBearAccountService.cs
@@ -0,0 +1,16 @@
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository.Entities;
+
+namespace PE_PRN232_FA25_TranNguyenVietQuang_BE_Service.Interfaces;
+
+public interface IBearAccountService
+{
+    Task<List<BearAccount>> GetAllAsync();
+    Task<BearAccount?> GetByKeysAsync(object[] keys);
+    Task AddAsync(BearAccount entity);
+    Task UpdateAsync(BearAccount entity);
+    Task DeleteAsync(BearAccount entity);    Task<BearAccount?> GetByUserNameAsync(string? userName);
+
+}
