1c6009d131f78d590d531e5339b6199a14dfebf4
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:44:27 +0700

    Upload FA25Bear.DataAccess/Models/BearAccount.cs

---
+++ FA25Bear.DataAccess/Models/BearAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FA25Bear.DataAccess.Models;
+
+public partial class BearAccount
+{
+    public int AccountId { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int Role { get; set; }
+
+    public string? Phone { get; set; }
+
+    public bool? IsActive { get; set; }
+}
