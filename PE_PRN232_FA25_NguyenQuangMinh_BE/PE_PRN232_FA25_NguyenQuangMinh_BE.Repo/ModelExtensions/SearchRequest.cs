325da55a1117ee4eb807ba0b39bd30377d94a5f1
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:22:40 +0700

    Upload PE_PRN232_FA25_NguyenQuangMinh_BE/PE_PRN232_FA25_NguyenQuangMinh_BE.Repo/ModelExtensions/SearchRequest.cs

---
+++ PE_PRN232_FA25_NguyenQuangMinh_BE/PE_PRN232_FA25_NguyenQuangMinh_BE.Repo/ModelExtensions/SearchRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_NguyenQuangMinh_BE.Repo.ModelExtensions
+{
+    public class SearchRequest
+    {
+        public int? CurrentPage { get; set; }
+
+        public int? PageSize { get; set; }
+    }
+
+    public class BearSearchRequest : SearchRequest
+    {
+        public string? BearName { get; set; }
+        public string? BearWeight { get; set; }
+        public string? BearTypeName { get; set; }
+    }
+}
