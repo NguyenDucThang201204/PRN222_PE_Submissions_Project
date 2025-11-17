8a660081975c41ab3db5e770c35a444072038f9a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:47:48 +0700

    Upload PE_PRN232_FA25_LeHoangTrong_BE.repo/Entities/BearAccount.cs

---
+++ PE_PRN232_FA25_LeHoangTrong_BE.repo/Entities/BearAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PE_PRN232_FA25_LeHoangTrong_BE.repo.Entities;
+
+public partial class BearAccount
+{
+    public int AccountId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public int? RoleId { get; set; }
+}
