d25d4d0287566bfd7b79c57c34ef930c366d57b3
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:09:15 +0700

    Upload BearPetManagement_Service/Services/BearService.cs

---
+++ BearPetManagement_Service/Services/BearService.cs
@@ -0,0 +1,22 @@
+﻿using BearPetManagement_Repository.NewFolder;
+using BearPetManagement_Repository.Repositories;
+
+namespace BearPetManagement_Service.Services
+{
+    public class BearService
+    {
+        private readonly BearRepository _repo;
+
+        public BearService(BearRepository repository)
+        {
+            _repo = repository;
+        }
+
+        public async Task<List<BearProfile>> GetAll()
+        {
+            return await _repo.GetAllAsync();
+        }
+
+
+    }
+}
