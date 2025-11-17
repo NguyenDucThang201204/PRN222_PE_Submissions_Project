544bded945adde2b0c432441bda4b1b6d7ea5dfa
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:46:15 +0700

    Upload PE_PRN232_FA25_DaoTrongTien_BE.DAL/Repositories/IBearProfileRepository.cs

---
+++ PE_PRN232_FA25_DaoTrongTien_BE.DAL/Repositories/IBearProfileRepository.cs
@@ -0,0 +1,21 @@
+﻿using PE_PRN232_FA25_DaoTrongTien_BE.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_DaoTrongTien_BE.DAL.Repositories
+{
+    public interface IBearProfileRepository
+    {
+
+        Task<List<BearProfile>> GetAllBearProfiles();
+        Task<BearProfile> GetBearProfileById(int id);
+        Task AddBearProfile(BearProfile BearProfile);
+        Task UpdateBearProfile(BearProfile BearProfile);
+        Task DeleteBearProfile(int id);
+
+
+    }
+}
