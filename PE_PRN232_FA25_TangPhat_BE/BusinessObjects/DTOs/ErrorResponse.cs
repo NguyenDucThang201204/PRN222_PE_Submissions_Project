3d8d2172b183bb192a69cf18b43d9bb6db663dff
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:46 +0700

    Upload PE_PRN232_FA25_TangPhat_BE/BusinessObjects/DTOs/ErrorResponse.cs

---
+++ PE_PRN232_FA25_TangPhat_BE/BusinessObjects/DTOs/ErrorResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.DTOs
+{
+    public class ErrorResponse
+    {
+        public string ErrorCode { get; set; }
+        public string Message { get; set; }
+
+        public static ErrorResponse Fail(ErrorCode code, string message)
+        {
+            return new ErrorResponse
+            {
+                ErrorCode = code.ToString(),
+                Message = message
+            };
+        }
+    }
+}
