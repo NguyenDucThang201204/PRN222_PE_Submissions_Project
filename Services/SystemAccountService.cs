05004c181bb132301f490a93dcf683e06321a01d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:10:45 +0700

    Upload Services/SystemAccountService.cs

---
+++ Services/SystemAccountService.cs
@@ -0,0 +1,31 @@
+﻿using Repos;
+using Repos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class SystemAccountService
+    {
+        private readonly SystemAccountsRepo _repository;
+        public SystemAccountService() => _repository = new SystemAccountsRepo();
+
+        public async Task<BearAccount> GetUserAccount(string email, string password)
+        {
+            try
+            {
+                return await _repository.GetUserAccountAsync(email, password);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while retrieving the user account: {ex.Message}");
+                return null;
+
+            }
+
+        }
+    }
+}
