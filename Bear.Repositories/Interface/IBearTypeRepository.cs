688e9c8098f4f96fd1930ab84dfd8f9f293a8010
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:10 +0700

    Upload Bear.Repositories/Interface/IBearTypeRepository.cs

---
+++ Bear.Repositories/Interface/IBearTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Bear.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bear.Repositories.Interface
+{
+    public interface IBearTypeRepository
+    {
+        Task<BearType> GetItemAsyncById(int? id);
+    }
+}
