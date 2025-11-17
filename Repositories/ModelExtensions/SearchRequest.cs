198d72f51388596fa7e3be89d4525a2a5463770f
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:15:19 +0700

    Upload Repositories/ModelExtensions/SearchRequest.cs

---
+++ Repositories/ModelExtensions/SearchRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.ModelExtensions
+{
+    public class SearchRequest
+    {
+        public int? CurrentPage { get; set; } = 1;
+        public int? PageSize { get; set; } = 10;
+    }
+
+    public class SearchDataRequest : SearchRequest
+    {
+        public double? Weight { get; set; }
+        public string? Characteristics { get; set; }
+        public string? CareNeeds { get; set; }
+    }
+}
