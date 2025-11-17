2a1d09dadfcefb2cbf5cf666af17ae6a981802b9
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:40 +0700

    Upload PE_PRN232_FA25_TangPhat_BE/Service/IProfileService.cs

---
+++ PE_PRN232_FA25_TangPhat_BE/Service/IProfileService.cs
@@ -0,0 +1,23 @@
+﻿using BusinessObjects.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IProfileService
+    {
+        Task<List<GetDTO>> GetAllAsync();
+
+        Task<GetDTO?> GetByIdAsync(int id);
+
+        Task<GetDTO?> CreateAsync(CreateDTO request);
+
+        Task<GetDTO?> UpdateAsync(int id, CreateDTO request);
+
+        Task<bool> DeleteAsync(int id);
+
+    }
+}
