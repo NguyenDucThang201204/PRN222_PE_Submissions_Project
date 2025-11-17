99d4622614dd3d43d10978c0a60269ae87c28253
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:59 +0700

    Upload PE_PRN232_FA25_NguyenThaiDuy_BE.Repositories/IAccountRepository.cs

---
+++ PE_PRN232_FA25_NguyenThaiDuy_BE.Repositories/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_NguyenThaiDuy_BE.Repositories
+{
+    public interface IAccountRepository
+    {
+        AuthResponse Authenticate(string email, string password);
+    }
+}
