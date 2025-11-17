2f1eca9d342214ff78485bc1b0d9782c30fc65cd
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:10:08 +0700

    Upload FA25Bear.Services/Services/AccountService.cs

---
+++ FA25Bear.Services/Services/AccountService.cs
@@ -0,0 +1,26 @@
+﻿using FA25Bear.Repositories.DTOs;
+using FA25Bear.Repositories.IRepositories;
+using FA25Bear.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FA25Bear.Services.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public AuthResponse Authenticate(string email, string password)
+        {
+            return _accountRepository.Authenticate(email, password);
+        }
+    }
+}
