4c4c983d2fb5db77d7465452077772d9f8ae937b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:46:13 +0700

    Upload PE_PRN232_FA25_DaoTrongTien_BE.DAL/Repositories/BearProfileRepository.cs

---
+++ PE_PRN232_FA25_DaoTrongTien_BE.DAL/Repositories/BearProfileRepository.cs
@@ -0,0 +1,52 @@
+﻿using PE_PRN232_FA25_DaoTrongTien_BE.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_DaoTrongTien_BE.DAL.Repositories
+{
+    public class BearProfileRepository:IBearProfileRepository
+    {
+
+        private readonly Fa25bearDbContext _context;
+        public BearProfileRepository(Fa25bearDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<BearProfile>> GetAllBearProfiles()
+        {
+            return _context.BearProfiles.ToList();
+        }
+
+        public async Task<BearProfile> GetBearProfileById(int id)
+        {
+            return _context.BearProfiles.FirstOrDefault(h => h.BearProfileId == id);
+        }
+
+        public async Task AddBearProfile(BearProfile BearProfile)
+        {
+            _context.BearProfiles.Add(BearProfile);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBearProfile(BearProfile BearProfile)
+        {
+            _context.Entry(BearProfile).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteBearProfile(int id)
+        {
+            var BearProfile = _context.BearProfiles.FirstOrDefault(h => h.BearProfileId == id);
+            if (BearProfile != null)
+            {
+                _context.BearProfiles.Remove(BearProfile);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
