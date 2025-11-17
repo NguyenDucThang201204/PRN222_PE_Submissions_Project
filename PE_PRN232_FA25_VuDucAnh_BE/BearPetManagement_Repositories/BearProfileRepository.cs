4612c44dc4e555aab1471bd654f811df141c7489
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:01:49 +0700

    Upload PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Repositories/BearProfileRepository.cs

---
+++ PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Repositories/BearProfileRepository.cs
@@ -0,0 +1,18 @@
+﻿using BearPetManagement_Repositories.DBContext;
+using BearPetManagement_Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement_Repositories
+{
+    public class BearProfileRepository : GenericRepository<BearProfile>
+    {
+        public BearProfileRepository() : base() { }
+        public BearProfileRepository(FA25BearDBContext context) : base(context) { }
+
+
+    }
+}
