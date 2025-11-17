7286591ee32708b60fc5fe3a8155b5588cfb3107
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:10:03 +0700

    Upload FA25Bear.Repositories/IRepositories/IAccountRepository.cs

---
+++ FA25Bear.Repositories/IRepositories/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using FA25Bear.Repositories.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FA25Bear.Repositories.IRepositories
+{
+    public interface IAccountRepository
+    {
+        AuthResponse Authenticate(string email, string password);
+    }
+}
