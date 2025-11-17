ba2a2eea1de1238346369796c8db264cc24bcec5
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:48:28 +0700

    Upload PE_PRN232_FA25_TruongNN_BE/Services/DTOs/BearProfileUpdateRequest.cs

---
+++ PE_PRN232_FA25_TruongNN_BE/Services/DTOs/BearProfileUpdateRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTOs
+{
+    public class BearProfileUpdateRequest
+    {
+        [Required]
+        public int BearTypeId { get; set; }
+        [Required]
+        [RegularExpression(@"^([A-Z0-9][a-zA-Z0-9#]*\s)*([A-Z0-9][a-zA-Z0-9#]*)$")]
+        [DefaultValue("default value")]
+        public string BearName { get; set; }
+        [Required]
+        [Range(15, double.MaxValue)]
+        public double Weight { get; set; }
+        [Required]
+        public string Characteristics { get; set; }
+        [Required]
+        public string CareNeeds { get; set; }
+        [Required]
+        public DateTime ModifiedDate { get; set; }
+    }
+}
