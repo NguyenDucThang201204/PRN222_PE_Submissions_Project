9d2b5ad418ed1cd10cc1c6b0be49e39a6538b664
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:00:55 +0700

    Upload DAL/Entities/BearAccount.cs

---
+++ DAL/Entities/BearAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Entities;
+
+public partial class BearAccount
+{
+    public int AccountId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string? FullName { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public int? RoleId { get; set; }
+}
