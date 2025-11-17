854599eeb248e2e94782f974d389354c7bf764bd
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:39:27 +0700

    Upload Services/BearTypeService.cs

---
+++ Services/BearTypeService.cs
@@ -0,0 +1,32 @@
+﻿using Repositories;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class BearTypeService
+    {
+        private readonly BearTypeRepository _repository;
+
+        public BearTypeService(BearTypeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<BearType> GetByIdAsync(int id)
+        {
+            try
+            {
+                return await _repository.GetByIdAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
