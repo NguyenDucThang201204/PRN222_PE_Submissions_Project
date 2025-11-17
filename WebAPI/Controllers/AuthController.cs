73309b927c68a0e12c11330eea8323907eda943b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:19 +0700

    Upload WebAPI/Controllers/AuthController.cs

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.Dto;
+using Services;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AccountService _service;
+
+        public AuthController(AccountService accountService)
+        {
+            _service = accountService;
+        }
+
+        [HttpPost("Accounts/Login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            try
+            {
+                var response = await _service.Authenticate(request);
+                if (response == null)
+                {
+                    return NotFound();
+                }
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
