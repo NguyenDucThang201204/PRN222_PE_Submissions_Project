22095018254b173f63ac7c2e20214b9a993e4aff
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:01:29 +0700

    Upload PE_PRN232_FA25_AnhTHQ_BE/PE_PRN232_FA25_AnhTHQ_DAL/Repostitories/BearAccountRepos.cs

---
+++ PE_PRN232_FA25_AnhTHQ_BE/PE_PRN232_FA25_AnhTHQ_DAL/Repostitories/BearAccountRepos.cs
@@ -0,0 +1,26 @@
+﻿using PE_PRN232_FA25_AnhTHQ_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_AnhTHQ_DAL.Repostitories
+{
+    public class BearAccountRepos
+    {
+        private readonly FA25BearDBContext _context;
+
+        public BearAccountRepos()
+        {
+            _context = new FA25BearDBContext();
+
+        }
+
+        public BearAccount Login(string email, string password)
+        {
+            return _context.BearAccounts
+                .FirstOrDefault(a => a.Email == email && a.Password == password);
+        }
+    }
+}
