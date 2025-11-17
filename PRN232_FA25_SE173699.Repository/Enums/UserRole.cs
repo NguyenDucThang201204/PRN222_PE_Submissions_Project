bfc33243b72fe8fde20e229d077cf4bd2cf6d627
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:22 +0700

    Upload PRN232_FA25_SE173699.Repository/Enums/UserRole.cs

---
+++ PRN232_FA25_SE173699.Repository/Enums/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_FA25_SE173699.Repository.Enums
+{
+    public enum UserRole
+    {
+        administrator = 1,
+        moderator = 2,
+        developer = 3,
+        member = 4,
+    }
+}
