756f4638fab13106820b88f37dea1fda32a2adef
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:39:03 +0700

    Upload PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository/ModelExtension/PaginationResult.cs

---
+++ PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository/ModelExtension/PaginationResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository.ModelExtension
+{
+    public class PaginationResult<T> where T : class
+    {
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSizes { get; set; }
+        public int CurrentPages { get; set; }
+
+        public T Items { get; set; }
+
+    }
+}
