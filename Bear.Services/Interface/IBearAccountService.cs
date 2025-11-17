44e738864c86c46f71b3ce2a47df91e15e58d0b9
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:19 +0700

    Upload Bear.Services/Interface/IBearAccountService.cs

---
+++ Bear.Services/Interface/IBearAccountService.cs
@@ -0,0 +1,15 @@
+﻿using Bear.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bear.Services.Interface
+{
+    public interface IBearAccountService
+    {
+        Task<BearAccountDTO> GetAccountByEmail(string email, string password);
+    }
+
+}
