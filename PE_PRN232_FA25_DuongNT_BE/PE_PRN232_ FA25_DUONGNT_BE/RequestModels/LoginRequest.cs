7a9962b6c216b7803a8ab101d9071297dc6c9cb1
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:47 +0700

    Upload PE_PRN232_FA25_DuongNT_BE/PE_PRN232_ FA25_DUONGNT_BE/RequestModels/LoginRequest.cs

---
+++ PE_PRN232_FA25_DuongNT_BE/PE_PRN232_ FA25_DUONGNT_BE/RequestModels/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace PE_PRN232__FA25_DUONGNT_BE.RequestModels
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
