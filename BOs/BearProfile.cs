24130709f7517879a54efdb6bef18145e2bbcbd5
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:17:32 +0700

    Upload BOs/BearProfile.cs

---
+++ BOs/BearProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BOs;
+
+public partial class BearProfile
+{
+    public string BearProfileId { get; set; } = null!;
+
+    public string? BearTypeId { get; set; }
+
+    public string? BearName { get; set; }
+
+    public double? BearWeight { get; set; }
+
+    public string? Characteristics { get; set; }
+
+    public string? CareNeeds { get; set; }
+
+    public DateOnly? ModifiedDate { get; set; }
+
+    public virtual BearType? BearType { get; set; }
+}
