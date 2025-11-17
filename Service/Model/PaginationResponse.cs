387968026a6930937f2cd2009e139c66a4c6a566
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:12 +0700

    Upload Service/Model/PaginationResponse.cs

---
+++ Service/Model/PaginationResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Service.Model
+{
+    public class PaginationResponse
+    {
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public IEnumerable<object> Items { get; set; } = Enumerable.Empty<object>();
+    }
+}
