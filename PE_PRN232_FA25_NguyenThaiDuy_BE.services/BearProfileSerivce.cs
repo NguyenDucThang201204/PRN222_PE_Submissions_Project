5fabd9ff8581a9ad26c8ff40afff81d1c5573984
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:12:03 +0700

    Upload PE_PRN232_FA25_NguyenThaiDuy_BE.services/BearProfileSerivce.cs

---
+++ PE_PRN232_FA25_NguyenThaiDuy_BE.services/BearProfileSerivce.cs
@@ -0,0 +1,34 @@
+﻿using PE_PRN232_FA25_NguyenThaiDuy_BE.Repositories;
+using PE_PRN232_FA25_NguyenThaiDuy_BE.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_NguyenThaiDuy_BE.services
+{
+    public class BearProfileSerivce : IBearProfileService
+    {
+        private readonly IBearProfileRepository _repository;
+
+        public BearProfileSerivce(IBearProfileRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<IEnumerable<BearProfile>> GetAllAsync() => _repository.GetAllAsync();
+
+        public Task<BearProfile?> GetByIdAsync(int id) => _repository.GetByIdAsync(id);
+
+        public Task<BearProfile> CreateAsync(BearProfileDto dto) => _repository.CreateAsync(dto);
+
+        public Task<bool> UpdateAsync(int id, BearProfileDto dto) => _repository.UpdateAsync(id, dto);
+
+        public Task<bool> DeleteAsync(int id) => _repository.DeleteAsync(id);
+
+        public Task<IEnumerable<BearProfile>> SearchAsync(string? bearName, double? bearWeight)
+            => _repository.SearchAsync(bearName, bearWeight);
+    }
+}
+
