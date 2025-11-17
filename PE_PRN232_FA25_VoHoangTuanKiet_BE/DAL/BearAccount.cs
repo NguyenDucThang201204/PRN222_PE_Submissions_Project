cba37fe9195661d0d1bc2aa24128dd7ea272b091
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:17:04 +0700

    Upload PE_PRN232_FA25_VoHoangTuanKiet_BE/DAL/BearAccount.cs

---
+++ PE_PRN232_FA25_VoHoangTuanKiet_BE/DAL/BearAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DAL;
+
+public partial class BearAccount
+{
+    public int AccountId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public int RoleId { get; set; }
+
+    public string GetRoleName()
+    {
+        return RoleId switch
+        {
+            1 => "Admin",
+            2 => "Manager",
+            3 => "Staff",
+            4 => "Member",
+        };
+    }
+}
