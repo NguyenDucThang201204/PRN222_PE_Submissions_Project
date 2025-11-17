34a7e8a6cfab2896af8c43afafc56b3bc5d153c5
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:12:16 +0700

    Upload BLL/BearProfile.cs

---
+++ BLL/BearProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BO;
+
+public partial class BearProfile
+{
+    public int BearProfileId { get; set; }
+
+    public int? BearTypeId { get; set; }
+
+    public string BearName { get; set; } = null!;
+
+    public string? Characteristics { get; set; }
+
+    public string? CareNeeds { get; set; }
+
+    public decimal? BearWeight { get; set; }
+
+    public int? Stock { get; set; }
+
+    public DateOnly? ModifiedDate { get; set; }
+
+    public virtual BearType? BearType { get; set; }
+}
