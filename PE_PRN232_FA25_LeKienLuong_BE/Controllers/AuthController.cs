58426ea6e2569878c507086300bcdbbb41a36b64
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:33 +0700

    Upload PE_PRN232_FA25_LeKienLuong_BE/Controllers/AuthController.cs

---
+++ PE_PRN232_FA25_LeKienLuong_BE/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Repo.Models;
+using Service;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PE_PRN232_FA25_LeKienLuong_BE.Controllers
+{
+    [Route("api/Accounts")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _service;
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthService service, IConfiguration config)
+        {
+            _service = service;
+            _config = config;
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var user = await _service.Authenticate(request.Email, request.Password);
+
+            if (user == null)
+                return Unauthorized();
+
+            var token = GenerateJSONWebToken(user);
+
+            return Ok(token);
+        }
+
+        private string GenerateJSONWebToken(BearAccount bearAcc)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"]
+                , _config["Jwt:Audience"]
+                , new Claim[]
+                {
+                    new(ClaimTypes.Name, bearAcc.Email),
+                    new(ClaimTypes.Role, bearAcc.RoleId.ToString()),
+                },
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenString;
+        }
+
+        public sealed record LoginRequest(string Email, string Password);
+    }
+}
