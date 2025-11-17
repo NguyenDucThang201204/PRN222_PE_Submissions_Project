53e37b7024fcde2d8e26201f47610cf3e68d6ed7
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:48:07 +0700

    Upload PE_PRN232_FA25_TruongNN_BE/Repositores/BearProfileRepository.cs

---
+++ PE_PRN232_FA25_TruongNN_BE/Repositores/BearProfileRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Repositores.AppDBContext;
+using Repositores.Models;
+using Repositories.Basic;
+
+namespace Repositores
+{
+    public class BearProfileRepository : GenericRepository<BearProfile>
+    {
+        public BearProfileRepository() { }
+        private readonly FA25BearDBContext _context;
+        public BearProfileRepository(FA25BearDBContext context) => _context = context;
+        public async Task<List<BearProfile>> GetAllAsync()
+        {
+            var items = await _context.BearProfiles
+                .Include(c => c.BearType)
+                .OrderByDescending(c => c.ModifiedDate)
+                .ToListAsync();
+
+            return items ?? new List<BearProfile>();
+        }
+
+        public async Task<BearProfile> GetByIdAsync(int id)
+        {
+            var item = await _context.BearProfiles
+                .Include(c => c.BearType)
+                .FirstOrDefaultAsync(c => c.BearProfileId == id);
+
+            return item;
+        }
+
+        public async Task<List<BearProfile>> SearchAsync(string? BearName, double? weight)
+        {
+            var items = await _context.BearProfiles
+                .Include(c => c.BearType)
+                .Where(c =>
+                    (string.IsNullOrEmpty(BearName) || c.BearName.Contains(BearName)) &&
+                    (weight == null || weight == 0 || c.Weight == weight)
+                )
+                .ToListAsync();
+
+            return items ?? new List<BearProfile>();
+        }
+    }
+}
