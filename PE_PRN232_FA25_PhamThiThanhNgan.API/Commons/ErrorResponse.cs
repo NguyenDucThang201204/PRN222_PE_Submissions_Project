9557ca04648093bc4af539c7f16d821d40ff78aa
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:09 +0700

    Upload PE_PRN232_FA25_PhamThiThanhNgan.API/Commons/ErrorResponse.cs

---
+++ PE_PRN232_FA25_PhamThiThanhNgan.API/Commons/ErrorResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace PE_PRN232_FA25_PhamThiThanhNgan.API.Commons
+{
+    public class ErrorResponse
+    {
+        public string ErrorCode { get; set; } = null!;
+        public string Message { get; set; } = null!;
+
+        private ErrorResponse(string errorCode, string message)
+        {
+            ErrorCode = errorCode;
+            Message = message;
+        }
+
+        public static ErrorResponse InvalidInput(string message)
+            => new ErrorResponse("HB40001", message);
+        public static ErrorResponse TokenInvalid(string message)
+            => new ErrorResponse("HB40101", message);
+        public static ErrorResponse PermissionDenied(string message)
+            => new ErrorResponse("HB40301", message);
+        public static ErrorResponse ResourceNotFound(string message)
+            => new ErrorResponse("HB40401", message);
+        public static ErrorResponse InternalServerError(string message)
+            => new ErrorResponse("HB50001", message);
+    }
+}
