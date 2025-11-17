5b399344bfe1f55eecb31817930dc4033ae8f510
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:15:36 +0700

    Upload PE_PRN232_FA25_LeCongHung_BLL/DTOs/BearProfileUpdateDTO.cs

---
+++ PE_PRN232_FA25_LeCongHung_BLL/DTOs/BearProfileUpdateDTO.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PE_PRN232_FA25_LeCongHung_BLL.DTOs
+{
+    public class BearProfileUpdateDTO
+    {
+        [Required]
+        public int BearProfileId { get; set; }
+
+        [Required]
+        public int BearTypeId { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
+        public string BearName { get; set; } = null!;
+
+        [Required]
+        public decimal BearWeight { get; set; }
+
+        [Required]
+        public string Characteristics { get; set; } = null!;
+
+        [Required]
+        public string CareNeeds { get; set; } = null!;
+    }
+}
+
