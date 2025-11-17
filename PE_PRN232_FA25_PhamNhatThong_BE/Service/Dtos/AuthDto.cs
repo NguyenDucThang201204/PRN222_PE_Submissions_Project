c08178c87b361bf7ae15f0427d0061e3d42d661b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:21 +0700

    Upload PE_PRN232_FA25_PhamNhatThong_BE/Service/Dtos/AuthDto.cs

---
+++ PE_PRN232_FA25_PhamNhatThong_BE/Service/Dtos/AuthDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Dtos
+{
+    public class LoginRequest
+    {
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public required string Token { get; set; }
+        public required string Role { get; set; }
+    }
+
+    public class AuthResult
+    {
+        public bool Success { get; set; }
+        public string? Token { get; set; }
+        public string? Role { get; set; }
+        public string? ErrorCode { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
