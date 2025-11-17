4c792d1e9808ec4c78a524bc066f78a0812f9c40
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:44:32 +0700

    Upload FA25Bear.Service/Imp/BearAccountService.cs

---
+++ FA25Bear.Service/Imp/BearAccountService.cs
@@ -0,0 +1,22 @@
+﻿using FA25Bear.DataAccess;
+using FA25Bear.DataAccess.Models;
+using FA25Bear.DataAccess.Models.DTOs;
+
+namespace FA25Bear.Service.Imp
+{
+    public class BearAccountService : IBearAccountService
+    {
+        private readonly IBearAccountRepo _repo;
+        public BearAccountService(IBearAccountRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public BearAccount CheckLogin(LoginRequestDTO lr)
+        {
+            string email = lr.Email ?? string.Empty;
+            string password = lr.Password ?? string.Empty;
+            return _repo.GetSystemAccountByEmaild(email, password);
+        }
+    }
+}
