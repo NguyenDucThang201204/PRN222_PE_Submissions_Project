3009d3566755541f18a66f9cd6ae79b64143f81a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:41 +0700

    Upload PE_PRN232_FA25_TangPhat_BE/Service/JWTService.cs

---
+++ PE_PRN232_FA25_TangPhat_BE/Service/JWTService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class JWTService : IJWTService
+    {
+        private readonly IConfiguration _config;
+        public JWTService(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string GenerateToken(string name, string email, int? role)
+        {
+            var jwtSettings = _config.GetSection("Jwt");
+
+            var keyString = jwtSettings["Key"];
+            if (string.IsNullOrEmpty(keyString))
+                throw new InvalidOperationException("JWT Key is missing in configuration.");
+
+            var expireString = jwtSettings["ExpireMinutes"];
+            if (string.IsNullOrEmpty(expireString))
+                throw new InvalidOperationException("JWT ExpireMinutes is missing in configuration.");
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyString));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, name),
+            new Claim(JwtRegisteredClaimNames.Email, email),
+            new Claim(ClaimTypes.Role, role.ToString()),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(double.Parse(expireString)),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
