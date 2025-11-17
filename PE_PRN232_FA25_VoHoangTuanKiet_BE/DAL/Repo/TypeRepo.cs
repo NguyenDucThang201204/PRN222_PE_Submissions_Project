7b5346c1e1a8b92318368638ec086974a72de8e7
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:17:17 +0700

    Upload PE_PRN232_FA25_VoHoangTuanKiet_BE/DAL/Repo/TypeRepo.cs

---
+++ PE_PRN232_FA25_VoHoangTuanKiet_BE/DAL/Repo/TypeRepo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repo
+{
+    public class TypeRepo
+    {
+        private readonly Fa25bearDbContext _context;
+
+        public TypeRepo(Fa25bearDbContext context)
+        {
+            _context = context;
+        }
+
+        public BearType? GetById(int id)
+        {
+            return _context.BearTypes.Find(id);
+        }
+
+        public List<BearType> GetAll()
+        {
+            return _context.BearTypes.ToList();
+        }
+    }
+}
