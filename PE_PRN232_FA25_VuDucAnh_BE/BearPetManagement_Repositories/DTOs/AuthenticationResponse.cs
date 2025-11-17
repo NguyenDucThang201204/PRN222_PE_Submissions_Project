c5e0c3d6037d115eb0aa2f2f726303d9bebca766
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:01:59 +0700

    Upload PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Repositories/DTOs/AuthenticationResponse.cs

---
+++ PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Repositories/DTOs/AuthenticationResponse.cs
@@ -0,0 +1,15 @@
+﻿using BearPetManagement_Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement_Repositories.DTOs
+{
+    public class AuthenticationResponse
+    {
+        public string Token { get; set; }
+        public BearAccount Account { get; set; }
+    }
+}
