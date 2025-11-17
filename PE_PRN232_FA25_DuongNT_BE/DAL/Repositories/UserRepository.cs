7a98ef537ef829bdbdf88b11fa8f3b77a11f8ae0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:43 +0700

    Upload PE_PRN232_FA25_DuongNT_BE/DAL/Repositories/UserRepository.cs

---
+++ PE_PRN232_FA25_DuongNT_BE/DAL/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class UserRepository
+    {
+        private readonly Fa25bearDbContext _context;
+
+        public UserRepository(Fa25bearDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<BearAccount?> GetByEmailAndPasswordAsync(string email, string password)
+        {
+            return await _context.BearAccounts
+                .FirstOrDefaultAsync(a => a.Email == email && a.Password == password );
+        }
+    }
+}
+
