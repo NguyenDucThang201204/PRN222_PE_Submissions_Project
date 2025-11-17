3304e26e524050701a7874a1f528f6a6474718ea
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:17:44 +0700

    Upload Repository/IBearAccountRepo.cs

---
+++ Repository/IBearAccountRepo.cs
@@ -0,0 +1,14 @@
+﻿using BOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IBearAccountRepo
+    {
+        public BearAccount GetSystemAccountByEmail(string email, string pass);
+    }
+}
