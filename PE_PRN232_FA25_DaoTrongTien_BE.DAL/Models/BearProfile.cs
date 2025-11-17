19e775ecd776486df637f836b641f3cb4aa5e248
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:46:09 +0700

    Upload PE_PRN232_FA25_DaoTrongTien_BE.DAL/Models/BearProfile.cs

---
+++ PE_PRN232_FA25_DaoTrongTien_BE.DAL/Models/BearProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PE_PRN232_FA25_DaoTrongTien_BE.DAL.Models;
+
+public partial class BearProfile
+{
+    public int BearProfileId { get; set; }
+
+    public int BearTypeId { get; set; }
+
+    public string? BearName { get; set; }
+
+    public int? BearWeight { get; set; }
+
+    public string? Characteristics { get; set; }
+
+    public bool? CareNeeds { get; set; }
+
+    public DateTime? ModifiedDate { get; set; }
+
+    public virtual BearType BearType { get; set; } = null!;
+}
