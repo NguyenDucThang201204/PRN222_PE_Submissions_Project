78d86075f3f9d3036913b17f504b48bed6c1dc94
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:44:18 +0700

    Upload FA25Bear.DataAccess/IBearProfileRepo.cs

---
+++ FA25Bear.DataAccess/IBearProfileRepo.cs
@@ -0,0 +1,14 @@
+﻿using FA25Bear.DataAccess.Models;
+
+namespace FA25Bear.DataAccess
+{
+    public interface IBearProfileRepo
+    {
+        List<BearProfile> GetAllBearProfiles();
+        BearProfile? GetBearProfileById(int id);
+        void AddBearProfile(BearProfile bp);
+        void UpdateBearProfile(BearProfile bp);
+        void DeleteBearProfile(int id);
+        IQueryable<BearProfile> Search(string? modelName, int? weight);
+    }
+}
