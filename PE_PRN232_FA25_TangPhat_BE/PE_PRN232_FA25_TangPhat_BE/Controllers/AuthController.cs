8d0d68131e9e8eca124aa0a25cdbd28260dce7de
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:47 +0700

    Upload PE_PRN232_FA25_TangPhat_BE/PE_PRN232_FA25_TangPhat_BE/Controllers/AuthController.cs

---
+++ PE_PRN232_FA25_TangPhat_BE/PE_PRN232_FA25_TangPhat_BE/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessObjects.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace PE_PRN232_FA25_TangPhat_BE.Controllers
+{
+    [Route("Accounts/Login")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAccountService _acc;
+
+        public AuthController(IAccountService acc)
+        {
+            _acc = acc;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Authenticate([FromBody] LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ErrorResponse.Fail(ErrorCode.HB40001, "Invalid Data"));
+            }
+            var response = await _acc.LoginAsync(request);
+            if (response == null)
+                return BadRequest(ErrorResponse.Fail(ErrorCode.HB40001, "Invalid username or password"));
+            return Ok(response);
+        }
+    }
+}
