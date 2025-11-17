404d3c64a780344974a48bae812f134f8f9fbf8a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:12:02 +0700

    Upload PE_PRN232_FA25_NguyenThaiDuy_BE.services/AccountService.cs

---
+++ PE_PRN232_FA25_NguyenThaiDuy_BE.services/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using PE_PRN232_FA25_NguyenThaiDuy_BE.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_NguyenThaiDuy_BE.services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public AuthResponse Authenticate(string email, string password)
+        {
+            return _accountRepository.Authenticate(email, password);
+        }
+    }
+}
