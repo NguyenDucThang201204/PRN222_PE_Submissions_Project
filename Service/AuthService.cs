d5d9c9766d8ab2a54e1a7037d3b513d2951fc65d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:01 +0700

    Upload Service/AuthService.cs

---
+++ Service/AuthService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Repository;
+using Repository.Models;
+using Service.Model;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace Service
+{
+    public class AuthService
+    {
+
+        private readonly UnitOfWork _unitOfWork;
+
+        public AuthService(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<LoginReponse?> Login(LoginDTO loginDTO)
+        {
+            var account = _unitOfWork.GetRepository<BearAccount>()
+                .GetByPropertyAsync(a => a.Email == loginDTO.UserName && a.Password == loginDTO.Password, tracked: false)
+                .Result;
+            if (account == null)
+            {
+                return null;
+            }
+
+            //Generate JWT Token
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true).Build();
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, account.Email),
+                new Claim("role", account.RoleId.ToString()),
+                new Claim("AccountId", account.AccountId.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var preparedToken = new JwtSecurityToken(
+                issuer: configuration["JwtSettings:Issuer"],
+                audience: configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(int.Parse(configuration["JwtSettings:AccessTokenExpirationMinutes"])),
+                signingCredentials: creds);
+
+            var token = new JwtSecurityTokenHandler().WriteToken(preparedToken);
+
+           return  new LoginReponse
+           {
+               Token = token,
+               Role = account.RoleId,
+           };
+        }
+    }
+}
