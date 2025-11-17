584b9a9ba57161f8c25f23f8a3f1cd925f12a649
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:52 +0700

    Upload PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_BLL/DTO/AuthenResponse.cs

---
+++ PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_BLL/DTO/AuthenResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE__PRN232_FA25_NamPK_BLL.DTO
+{
+    public class AuthenResponse
+    {
+        public string Token { get; set; }
+    }
+}
