5056349ff7db50650f8794522a4b3ac9007c9f34
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:16 +0700

    Upload PRN232_FA25_SE173699.API/Controllers/AuthController.cs

---
+++ PRN232_FA25_SE173699.API/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRN232_FA25_SE173699.API.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
