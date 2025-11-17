9f30cb4e4f45fa316d7daab4c802b63452600a0e
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:12:19 +0700

    Upload BO/BearAccountDAO.cs

---
+++ BO/BearAccountDAO.cs
@@ -0,0 +1,24 @@
+﻿using BO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class BearAccountDAO
+    {
+        private readonly Fa25bearDbContext _context;
+        public BearAccountDAO(Fa25bearDbContext context)
+        {
+            _context = context;
+        }
+        public BearAccount Login(string userName ,string password) {
+
+            var account = _context.BearAccounts.FirstOrDefault(a => a.Username == userName && a.Password == password);
+        return account;    
+        }
+    }
+}
