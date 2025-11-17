23a1dbcb920f310889c94c30a18411e3c18a234b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:14 +0700

    Upload PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Repository/IBearAccountRepository.cs

---
+++ PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Repository/IBearAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IBearAccountRepository
+    {
+        public BearAccount GetBearAccount(string email, string password);
+
+    }
+}
