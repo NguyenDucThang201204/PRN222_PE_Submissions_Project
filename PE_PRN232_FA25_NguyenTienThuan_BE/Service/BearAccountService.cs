8e08f284657a36b7721f53252487ffa368354189
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:34 +0700

    Upload PE_PRN232_FA25_NguyenTienThuan_BE/Service/BearAccountService.cs

---
+++ PE_PRN232_FA25_NguyenTienThuan_BE/Service/BearAccountService.cs
@@ -0,0 +1,30 @@
+﻿using Repository.Models;
+using Repository.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class BearAccountService
+    {
+        private BearAccountRepo bearAccountRepo;
+
+        public BearAccountService(BearAccountRepo bearAccountRepo)
+        {
+            this.bearAccountRepo = bearAccountRepo;
+        }
+
+        public async Task<BearAccount> GetAccountByEmailAndPassword(string email, string password)
+        {
+            var account = await bearAccountRepo.GetAccountByEmail(email);
+            if (account == null || account.Password != password)
+            {
+                throw new UnauthorizedAccessException("Invalid email or password.");
+            }
+            return account;
+        }
+    }
+}
