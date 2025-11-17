b45c15b46cd5b6431fe39daa66bfca2aa9744a2a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:40:37 +0700

    Upload Services.ThienNH/IBearTypeService.cs

---
+++ Services.ThienNH/IBearTypeService.cs
@@ -0,0 +1,15 @@
+﻿using Repositories.ThienNH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ThienNH
+{
+    public interface IBearTypeService
+    {
+        Task<List<BearType>> GetAllAsync();
+        Task<BearType> GetByIdAsync(int id);
+    }
+}
