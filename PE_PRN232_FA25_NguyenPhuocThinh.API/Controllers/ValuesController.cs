86be53fa969ed6ac9001841f676364cbfa5be232
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:41 +0700

    Upload PE_PRN232_FA25_NguyenPhuocThinh.API/Controllers/ValuesController.cs

---
+++ PE_PRN232_FA25_NguyenPhuocThinh.API/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PE_PRN232_FA25_NguyenPhuocThinh.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
