420e0796b33aa628408e8427eb493a39d4cfd12d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:10:52 +0700

    Upload Repos/ModelExtensions/SearchRequest.cs

---
+++ Repos/ModelExtensions/SearchRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repos.ModelExtensions
+{
+    public class SearchRequest
+    {
+        public int? CurrentPage { get; set; }
+
+        public int? PageSize { get; set; } = 10;
+    }
+
+    public class MainSearchRequest : SearchRequest
+    {
+        public string? BearName { get; set; }
+
+        public decimal? BearWeight { get; set; }
+
+        public string? BearTypeName { get; set; }
+    }
+}
