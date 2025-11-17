99ce36e0ff0a0f433d812d1d623092c1dc8714a0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:10:42 +0700

    Upload Repos/SystemAccountsRepo.cs

---
+++ Repos/SystemAccountsRepo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repos.Basic;
+using Repos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repos
+{
+    public class SystemAccountsRepo : GenericRepository<BearAccount>
+    {
+        public SystemAccountsRepo() => _context ??= new
+            DBContext.FA2025BearDBContext();
+        public SystemAccountsRepo(DBContext.FA2025BearDBContext context) => _context = context;
+        public async Task<BearAccount> GetUserAccountAsync(string email, string password)
+        {
+            return await _context.BearAccount.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+
+
+            //return await _context.SystemUserAccounts.FirstOrDefaultAsync(u => u.UserName == userName && u.Password == password && u.IsActive == true);
+            //return await _context.SystemUserAccounts.FirstOrDefaultAsync(u => u.Email == userName && u.Password == password && u.IsActive == true);
+            //return await _context.SystemUserAccounts.FirstOrDefaultAsync(u => u.Phone == userName && u.Password == password && u.IsActive == true);
+            //return await _context.SystemUserAccounts.FirstOrDefaultAsync(u => u.EmployeeCode == userName && u.Password == password && u.IsActive == true);
+        }
+    }
+}
