94fd04e22c6bee57034d7534272be22e965c3c32
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:47 +0700

    Upload BearPetManagement.Repositories/BearAcountRepository.cs

---
+++ BearPetManagement.Repositories/BearAcountRepository.cs
@@ -0,0 +1,24 @@
+﻿using BearPetManagement.Repositories.Basic;
+using BearPetManagement.Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement.Repositories
+{
+    public class BearAcountRepository : GenericRepository<BearAccount>
+    {
+        public BearAcountRepository() { }
+
+        public BearAcountRepository(FA25BearDBContext context)
+        => _context = context;
+
+        public async Task<BearAccount?> GetUserAccount(string email, string password)
+        {
+            return await _context.BearAccounts.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+        }
+    }
+}
