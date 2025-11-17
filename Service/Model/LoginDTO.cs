dd19f912ff5f32e4571d7fe978e6dd3c38797045
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:10 +0700

    Upload Service/Model/LoginDTO.cs

---
+++ Service/Model/LoginDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Service.Model
+{
+    public class LoginDTO
+    {
+
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
