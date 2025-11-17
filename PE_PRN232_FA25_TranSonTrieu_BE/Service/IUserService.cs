4fd35fb1916ff5e7d8e4c75ef5546db6b8c3d2ff
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:46:56 +0700

    Upload PE_PRN232_FA25_TranSonTrieu_BE/Service/IUserService.cs

---
+++ PE_PRN232_FA25_TranSonTrieu_BE/Service/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using DAO.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IUserService
+    {
+        public Task<LoginResponse> Login(LoginRequest loginDto);
+    }
+}
