40d945364591aea968b3c609b9f8ac821e4dcd5a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:41:10 +0700

    Upload PE_PRN232_FA25_DinhLeThinh_BE/Repository/IBearRepo.cs

---
+++ PE_PRN232_FA25_DinhLeThinh_BE/Repository/IBearRepo.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+using Model.DTOs;
+
+namespace Repository
+{
+    public interface IBearRepo
+    {
+        Task<List<BearDto>> GetAllAsync();
+
+        Task<BearDto?> GetByIdAsync(int id);
+
+        Task<BearProfile?> GetByIdAsyncNoDto(int id);
+
+        Task<BearDto?> UpdateAsync(BearProfile bear);
+
+        Task<BearDto?> CreateAsync(BearProfile bear);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
