e1872cdeef82f7303657958a59fb1eb7fe14e8b0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:52 +0700

    Upload BearPetManagement.Services/BearAccountService.cs

---
+++ BearPetManagement.Services/BearAccountService.cs
@@ -0,0 +1,21 @@
+﻿using BearPetManagement.Repositories;
+using BearPetManagement.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement.Services
+{
+    public class BearAccountService
+    {
+        private readonly BearAcountRepository _AccountRepository;
+        public BearAccountService() => _AccountRepository ??= new BearAcountRepository();
+
+        public async Task<BearAccount?> GetUserAccount(string email, string password)
+        {
+            return await _AccountRepository.GetUserAccount(email, password);
+        }
+    }
+}
