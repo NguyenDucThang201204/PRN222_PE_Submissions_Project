e2b79c45ceb4ce991a9b8ef30b43f231fbdc752e
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:46 +0700

    Upload PE_PRN232_FA25_DuongNT_BE/PE_PRN232_ FA25_DUONGNT_BE/RequestModels/ApiErrorResponse.cs

---
+++ PE_PRN232_FA25_DuongNT_BE/PE_PRN232_ FA25_DUONGNT_BE/RequestModels/ApiErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace PE_PRN232__FA25_DUONGNT_BE.RequestModels
+{
+    public class ApiErrorResponse
+    {
+        public string ErrorCode { get; set; } = null!;
+        public int Status { get; set; }
+        public string Message { get; set; } = null!;
+    }
+}
