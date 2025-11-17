cb680470c47fc482ac802213f3608390565d7b84
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:10:41 +0700

    Upload Repos/SubRepo.cs

---
+++ Repos/SubRepo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repos.Basic;
+using Repos.DBContext;
+using Repos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repos
+{
+    public class SubRepo : GenericRepository<BearType>
+    {
+        public SubRepo() => _context ??= new DBContext.FA2025BearDBContext();
+        public SubRepo(FA2025BearDBContext context) => _context = context;
+
+    }
+}
