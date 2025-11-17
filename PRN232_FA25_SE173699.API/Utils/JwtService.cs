11a9b9a5e5787169b6393b034d04bd02fe0e66df
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:18 +0700

    Upload PRN232_FA25_SE173699.API/Utils/JwtService.cs

---
+++ PRN232_FA25_SE173699.API/Utils/JwtService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+using PRN232_FA25_SE173699.Repository.Models;
+
+namespace PRN232_FA25_SE173699.API.Utils
+{
+    public class JwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(BearAccount user)
+        {
+            var jwtSettings = _configuration.GetSection("Jwt");
+            var secretKey = jwtSettings["Secret"];
+            var issuer = jwtSettings["Issuer"];
+            var audience = jwtSettings["Audience"];
+            var expiry = int.Parse(jwtSettings["ExpiryMinutes"] ?? "60");
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, user.AccountId.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email)
+        };
+
+            var token = new JwtSecurityToken(
+                issuer,
+                audience,
+                claims,
+                expires: DateTime.UtcNow.AddMinutes(expiry),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
