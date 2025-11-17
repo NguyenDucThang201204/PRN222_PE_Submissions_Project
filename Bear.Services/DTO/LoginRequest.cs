1ab3b166f2ed91f654062cb7dfcef0994a1dbf99
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:17 +0700

    Upload Bear.Services/DTO/LoginRequest.cs

---
+++ Bear.Services/DTO/LoginRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bear.Services.DTO
+{
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string userName { get; set; } = string.Empty;
+
+        [Required]
+        public string password { get; set; } = string.Empty ;
+    }
+}
