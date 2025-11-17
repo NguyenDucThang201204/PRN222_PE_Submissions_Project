566654a241d7af0f1439d4fd9f88fed16ddf89e4
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:44:30 +0700

    Upload FA25Bear.DataAccess/Repo/BearAccountRepo.cs

---
+++ FA25Bear.DataAccess/Repo/BearAccountRepo.cs
@@ -0,0 +1,19 @@
+﻿using FA25Bear.DataAccess.Models;
+
+namespace FA25Bear.DataAccess.Repo
+{
+    public class BearAccountRepo : IBearAccountRepo
+    {
+        private readonly Fa25bearDbContext _dbContext;
+
+        public BearAccountRepo(Fa25bearDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public BearAccount? GetSystemAccountByEmaild(string email, string password)
+        {
+            return _dbContext.BearAccounts.FirstOrDefault(sa => sa.Email == email && sa.Password == password);
+        }
+    }
+}
