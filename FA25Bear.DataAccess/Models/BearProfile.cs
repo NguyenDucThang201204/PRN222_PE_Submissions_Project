d712f4c9405e4a24eca2081b21161873f94a0e40
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:44:28 +0700

    Upload FA25Bear.DataAccess/Models/BearProfile.cs

---
+++ FA25Bear.DataAccess/Models/BearProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FA25Bear.DataAccess.Models;
+
+public partial class BearProfile
+{
+    public int BearProfileId { get; set; }
+
+    public int? BearTypeId { get; set; }
+
+    public string BearName { get; set; } = null!;
+
+    public int? BearWeight { get; set; }
+
+    public string? Characteristics { get; set; }
+
+    public string? CareNeeds { get; set; }
+
+    public DateOnly? ModifiedDate { get; set; }
+
+    public virtual BearType? BearType { get; set; }
+}
