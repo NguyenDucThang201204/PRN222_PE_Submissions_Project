a0e294cd942d87ce0537e211e1fe8e7222fb9928
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:35 +0700

    Upload PE_PRN232_FA25_TangPhat_BE/Repository/IAccountRepo.cs

---
+++ PE_PRN232_FA25_TangPhat_BE/Repository/IAccountRepo.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IAccountRepo
+    {
+        public Task<BearAccount?> LoginAsync(string email, string pass);
+    }
+}
