3235a2738b2efca1ebf2c8189f97113e254e93bb
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:12 +0700

    Upload PE_PRN232_FA25_PhamNhatThong_BE/Service/DependencyInjection.cs

---
+++ PE_PRN232_FA25_PhamNhatThong_BE/Service/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Service.Security;
+
+
+namespace Service
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddService(this IServiceCollection services)
+        {
+            services.AddSingleton<JwtHelper>();
+            services.AddScoped<AuthService>();
+            services.AddScoped<BearService>();
+            return services;
+        }
+    }
+}
