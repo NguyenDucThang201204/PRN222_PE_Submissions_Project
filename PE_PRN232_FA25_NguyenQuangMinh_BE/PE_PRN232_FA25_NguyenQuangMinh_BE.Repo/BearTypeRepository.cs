37852b5536bc2d68d98be88a53ae37d0e19a58ba
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:22:22 +0700

    Upload PE_PRN232_FA25_NguyenQuangMinh_BE/PE_PRN232_FA25_NguyenQuangMinh_BE.Repo/BearTypeRepository.cs

---
+++ PE_PRN232_FA25_NguyenQuangMinh_BE/PE_PRN232_FA25_NguyenQuangMinh_BE.Repo/BearTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using PE_PRN232_FA25_NguyenQuangMinh_BE.Repo.Basic;
+using PE_PRN232_FA25_NguyenQuangMinh_BE.Repo.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_NguyenQuangMinh_BE.Repo
+{
+    public class BearTypeRepository : GenericRepository<BearTypeRepository>
+    {
+        public BearTypeRepository() => _context ??= new DBContext.FA25BearDBContext();
+        public BearTypeRepository(FA25BearDBContext context) => _context = context;
+    }
+}
