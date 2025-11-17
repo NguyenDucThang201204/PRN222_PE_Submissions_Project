1271dd909b1c71bf37edc92dda27898aef1c85b3
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:40 +0700

    Upload PE_PRN232_FA25_NguyenPhuocThinh.API/Controllers/AccountController.cs

---
+++ PE_PRN232_FA25_NguyenPhuocThinh.API/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Requests;
+using Service;
+
+namespace PE_PRN232_FA25_NguyenPhuocThinh.API.Controllers
+{
+    [Route("api/Accounts")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AccountController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var response = await _authService.LoginAsync(request);
+            return Ok(response);
+        }
+    }
+}
