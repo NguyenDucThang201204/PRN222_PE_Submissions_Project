bf0d30be5ce18e92eed52bcd98e4e377e7e5f762
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:39:58 +0700

    Upload PE_PRN232_FA25_ThangHN_BE/Repository/ModelExtensions/SearchRequest.cs

---
+++ PE_PRN232_FA25_ThangHN_BE/Repository/ModelExtensions/SearchRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Repository.ModelExtensions
+{
+    public class SearchRequest
+    {
+        public int? CurrentPage { get; set; }
+        public int? PageSize { get; set; }
+
+    }
+
+    public class SearchRequestDto : SearchRequest
+    {
+        public string? BearName { get; set; }
+        public double? BearWeight { get; set; }
+        public string? BearTypeName { get; set; }
+    }
+}
