130a5300f1da7039d350a9469b23a559554b9083
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:09:28 +0700

    Upload PE_PRN232_FA25_NguyenThanhBinh_BE/Repo/AccountRepo.cs

---
+++ PE_PRN232_FA25_NguyenThanhBinh_BE/Repo/AccountRepo.cs
@@ -0,0 +1,34 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo
+{
+    public class AccountRepo : IAccountRepo
+    {
+        private readonly Fa25bearDbContext _db;
+        private static AccountRepo _instance;
+        public AccountRepo()
+        {
+            _db = new Fa25bearDbContext();
+        }
+        public static AccountRepo Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new AccountRepo();
+                }
+                return _instance;
+            }
+        }
+        public BearAccount GetAccountByEmail(string email, string pass)
+        {
+            return _db.BearAccounts.FirstOrDefault(a => a.Email.Equals(email) && a.Password.Equals(pass));
+        }
+    }
+}
