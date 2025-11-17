1e94d78b88ab7fd9886a4a2724bc2b7ba14ab74d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:17:21 +0700

    Upload PE_PRN232_FA25_VoHoangTuanKiet_BE/PE_PRN232_FA25_VoHoangTuanKiet_BE/Models/LoginModel.cs

---
+++ PE_PRN232_FA25_VoHoangTuanKiet_BE/PE_PRN232_FA25_VoHoangTuanKiet_BE/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PE_PRN232_FA25_VoHoangTuanKiet_BE.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
