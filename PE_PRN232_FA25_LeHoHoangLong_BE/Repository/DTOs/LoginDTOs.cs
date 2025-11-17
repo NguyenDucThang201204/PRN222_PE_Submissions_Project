f5db37a27f87b4d7a94c0fac38da5465e6188ddd
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:18:13 +0700

    Upload PE_PRN232_FA25_LeHoHoangLong_BE/Repository/DTOs/LoginDTOs.cs

---
+++ PE_PRN232_FA25_LeHoHoangLong_BE/Repository/DTOs/LoginDTOs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DTOs
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public int Role { get; set; }
+    }
+}
