ca15359bb95c5ed7dad492b870cbe4fd87ed0848
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:09 +0700

    Upload Service/Model/BearCreateDTO.cs

---
+++ Service/Model/BearCreateDTO.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Service.Model
+{
+    public class BearCreateDTO
+    {
+        public int BearTypeId { get; set; }
+
+        public string BearName { get; set; } = null!;
+       
+        public double Weight { get; set; }
+
+        public string Characteristics { get; set; } = null!;
+
+        public string CareNeeds { get; set; } = null!;
+
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}
