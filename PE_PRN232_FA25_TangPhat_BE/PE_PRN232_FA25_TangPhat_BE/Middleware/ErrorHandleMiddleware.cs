20a44f25ef12e8ccbecd36c6aabf8b80e56025fd
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:49 +0700

    Upload PE_PRN232_FA25_TangPhat_BE/PE_PRN232_FA25_TangPhat_BE/Middleware/ErrorHandleMiddleware.cs

---
+++ PE_PRN232_FA25_TangPhat_BE/PE_PRN232_FA25_TangPhat_BE/Middleware/ErrorHandleMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+
+namespace PE_PRN232_FA25_TangPhat_BE.Controllers
+{
+    public class ErrorHandleMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandleMiddleware> _logger;
+        public ErrorHandleMiddleware(RequestDelegate next, ILogger<ErrorHandleMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+
+                switch (context.Response.StatusCode)
+                {
+                    case (int)HttpStatusCode.Unauthorized:
+                        await WriteError(context, "HB40101", "Token missinginvalid", HttpStatusCode.Unauthorized);
+                        break;
+
+                    case (int)HttpStatusCode.Forbidden:
+                        await WriteError(context, "HB40301", "Permission denied", HttpStatusCode.Forbidden);
+                        break;
+                    case (int)HttpStatusCode.NotFound:
+                        await WriteError(context, "HB40401", "Resource not found", HttpStatusCode.NotFound);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                await WriteError(context, "HB50001", "Internal server error", HttpStatusCode.InternalServerError);
+            }
+        }
+        private async Task WriteError(HttpContext context, string errorCode, string message, HttpStatusCode statusCode)
+        {
+            if (context.Response.HasStarted)
+            {
+                _logger?.LogWarning("The response has already started. Cannot write error response. ErrorCode={ErrorCode} Message={Message}", errorCode, message);
+                return;
+            }
+
+            context.Response.Clear();
+            context.Response.ContentType = "application/json";
+
+            var error = new
+            {
+                code = errorCode,
+                message
+            };
+
+            await context.Response.WriteAsJsonAsync(error);
+        }
+    }
+}
