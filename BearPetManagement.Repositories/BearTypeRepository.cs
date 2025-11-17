36600bc439a8545ded42e5b2b574fddfca954cc5
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:50 +0700

    Upload BearPetManagement.Repositories/BearTypeRepository.cs

---
+++ BearPetManagement.Repositories/BearTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using BearPetManagement.Repositories.Basic;
+using BearPetManagement.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement.Repositories
+{
+    public class BearTypeRepository : GenericRepository<BearType>
+    {
+        public BearTypeRepository() { }
+        public BearTypeRepository(FA25BearDBContext context) => _context = context;
+    }
+}
