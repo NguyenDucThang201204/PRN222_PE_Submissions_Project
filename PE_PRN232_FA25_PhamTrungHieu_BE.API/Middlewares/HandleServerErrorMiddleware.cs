f8896b2af320f60474ec6612afc99ef1382d84d6
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:14:49 +0700

    Upload PE_PRN232_FA25_PhamTrungHieu_BE.API/Middlewares/HandleServerErrorMiddleware.cs

---
+++ PE_PRN232_FA25_PhamTrungHieu_BE.API/Middlewares/HandleServerErrorMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using PE_PRN232_FA25_PhamTrungHieu_BE.DAL.ModelExtensions;
+
+namespace PE_PRN232_FA25_PhamTrungHieu_BE.API.Extensions
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class HandleServerErrorMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public HandleServerErrorMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception)
+            {
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await httpContext.Response.WriteAsJsonAsync(ErrorResponse.InternalException());
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class HandleServerErrorMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHandleServerErrorMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HandleServerErrorMiddleware>();
+        }
+    }
+}
