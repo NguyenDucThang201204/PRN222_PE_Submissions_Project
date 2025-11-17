4061961a025ed52e712dda49d81930e52aed2cfa
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:14:55 +0700

    Upload PE_PRN232_FA25_PhamTrungHieu_BE.DAL/ModelExtensions/ErrorResponse.cs

---
+++ PE_PRN232_FA25_PhamTrungHieu_BE.DAL/ModelExtensions/ErrorResponse.cs
@@ -0,0 +1,50 @@
+﻿namespace PE_PRN232_FA25_PhamTrungHieu_BE.DAL.ModelExtensions
+{
+    public class ErrorResponse
+    {
+        public string ErrorCode { get; set; }
+        public string Message { get; set; }
+
+        public static ErrorResponse Invalid(string? newMessage = "")
+        {
+            return new ErrorResponse
+            {
+                ErrorCode = "HB40001",
+                Message = !string.IsNullOrEmpty(newMessage) ? newMessage : "Missing/invalid input"
+            };
+        }
+        public static ErrorResponse Unauthor(string? newMessage = "")
+        {
+            return new ErrorResponse
+            {
+                ErrorCode = "HB40101",
+                Message = !string.IsNullOrEmpty(newMessage) ? newMessage : "Token missing/invalid"
+            };
+        }
+        public static ErrorResponse Deny(string? newMessage = "")
+        {
+            return new ErrorResponse
+            {
+                ErrorCode = "HB40301",
+                Message = !string.IsNullOrEmpty(newMessage) ? newMessage : "Permission denied"
+            };
+        }
+        public static ErrorResponse NotFound(string? newMessage = "")
+        {
+            return new ErrorResponse
+            {
+                ErrorCode = "HB40401",
+                Message = !string.IsNullOrEmpty(newMessage) ? newMessage : "Resource not found"
+            };
+        }
+        public static ErrorResponse InternalException(string? newMessage = "")
+        {
+            return new ErrorResponse
+            {
+                ErrorCode = "HB50001",
+                Message = !string.IsNullOrEmpty(newMessage) ? newMessage : "Internal server error"
+            };
+        }
+    }
+
+}
