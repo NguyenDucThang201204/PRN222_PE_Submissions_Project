142d03d7e8f56805d3c4fcbc12b101646e9983ca
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:53 +0700

    Upload PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_BLL/DTO/AuthRequest.cs

---
+++ PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_BLL/DTO/AuthRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE__PRN232_FA25_NamPK_BLL.DTO
+{
+    public class AuthRequest
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; } 
+    }
+}
