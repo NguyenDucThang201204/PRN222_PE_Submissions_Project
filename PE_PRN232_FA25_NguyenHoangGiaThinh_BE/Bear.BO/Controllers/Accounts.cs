981f277e1e1508b840f61823d6d24bb0c2264e6a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:24 +0700

    Upload PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Bear.BO/Controllers/Accounts.cs

---
+++ PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Bear.BO/Controllers/Accounts.cs
@@ -0,0 +1,31 @@
+﻿using BO.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace Bear.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class Accounts : ControllerBase
+    {
+        private readonly IBearAccountService _bearAccountService;
+
+        public Accounts (IBearAccountService bearAccountService)
+        {
+            _bearAccountService = bearAccountService;
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] LoginRequest login)
+        {
+            var result = _bearAccountService.Login(login);
+            if (result == null)
+            {
+                return Unauthorized("Invalid Email or password");
+            }
+
+            return Ok(result);
+        }
+    }
+}
