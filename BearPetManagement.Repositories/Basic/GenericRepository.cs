b9efc2fa711efb9c7054ccf1ebdd5e999944e95a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:58 +0700

    Upload BearPetManagement.Repositories/Basic/GenericRepository.cs

---
+++ BearPetManagement.Repositories/Basic/GenericRepository.cs
@@ -0,0 +1,60 @@
+﻿using BearPetManagement.Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement.Repositories.Basic
+{
+    public class GenericRepository<T> where T : class
+    {
+        protected FA25BearDBContext _context;
+
+        public GenericRepository()
+        {
+            _context ??= new FA25BearDBContext();
+        }
+
+        public GenericRepository(FA25BearDBContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<int> CreateAsync(T entity)
+        {
+            _context.Add(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> UpdateAsync(T entity)
+        {
+            _context.ChangeTracker.Clear();
+            var tracker = _context.Attach(entity);
+            tracker.State = EntityState.Modified;
+            return await _context.SaveChangesAsync();
+        }
+
+
+        public async Task<bool> RemoveAsync(T entity)
+        {
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+    }
+}
