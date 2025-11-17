73b9984008a9ca521bd0cdc58aa7895d152fec54
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:35 +0700

    Upload PE_PRN232_FA25_TangPhat_BE/Repository/IProfileRepo.cs

---
+++ PE_PRN232_FA25_TangPhat_BE/Repository/IProfileRepo.cs
@@ -0,0 +1,29 @@
+﻿using BusinessObjects;
+using BusinessObjects.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IProfileRepo
+    {
+        Task<List<GetDTO>> GetAllAsync();
+
+        Task<GetDTO?> GetById(int id);
+
+        Task<GetDTO?> CreateAsync(BearProfile profile);
+
+        Task<BearType?> GetTypeById(int id);
+
+        Task<BearProfile?> GetByIdForUpdate(int id);
+
+        Task<GetDTO?> UpdateAsync(BearProfile profile);
+
+        Task<bool> DeleteAsync(int id);
+
+    }
+}
+
