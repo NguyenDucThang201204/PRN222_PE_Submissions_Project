bf05eb92b804b643ed1146e3aebb8afd4ce459c5
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:12 +0700

    Upload Repositories/Dto/AccountDTO.cs

---
+++ Repositories/Dto/AccountDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Dto
+{
+    public class AccountDTO
+    {
+    }
+
+    public class LoginRequest
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public string Role { get; set; }
+    }
+}
