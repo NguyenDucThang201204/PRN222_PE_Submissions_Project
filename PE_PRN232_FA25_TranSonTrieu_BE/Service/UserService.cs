2a9d67b960bd4e72f020d8704fb933db2be7394e
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:46:57 +0700

    Upload PE_PRN232_FA25_TranSonTrieu_BE/Service/UserService.cs

---
+++ PE_PRN232_FA25_TranSonTrieu_BE/Service/UserService.cs
@@ -0,0 +1,28 @@
+﻿using DAO.DTO;
+using DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class UserService : IUserService
+    {
+        private readonly UserDAO _dao;
+        public UserService(UserDAO dao)
+        {
+            _dao = dao;
+        }
+
+        public Task<LoginResponse> Login(LoginRequest loginDto)
+        {
+            if (loginDto == null)
+                throw new ArgumentNullException(nameof(loginDto));
+
+            return _dao.Login(loginDto);
+        }
+
+    }
+}
