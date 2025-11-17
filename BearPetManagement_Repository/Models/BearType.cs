bff7024997995de9bac64330c78d758206cfc667
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:09:12 +0700

    Upload BearPetManagement_Repository/Models/BearType.cs

---
+++ BearPetManagement_Repository/Models/BearType.cs
@@ -0,0 +1,15 @@
+﻿using BearPetManagement_Repository.NewFolder;
+using System.Text.Json.Serialization;
+
+namespace BearPetManagement_Repository.Models
+{
+    public class BearType
+    {
+        public int BearTypeId { get; set; }
+        public string BearTypeName { get; set; }
+        public string Origin { get; set; }
+        public string Description { get; set; }
+        [JsonIgnore]
+        public virtual List<BearProfile> BearProfiles { get; set; }
+    }
+}
