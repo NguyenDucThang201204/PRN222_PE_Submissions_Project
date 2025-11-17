f2952bee9c5140638d3ab3ec5e88a546970aeff9
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:00:52 +0700

    Upload BLL/IServices/ILoginService.cs

---
+++ BLL/IServices/ILoginService.cs
@@ -0,0 +1,14 @@
+﻿using DAL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.IServices
+{
+    public interface ILoginService
+    {
+        Task<LoginResponseDTO> LoginFunc(string email, string password);
+    }
+}
