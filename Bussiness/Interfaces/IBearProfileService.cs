a3ba323ad89b113d161f7dc6e0a4e41ea1cabfe8
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:35:44 +0700

    Upload Bussiness/Interfaces/IBearProfileService.cs

---
+++ Bussiness/Interfaces/IBearProfileService.cs
@@ -0,0 +1,14 @@
+﻿using Data.Entities;
+using System.Collections.Generic;
+
+namespace Business.Interfaces
+{
+    public interface IBearProfileService
+    {
+        IEnumerable<BearProfile> GetAll();
+        BearProfile? GetById(int id);
+        void Add(BearProfile bear);
+        void Update(int id, BearProfile bear);
+        void Delete(int id);
+    }
+}
