6604f56c8e9644ed9a4d9d9b941da98081feedfe
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:37:55 +0700

    Upload PE_PRN232_FA25_PhanXuanPhu_BE/Services/BearProfileService.cs

---
+++ PE_PRN232_FA25_PhanXuanPhu_BE/Services/BearProfileService.cs
@@ -0,0 +1,61 @@
+﻿using Repositories;
+using Repositories.ModelExtensions;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class BearProfileService
+    {
+        private readonly BearProfileRepository _repo;
+
+        public BearProfileService()
+        {
+            _repo = new BearProfileRepository();
+        }
+
+        public async Task<List<BearProfile>> GetAllAsync()
+        {
+            return await _repo.GetAllAsync();
+        }
+
+        public async Task<BearProfile> GetByIdAsync(int id)
+        {
+            return await _repo.GetByIdAsync(id);
+        }
+
+        public async Task<int> CreateAsync(BearProfile item)
+        {
+            return await _repo.CreateAsync(item);
+        }
+
+        public async Task<int> UpdateAsync(BearProfile item)
+        {
+            return await _repo.UpdateAsync(item);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var item = await _repo.GetByIdAsync(id);
+            if (item != null)
+            {
+                return await _repo.RemoveAsync(item);
+            }
+            return false;
+        }
+
+        public async Task<List<BearProfile>> SearchAsync(string? BearName, double? BearWeight)
+        {
+            return await _repo.SearchAsync(BearName, BearWeight);
+        }
+
+        public async Task<PaginationResult<List<BearProfile>>> SearchWithPagingAsync(BearProfileRequest searchRequest)
+        {
+            return await _repo.SearchWithPagingAsync(searchRequest);
+        }
+    }
+}
