16c6d6d4e11e10980394fd34900ee28af75d6199
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:11 +0700

    Upload PRN232_FA25_SE173699.Repository/BearProfileRepository.cs

---
+++ PRN232_FA25_SE173699.Repository/BearProfileRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_FA25_SE173699.Repository
+{
+    public class BearProfileRepository
+    {
+    }
+}
