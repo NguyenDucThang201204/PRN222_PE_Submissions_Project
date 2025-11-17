cd91d85c28be74ea62477eefa09bc8895cbec6f0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:06 +0700

    Upload PE_PRN232_FA25_NguyenHoangGiaThinh_BE/DAO/BearAccountDAO.cs

---
+++ PE_PRN232_FA25_NguyenHoangGiaThinh_BE/DAO/BearAccountDAO.cs
@@ -0,0 +1,24 @@
+﻿using BO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class BearAccountDAO
+    {
+        private readonly Fa25bearDbContext _context;
+        public BearAccountDAO(Fa25bearDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public BearAccount? GetBearAccount(string email, string password)
+        {
+            return _context.BearAccounts.SingleOrDefault(m => m.Emal.Equals(email) && m.Password.Equals(password));
+
+        }
+    }
+}
