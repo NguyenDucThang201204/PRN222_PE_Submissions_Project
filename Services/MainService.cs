e02260a2229514d63c06c4d6a338d99f8266887e
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:35:11 +0700

    Upload Services/MainService.cs

---
+++ Services/MainService.cs
@@ -0,0 +1,36 @@
+﻿using Repositories;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class MainService
+    {
+        private readonly MainRepository _repository;
+
+        public MainService()
+        {
+            _repository ??= new MainRepository();
+        }
+        public async Task<List<BearProfile>> GetAllAsync()
+        {
+            try
+            {
+                return await _repository.GetAllAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while retrieving items: {ex.Message}");
+            }
+        }
+
+        public Task<BearProfile> GetByIdAsync(int id)
+        {
+            return _repository.GetByIdAsync(id);
+        }
+    }
+}
