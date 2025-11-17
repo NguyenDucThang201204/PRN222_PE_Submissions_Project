5e762c8d73e251716889540a3e5f1c90f7356bcc
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:48:07 +0700

    Upload PE_PRN232_FA25_TruongNN_BE/Repositores/BearTypeRepository.cs

---
+++ PE_PRN232_FA25_TruongNN_BE/Repositores/BearTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repositores.Models;
+using Repositories.Basic;
+
+namespace Repositores
+{
+    public class BearTypeRepository : GenericRepository<BearType>
+    {
+    }
+}
