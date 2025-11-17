06124f0033ad2f3c1134ec847ed86ef63e40ec85
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:12 +0700

    Upload WebApi/Controllers/AuthController.cs

---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repository.DTO;
+using Service;
+
+namespace WebApi.api.Controllers
+{
+    [ApiController]
+    [Route("Account/Login")]
+    public class AuthController : Controller
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+
+            if (string.IsNullOrWhiteSpace(request.userName) || string.IsNullOrWhiteSpace(request.Password))
+            {
+                return BadRequest(new
+                {
+                    errorCode = "HB40001",
+                    message = "Missing/invalid input"
+                });
+            }
+
+            var result = await _authService.LoginAsync(request.userName, request.Password);
+
+            if (result == null)
+            {
+                return Unauthorized(new
+                {
+                    errorCode = "HB40101",
+                    message = "Token missing/invalid"
+                });
+            }
+
+            return Ok(result);
+        }
+    }
+}
