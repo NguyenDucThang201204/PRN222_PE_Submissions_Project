3fcb3e41b3a5904a2f88570001b46dca6968ea3a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:30 +0700

    Upload Service/AuthService.cs

---
+++ Service/AuthService.cs
@@ -0,0 +1,30 @@
+﻿using Repo;
+using Repo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IAuthService
+    {
+        Task<BearAccount> Authenticate(string email, string password);
+    }
+
+    public class AuthService : IAuthService
+    {
+        private readonly AuthRepo _repo;
+
+        public AuthService()
+        {
+            _repo = new AuthRepo();
+        }
+
+        public async Task<BearAccount> Authenticate(string email, string password)
+        {
+            return await _repo.GetAccount(email, password);
+        }
+    }
+}
