d006f8b8751cd3486d87f73bd5e630767066a88d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:47:14 +0700

    Upload Repository/AccountRepository.cs

---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOs.Models;
+using DAOs;
+
+namespace Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public async Task<BearAccount> Login(string email, string password)
+        {
+            return await AccountDAO.Instance.Login(email, password);
+        }
+    }
+}
