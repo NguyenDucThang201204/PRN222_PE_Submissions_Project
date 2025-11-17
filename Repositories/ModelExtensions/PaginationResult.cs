277e8571c0a5da285185249f5041e1b3674a00d1
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:39:32 +0700

    Upload Repositories/ModelExtensions/PaginationResult.cs

---
+++ Repositories/ModelExtensions/PaginationResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.ModelExtensions
+{
+    public class PaginationResult<T> where T : class
+    {
+        public int TotaItems { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+
+        public T Items { get; set; }
+    }
+
+}
