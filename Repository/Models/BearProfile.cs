d838a8d58f4cfbf49cdf76e3cc319b3862cb40eb
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:40:18 +0700

    Upload Repository/Models/BearProfile.cs

---
+++ Repository/Models/BearProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repository.Models;
+
+public partial class BearProfile
+{
+    public int BearProfileId { get; set; }
+
+    public int BearTypeId { get; set; }
+
+    public string BearName { get; set; } = null!;
+
+    public decimal BearWeight { get; set; }
+
+    public string Characteristics { get; set; } = null!;
+
+    public string CareNeeds { get; set; } = null!;
+
+    public DateTime ModifiedDate { get; set; }
+
+    public virtual BearType BearType { get; set; } = null!;
+}
