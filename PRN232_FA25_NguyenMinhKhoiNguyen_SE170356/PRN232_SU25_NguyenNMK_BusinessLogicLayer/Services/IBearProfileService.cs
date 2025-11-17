5672037c133da20acc24e3135f6349af946a559a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:47 +0700

    Upload PRN232_FA25_NguyenMinhKhoiNguyen_SE170356/PRN232_SU25_NguyenNMK_BusinessLogicLayer/Services/IBearProfileService.cs

---
+++ PRN232_FA25_NguyenMinhKhoiNguyen_SE170356/PRN232_SU25_NguyenNMK_BusinessLogicLayer/Services/IBearProfileService.cs
@@ -0,0 +1,19 @@
+﻿using PRN232_SU25_NguyenNMK_DataAccessLayer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_SU25_NguyenNMK_BusinessLogicLayer.Services
+{
+    public interface IBearProfileService
+    {
+        Task<IEnumerable<BearDTO>> GetAllAsync(string productName = null, string material = null);
+        Task<BearDTO> GetByIdAsync(int id);
+        Task<BearDTO> CreateAsync(BearCreateUpdateDTO dto);
+        Task<BearDTO> UpdateAsync(int id, BearCreateUpdateDTO dto);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<IGrouping<string, BearDTO>>> SearchAsync(string productName, string material);
+    }
+}
