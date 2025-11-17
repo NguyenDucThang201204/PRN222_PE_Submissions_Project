f6a15ecf6d10469fa6a6844d0ba149b69855cbac
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:10:17 +0700

    Upload Service/AuthService.cs

---
+++ Service/AuthService.cs
@@ -0,0 +1,26 @@
+﻿using Repository.Models;
+using Repository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IGenericRepository<BearAccount> _repository;
+
+        public AuthService(IGenericRepository<BearAccount> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<BearAccount> Login(string userName, string password)
+        {
+            var account = await _repository.GetSingleByConditionAsynce(u => u.Email == userName & u.Password == password);
+            return account;
+        }
+    }
+}
