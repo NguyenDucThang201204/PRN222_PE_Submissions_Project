cede783ea5634b4471fd73fcf3c1bbddeb008f09
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:15:00 +0700

    Upload PE_PRN232_FA25_PhamTrungHieu_BE.DAL/Repositories/Class1.cs

---
+++ PE_PRN232_FA25_PhamTrungHieu_BE.DAL/Repositories/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_PhamTrungHieu_BE.DAL.Repositories
+{
+    internal class Class1
+    {
+    }
+}
