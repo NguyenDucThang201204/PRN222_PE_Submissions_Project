7e9dbf8e000dfc3ecb362c9006df6105ab555a8b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:47:08 +0700

    Upload PE_PRN232_FA25_NguyenThiMaiTrinh_BE/LoginDTO.cs

---
+++ PE_PRN232_FA25_NguyenThiMaiTrinh_BE/LoginDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace PE_PRN232_FA25_NguyenThiMaiTrinh_BE
+{
+    public class LoginRequestDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponseDTO
+    {
+        public string Token { get; set; }
+        public string Role { get; set; }
+
+    }
+}
