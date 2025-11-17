4dd2e6ee4931df33798e2d73ab918267e5604c6d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:46:14 +0700

    Upload PE_PRN232_FA25_DaoTrongTien_BE.DAL/Repositories/IBearAccountRepository.cs

---
+++ PE_PRN232_FA25_DaoTrongTien_BE.DAL/Repositories/IBearAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using PE_PRN232_FA25_DaoTrongTien_BE.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_DaoTrongTien_BE.DAL.Repositories
+{
+    public interface IBearAccountRepository
+    {
+        Task<BearAccount> GetBearAccount(string email, string password);
+    }
+}
