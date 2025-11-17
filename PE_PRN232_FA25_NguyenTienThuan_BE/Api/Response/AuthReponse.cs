91c2fa929300d95bb2afd31e7e9f9f2993bc1b56
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:41 +0700

    Upload PE_PRN232_FA25_NguyenTienThuan_BE/Api/Response/AuthReponse.cs

---
+++ PE_PRN232_FA25_NguyenTienThuan_BE/Api/Response/AuthReponse.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace Api.Response
+{
+    public class AuthReponse
+    {
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
+
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+    }
+}
