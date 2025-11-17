0068108e27e142dca2ded69ce71ca9749d2bc638
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:09:12 +0700

    Upload BearPetManagement_Repository/Repositories/BearRepository.cs

---
+++ BearPetManagement_Repository/Repositories/BearRepository.cs
@@ -0,0 +1,13 @@
+﻿using BearPetManagement_Repository.NewFolder;
+using Microsoft.EntityFrameworkCore;
+
+namespace BearPetManagement_Repository.Repositories
+{
+    public class BearRepository : GenericRepository<BearProfile>
+    {
+        public override async Task<List<BearProfile>> GetAllAsync()
+        {
+            return await _context.BearProfiles.Include(b => b.BearType).ToListAsync();
+        }
+    }
+}
