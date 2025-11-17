7d3617cff5be67fdc85b1fdd2fe041b84b094330
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:01:54 +0700

    Upload PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Services/JwtService.cs

---
+++ PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Services/JwtService.cs
@@ -0,0 +1,47 @@
+﻿using BearPetManagement_Repositories.JWT;
+using BearPetManagement_Repositories.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement_Services
+{
+    public class JwtService
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public JwtService(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public string GenerateToken(BearAccount account)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Email, account.Email ?? ""),
+            new Claim(ClaimTypes.Name, account.FullName ?? ""),
+            new Claim("Roles", account.RoleId.ToString() ?? "")
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpirationInMinutes),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
