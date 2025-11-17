4efb30609521b7bd23bcea6320a47de8098d4985
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:10:19 +0700

    Upload Service/IItemService.cs

---
+++ Service/IItemService.cs
@@ -0,0 +1,14 @@
+﻿using Repository.Models;
+
+namespace Service
+{
+    public interface IItemService
+    {
+        Task<BearProfile> AddAsync(BearProfile item);
+        Task<bool> DeleteAsync(int id);
+        Task<List<BearProfile>> GetAll();
+        Task<BearProfile> GetByIdAsync(int id);
+        Task<List<BearProfile>> Search(string bearName, double? weight, string? bearTypeName);
+        Task<BearProfile> UpdateAsynce(BearProfile item);
+    }
+}
