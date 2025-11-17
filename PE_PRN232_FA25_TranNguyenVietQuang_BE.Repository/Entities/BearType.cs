eccf8042804dec9303158ba6ebed6506fadad41a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:38:55 +0700

    Upload PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository/Entities/BearType.cs

---
+++ PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository/Entities/BearType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository.Entities;
+
+public partial class BearType
+{
+    [Key]
+    public string BearTypeId { get; set; } = null!;
+
+    public string? BearTypeName { get; set; }
+
+    public string? Origin { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<BearProfile> BearProfiles { get; set; } = new List<BearProfile>();
+}
