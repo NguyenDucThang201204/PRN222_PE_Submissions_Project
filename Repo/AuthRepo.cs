f90a28a6e3704a58c20099bf338fa8ad8b4761a4
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:26 +0700

    Upload Repo/AuthRepo.cs

---
+++ Repo/AuthRepo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pregTrackSys.Repositories.Base;
+using Repo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo
+{
+    public interface IAuthRepo
+    {
+        Task<BearAccount> GetAccount(string email, string password);
+    }
+
+    public class AuthRepo : GenericRepository<BearAccount>, IAuthRepo
+    {
+        public AuthRepo() { }
+
+        public async Task<BearAccount> GetAccount(string email, string password)
+        {
+            var account = await _context
+                .BearAccount
+                .FirstOrDefaultAsync(acc => acc.Email == email && acc.Password == password);
+            return account;
+        }
+    }
+}
