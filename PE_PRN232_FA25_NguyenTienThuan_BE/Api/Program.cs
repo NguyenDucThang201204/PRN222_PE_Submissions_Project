6c559f57fb16b9902ea7d8307e8eb1804f464bc5
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:33 +0700

    Upload PE_PRN232_FA25_NguyenTienThuan_BE/Api/Program.cs

---
+++ PE_PRN232_FA25_NguyenTienThuan_BE/Api/Program.cs
@@ -0,0 +1,47 @@
+////using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Repository.Repos;
+using Service;
+using System.Text;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<JwtUtil>();
+builder.Services.AddScoped<BearAccountRepo>();
+builder.Services.AddScoped<BearAccountService>();
+
+builder.Services.AddScoped<BearProfileRepo>();
+builder.Services.AddScoped<BearProfileService>();
+
+builder.Services.AddScoped<BearTypeRepo>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
