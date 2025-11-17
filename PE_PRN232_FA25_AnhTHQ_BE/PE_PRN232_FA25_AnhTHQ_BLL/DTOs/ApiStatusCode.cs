bb4c24b9f71224b7de94204b8eeb9468ed7e5958
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:01:20 +0700

    Upload PE_PRN232_FA25_AnhTHQ_BE/PE_PRN232_FA25_AnhTHQ_BLL/DTOs/ApiStatusCode.cs

---
+++ PE_PRN232_FA25_AnhTHQ_BE/PE_PRN232_FA25_AnhTHQ_BLL/DTOs/ApiStatusCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_AnhTHQ_BLL.DTOs
+{
+    public enum ApiStatusCode
+    {
+        HB40001, // Missing/invalid input
+        HB40101, // Token missing/invalid
+        HB40301, // Permission denied
+        HB40401, // Resource not found
+        HB50001  // Internal server error
+    }
+}
