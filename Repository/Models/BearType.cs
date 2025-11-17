f87415165ffddfd54ca886bfdf4866f08620f918
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:11 +0700

    Upload Repository/Models/BearType.cs

---
+++ Repository/Models/BearType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repository.Models;
+
+public partial class BearType
+{
+    public int BearTypeId { get; set; }
+
+    public string BearTypeName { get; set; } = null!;
+
+    public string? Origin { get; set; }
+
+    public int? Description { get; set; }
+
+    public virtual ICollection<BearProfile> BearProfiles { get; set; } = new List<BearProfile>();
+}
