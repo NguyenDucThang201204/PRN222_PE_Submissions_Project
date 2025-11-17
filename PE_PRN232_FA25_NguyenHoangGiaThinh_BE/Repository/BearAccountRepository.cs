728c6578bad8a85ec6786f359dc4333865552226
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:10 +0700

    Upload PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Repository/BearAccountRepository.cs

---
+++ PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Repository/BearAccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using BO;
+using DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class BearAccountRepository : IBearAccountRepository
+    {
+        private readonly BearAccountDAO _account;
+
+        public BearAccountRepository (BearAccountDAO account)
+        {
+            _account = account;
+        }
+        public BearAccount GetBearAccount(string email, string password) => _account.GetBearAccount(email, password);
+        
+    }
+}
