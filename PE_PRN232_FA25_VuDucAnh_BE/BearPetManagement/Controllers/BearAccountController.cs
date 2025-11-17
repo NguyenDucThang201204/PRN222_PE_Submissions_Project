e607c9f14f382ce3a1348e905b3728b129e53607
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:01:55 +0700

    Upload PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement/Controllers/BearAccountController.cs

---
+++ PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement/Controllers/BearAccountController.cs
@@ -0,0 +1,53 @@
+﻿using BearPetManagement_Repositories.DTOs;
+using BearPetManagement_Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BearPetManagement.Controllers
+{
+    [ApiController]
+    [Route("Accounts")]
+    public class BearAccountController : Controller
+    {
+        private readonly BearAccountService _bearAccountService;
+
+        public BearAccountController(BearAccountService bearAccountService)
+        {
+            _bearAccountService = bearAccountService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                var error = ApiError._errors[400];
+                return BadRequest(new ApiError(error.ErrorCode, "Missing/invalid input"));
+            }
+
+            try
+            {
+                var account = await _bearAccountService.GetAccountAsync(loginDTO.userName, loginDTO.Password);
+                if (account == null)
+                {
+                    var error = ApiError._errors[404];
+                    return NotFound(new ApiError(error.ErrorCode, "Account not found"));
+                }
+
+                var token = await _bearAccountService.GenerateTokenAsync(account);
+
+                var response = new AuthenticationResponse
+                {
+                    Token = token,
+                    Account = account
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                var error = ApiError._errors[500];
+                return StatusCode(500, new ApiError(error.ErrorCode, "Internal server error"));
+            }
+        }
+    }
+}
