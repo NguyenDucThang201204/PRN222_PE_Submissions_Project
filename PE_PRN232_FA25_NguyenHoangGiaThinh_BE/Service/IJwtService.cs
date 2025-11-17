971afabc1926cb9aa5f090c8063577926bcdd563
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:21 +0700

    Upload PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Service/IJwtService.cs

---
+++ PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Service/IJwtService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IJwtService
+    {
+        string GenerateToken(string serId, string email, IList<string> roles = null);
+        ClaimsPrincipal ValidateToken(string token);
+        
+    }
+}
