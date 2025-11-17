4234ebbfae394e14ceee9207aa9660adf496d8eb
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:00 +0700

    Upload Repository/UnitOfWork.cs

---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Repository.Models;
+
+namespace Repository
+{
+    public class UnitOfWork
+    {
+        private readonly FA25BearDBContext _dbContext;
+        public UnitOfWork(FA25BearDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public GenericRepository<T> GetRepository<T>() where T : class
+        {
+            return new GenericRepository<T>(_dbContext);
+        }
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+        public async Task SaveAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
