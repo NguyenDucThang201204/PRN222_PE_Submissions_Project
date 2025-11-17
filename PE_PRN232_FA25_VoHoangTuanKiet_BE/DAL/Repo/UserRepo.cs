2240c57404acad6cb8ddef0c379d3af63672eae0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:17:17 +0700

    Upload PE_PRN232_FA25_VoHoangTuanKiet_BE/DAL/Repo/UserRepo.cs

---
+++ PE_PRN232_FA25_VoHoangTuanKiet_BE/DAL/Repo/UserRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repo
+{
+    public class UserRepo
+    {
+        private readonly Fa25bearDbContext _context;
+
+        public UserRepo(Fa25bearDbContext context)
+        {
+            _context = context;
+        }
+
+        public BearAccount? Login(string email)
+        {
+            return _context.BearAccounts.FirstOrDefault(user => user.Email == email);
+        }
+    }
+}
