078795e1e8b4dc91b7048c66fe5ea25b48351c38
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:14:53 +0700

    Upload PE_PRN232_FA25_PhamTrungHieu_BE.BAL/Services/IBearAccountService.cs

---
+++ PE_PRN232_FA25_PhamTrungHieu_BE.BAL/Services/IBearAccountService.cs
@@ -0,0 +1,9 @@
+﻿using PE_PRN232_FA25_PhamTrungHieu_BE.DAL.ModelExtensions;
+
+namespace PE_PRN232_FA25_PhamTrungHieu_BE.BAL.Services
+{
+    public interface IBearAccountService
+    {
+        Task<LoginResponse> LoginAsync(LoginRequest loginRequest);
+    }
+}
