cf842a6bfbac89d31e741d2f0712910724321362
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:38:30 +0700

    Upload Repositories/ModelExtensions/UpdateRequest.cs

---
+++ Repositories/ModelExtensions/UpdateRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Repositories.ModelExtensions
+{
+    public class UpdateRequest
+    {
+        [Required(ErrorMessage = "BearName is required")]
+        public string BearName { get; set; }
+
+        [Required(ErrorMessage = "BearWeight is required")]
+        [Range(15, double.MaxValue, ErrorMessage = "BearWeight must be greater than 15")]
+        public double BearWeight { get; set; }
+
+        [Required(ErrorMessage = "Length is required")]
+        public string Characteristics { get; set; }
+
+        [Required(ErrorMessage = "CareNeeds is required")]
+        public string CareNeeds { get; set; }
+
+        [Required(ErrorMessage = "ModifiedDate is required")]
+        public DateTime ModifiedDate { get; set; }
+
+        [Required(ErrorMessage = "BearTypeId is required")]
+        public int BearTypeId { get; set; }
+    }
+}
