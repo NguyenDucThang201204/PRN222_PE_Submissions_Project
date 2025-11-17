5bf1b118fd668f7b2d6268c2958c3f79b8e4e10c
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:22:28 +0700

    Upload PE_PRN232_FA25_NguyenQuangMinh_BE/PE_PRN232_FA25_NguyenQuangMinh_BE.Service/BearTypeService.cs

---
+++ PE_PRN232_FA25_NguyenQuangMinh_BE/PE_PRN232_FA25_NguyenQuangMinh_BE.Service/BearTypeService.cs
@@ -0,0 +1,20 @@
+﻿using PE_PRN232_FA25_NguyenQuangMinh_BE.Repo;
+using PE_PRN232_FA25_NguyenQuangMinh_BE.Repo.Basic;
+using PE_PRN232_FA25_NguyenQuangMinh_BE.Repo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_NguyenQuangMinh_BE.Service
+{
+    public class BearTypeService
+    {
+        private readonly GenericRepository<BearType> _repository;
+
+        public BearTypeService() => _repository = new GenericRepository<BearType>();
+
+        public async Task<List<BearType>> GetAllAsync() => await _repository.GetAllAsync();
+    }
+}
