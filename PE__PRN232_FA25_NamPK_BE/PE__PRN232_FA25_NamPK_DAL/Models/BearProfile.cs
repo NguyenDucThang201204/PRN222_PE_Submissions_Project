0566fbddaa90f6e741ac18db1ce1cafb4d80ba2e
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:56 +0700

    Upload PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_DAL/Models/BearProfile.cs

---
+++ PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_DAL/Models/BearProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PE__PRN232_FA25_NamPK_DAL.Models;
+
+public partial class BearProfile
+{
+    public int BearProfileId { get; set; }
+
+    public int? BearTypeId { get; set; }
+
+    public string BearName { get; set; } = null!;
+
+    public decimal? BearWeight { get; set; }
+
+    public string? Characteristics { get; set; }
+
+    public int? CareNeeds { get; set; }
+
+    public DateOnly? ModifiedDate { get; set; }
+
+    public virtual BearType? BearType { get; set; }
+}
