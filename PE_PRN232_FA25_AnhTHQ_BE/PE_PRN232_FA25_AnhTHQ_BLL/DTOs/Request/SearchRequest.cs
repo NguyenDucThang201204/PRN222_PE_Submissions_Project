e27a82e9278b66292f98227a5bb0963400b3e834
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:01:31 +0700

    Upload PE_PRN232_FA25_AnhTHQ_BE/PE_PRN232_FA25_AnhTHQ_BLL/DTOs/Request/SearchRequest.cs

---
+++ PE_PRN232_FA25_AnhTHQ_BE/PE_PRN232_FA25_AnhTHQ_BLL/DTOs/Request/SearchRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_AnhTHQ_BLL.DTOs.Request
+{
+    public class SearchRequest
+    {
+        public int currentPage { get; set; } = 1;
+        public int pageSize { get; set; } = 10;
+        public String bearName { get; set; } = "";
+        public int bearWeight { get; set; } = 0;
+        public String bearTypeName { get; set; } = "";
+    }
+}
