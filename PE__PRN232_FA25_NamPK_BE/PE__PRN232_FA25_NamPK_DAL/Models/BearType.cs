da42c9d5aecf6458e18929cabfb4d1342cd17759
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:57 +0700

    Upload PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_DAL/Models/BearType.cs

---
+++ PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_DAL/Models/BearType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PE__PRN232_FA25_NamPK_DAL.Models;
+
+public partial class BearType
+{
+    public int BearTypeId { get; set; }
+
+    public string BearTypeName { get; set; } = null!;
+
+    public string? Origin { get; set; }
+
+    public string? Description { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<BearProfile> BearProfiles { get; set; } = new List<BearProfile>();
+}
