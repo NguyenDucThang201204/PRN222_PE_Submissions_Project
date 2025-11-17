bc3694ee3d99b8ebb7ac24783ad1940bf5f5d206
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:25 +0700

    Upload PE_PRN232_FA25_Hungnt_BE/Repositories/DBContext/Fa25bearDbContext.cs

---
+++ PE_PRN232_FA25_Hungnt_BE/Repositories/DBContext/Fa25bearDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Repositories.DBContext;
+
+public partial class Fa25bearDbContext : DbContext
+{
+    public Fa25bearDbContext()
+    {
+    }
+
+    public Fa25bearDbContext(DbContextOptions<Fa25bearDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<BearAccount> BearAccounts { get; set; }
+
+    public static string GetConnectionString(string connectionStringName)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string connectionString = config.GetConnectionString(connectionStringName);
+        return connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(GetConnectionString("DefaultConnection")).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BearAccount>(entity =>
+        {
+            entity.HasKey(e => e.AccountId).HasName("PK__BearAcco__349DA58637EF4385");
+
+            entity.ToTable("BearAccount");
+
+            entity.HasIndex(e => e.Email, "UQ__BearAcco__A9D10534D917BDDF").IsUnique();
+
+            entity.Property(e => e.AccountId)
+                .ValueGeneratedNever()
+                .HasColumnName("AccountID");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(100);
+            entity.Property(e => e.Phone).HasMaxLength(100);
+            entity.Property(e => e.UserName).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
