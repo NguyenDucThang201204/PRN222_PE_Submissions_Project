edffcdd6204d2be5eb4080e7a8de99ecedc7fef0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:39:25 +0700

    Upload Services/BearAccountService.cs

---
+++ Services/BearAccountService.cs
@@ -0,0 +1,32 @@
+﻿using Repositories;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class BearAccountService
+    {
+        private readonly BearAccountRepository _repository;
+        public BearAccountService()
+        {
+            _repository = new BearAccountRepository();
+        }
+
+        public async Task<BearAccount> GetUserAccount(string email, string password)
+        {
+            try
+            {
+                return await _repository.GetUserAccount(email, password);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+    }
+}
