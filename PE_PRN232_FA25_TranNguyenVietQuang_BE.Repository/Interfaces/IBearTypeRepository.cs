4a836efbb4b47cab7c6672463181b34107f0134a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:39:02 +0700

    Upload PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository/Interfaces/IBearTypeRepository.cs

---
+++ PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository/Interfaces/IBearTypeRepository.cs
@@ -0,0 +1,10 @@
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository.Entities;
+
+namespace PE_PRN232_FA25_TranNguyenVietQuang_BE_Repository.Interfaces;
+
+public interface IBearTypeRepository : PE_PRN232_FA25_TranNguyenVietQuang_BE_Repository.IBaseRepository<BearType>
+{
+}
