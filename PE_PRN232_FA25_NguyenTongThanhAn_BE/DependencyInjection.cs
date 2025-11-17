c874f036b7582fe51627ccab63cd3e92ee87ffc2
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:02:10 +0700

    Upload PE_PRN232_FA25_NguyenTongThanhAn_BE/DependencyInjection.cs

---
+++ PE_PRN232_FA25_NguyenTongThanhAn_BE/DependencyInjection.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.OData;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Service;
+using Service.Configurations;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace PE_PRN232_FA25_NguyenTongThanhAn_BE
+{
+    public static class DependencyInjection
+    {
+        public static void AddApiServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add services to the container.
+            services.AddControllers()
+                .AddOData(options =>
+                {
+                    options
+                    .Select()
+                    .Filter()
+                    .OrderBy()
+                    .Expand()
+                    .SetMaxTop(100)
+                    .Count()
+                    .AddRouteComponents("odata", Configurations.ODataConfig.GetEDModel())
+                    ;
+                })
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                    options.JsonSerializerOptions.WriteIndented = true;
+                })
+                ;
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Please Enter Token: ",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        []
+                    }
+                });
+            });
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    RequireAudience = true,
+                    RequireSignedTokens = true,
+                    RequireExpirationTime = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!)) ?? throw new ArgumentException("Jwt:Key not found")
+                };
+            });
+
+        }
+    }
+}
