08fab7216755284868efe71689ad068f94daf51a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:47:18 +0700

    Upload Services/AccountService.cs

---
+++ Services/AccountService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOs.Models;
+using Repository;
+
+namespace Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _accountRepository;
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        public async Task<BearAccount> Login(string email, string password)
+        {
+            return await _accountRepository.Login(email, password);
+        }
+    }
+}
