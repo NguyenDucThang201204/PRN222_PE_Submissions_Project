7a3e48d67fafdae01f2a1b1a63b5061b38880fb5
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:35:34 +0700

    Upload Application/EndPoints/EndPoints.cs

---
+++ Application/EndPoints/EndPoints.cs
@@ -0,0 +1,21 @@
+﻿namespace Application.EndPoints
+{
+    public static class EndPoints
+    {
+        public static class BearProfile
+        {
+            public const string Base = "/api/BearProfile";
+            public const string GetAll = Base;
+            public const string GetById = Base + "/{id}";
+            public const string Create = Base;
+            public const string Update = Base + "/{id}";
+            public const string Delete = Base + "/{id}";
+            public const string Search = Base + "/search"; 
+        }
+
+        public static class Auth
+        {
+            public const string Login = "/Accounts/login";
+        }
+    }
+}
