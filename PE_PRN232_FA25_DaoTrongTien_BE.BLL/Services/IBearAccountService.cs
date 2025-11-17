91afb2e6e71e3b51b8a1c00149d33b72820a8cac
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:46:07 +0700

    Upload PE_PRN232_FA25_DaoTrongTien_BE.BLL/Services/IBearAccountService.cs

---
+++ PE_PRN232_FA25_DaoTrongTien_BE.BLL/Services/IBearAccountService.cs
@@ -0,0 +1,15 @@
+﻿using PE_PRN232_FA25_DaoTrongTien_BE.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_DaoTrongTien_BE.BLL.Services
+{
+    public interface IBearAccountService
+    {
+        Task<string> GetBearAccount(string email, string password);
+        string GenerateJSONWebToken(BearAccount bearAccount);
+    }
+}
