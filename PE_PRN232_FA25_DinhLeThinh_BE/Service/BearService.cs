d480437d37f51a543f501c8f3c85a982fe20a5e6
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:41:12 +0700

    Upload PE_PRN232_FA25_DinhLeThinh_BE/Service/BearService.cs

---
+++ PE_PRN232_FA25_DinhLeThinh_BE/Service/BearService.cs
@@ -0,0 +1,30 @@
+﻿using Model.DTOs;
+using Repository;
+
+namespace Service
+{
+    public class BearService : IBearService
+    {
+        private readonly IBearRepo _bearRepo;
+
+        public BearService(IBearRepo bear)
+        {
+            _bearRepo = bear;
+        }
+
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            return await _bearRepo.DeleteAsync(id); ;
+        }
+
+        public async Task<List<BearDto>> GetAllBear()
+        {
+            return await _bearRepo.GetAllAsync();
+        }
+
+        public async Task<BearDto?> GetByIdAsync(int id)
+        {
+            return await _bearRepo.GetByIdAsync(id);
+        }
+    }
+}
