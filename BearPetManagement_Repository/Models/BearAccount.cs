aa91fdcf88cf95119f766a1a0ee766b4f505887a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:09:10 +0700

    Upload BearPetManagement_Repository/Models/BearAccount.cs

---
+++ BearPetManagement_Repository/Models/BearAccount.cs
@@ -0,0 +1,13 @@
+﻿namespace BearPetManagement_Repository.Models
+{
+    public class BearAccount
+    {
+        public int AccountID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int RoleId { get; set; }
+    }
+}
