9266434fc824911ce4467f690d179ca5d7ad3b8f
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:44:17 +0700

    Upload FA25Bear.DataAccess/IBearAccountRepo.cs

---
+++ FA25Bear.DataAccess/IBearAccountRepo.cs
@@ -0,0 +1,10 @@
+﻿using FA25Bear.DataAccess.Models;
+
+namespace FA25Bear.DataAccess
+{
+    public interface IBearAccountRepo
+    {
+        BearAccount? GetSystemAccountByEmaild(string email, string password);
+
+    }
+}
