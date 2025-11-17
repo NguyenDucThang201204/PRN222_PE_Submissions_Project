487fa22dc577aae5e0e631395eb661f96c91f725
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:54 +0700

    Upload BearPetManagement.Services/BearTypeService.cs

---
+++ BearPetManagement.Services/BearTypeService.cs
@@ -0,0 +1,22 @@
+﻿using BearPetManagement.Repositories;
+using BearPetManagement.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement.Services
+{
+    public class BearTypeService
+    {
+        private readonly BearTypeRepository _Repository;
+        public BearTypeService() => _Repository = new BearTypeRepository();
+        public BearTypeService(BearTypeRepository repository) => _Repository = repository;
+
+        public async Task<List<BearType>> GetAllBearTypesAsync()
+        {
+            return await _Repository.GetAllAsync();
+        }
+    }
+}
