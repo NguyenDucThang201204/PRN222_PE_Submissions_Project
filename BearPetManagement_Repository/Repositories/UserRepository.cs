22733eb1c7b554e49c4bb56be3d13e940253d1b1
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:09:14 +0700

    Upload BearPetManagement_Repository/Repositories/UserRepository.cs

---
+++ BearPetManagement_Repository/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using BearPetManagement_Repository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BearPetManagement_Repository.Repositories
+{
+    public class UserRepository : GenericRepository<BearAccount>
+    {
+
+        public async Task<BearAccount> GetUserByEmailPassword(string username, string password)
+        {
+            return await _context.BearAccounts.FirstOrDefaultAsync(u => u.UserName == username && u.Password == password);
+        }
+
+        public async Task<List<int>> GetAllRoleId()
+        {
+            return await _context.BearAccounts.Select(u => u.RoleId).ToListAsync();
+        }
+
+    }
+}
