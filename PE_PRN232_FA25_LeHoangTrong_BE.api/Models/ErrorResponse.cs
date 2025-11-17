e6ce7974d915ff50f9cf9f3952af0c27d326d090
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:47:45 +0700

    Upload PE_PRN232_FA25_LeHoangTrong_BE.api/Models/ErrorResponse.cs

---
+++ PE_PRN232_FA25_LeHoangTrong_BE.api/Models/ErrorResponse.cs
@@ -0,0 +1,7 @@
+namespace PE_PRN232_FA25_LeHoangTrong_BE_api.Models;
+
+public sealed class ErrorResponse
+{
+    public string ErrorCode { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}
