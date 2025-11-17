a65aee50cf73cbdcb83ff80ce7da324da9fd9f09
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:46:58 +0700

    Upload PE_PRN232_FA25_TranSonTrieu_BE/DAO/DTO/LoginResponse.cs

---
+++ PE_PRN232_FA25_TranSonTrieu_BE/DAO/DTO/LoginResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.DTO
+{
+    public class LoginResponse
+    {
+        public string token { get; set; }
+        public string Role { get; set; }
+    }
+}
