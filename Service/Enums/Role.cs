5ef4711be2ff6b4e23081a1e3d6425dc3fe1f084
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:02:48 +0700

    Upload Service/Enums/Role.cs

---
+++ Service/Enums/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Enums
+{
+    public enum Role
+    {
+        Admin = 1,
+        Manager = 2,
+        Staff = 3,
+        Member = 4,
+    }
+}
