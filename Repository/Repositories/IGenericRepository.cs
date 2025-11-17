1f8905ca6b474efb4f5d35645a00fed5c31af82a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:40:22 +0700

    Upload Repository/Repositories/IGenericRepository.cs

---
+++ Repository/Repositories/IGenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using Repository.Extras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories;
+public interface IGenericRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(object id);
+    Task<List<TEntity>> GetAllAsync();
+    Task AddAsync(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    Task SaveAsync();
+
+    Task<List<TEntity>> GetAsync(
+        Expression<Func<TEntity, bool>>? filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        string? includeProperties = null,
+        int? skip = null,
+        int? take = null);
+
+    Task<PagedResult<TEntity>> GetPagedAsync(
+        int pageNumber,
+        int pageSize,
+        Expression<Func<TEntity, bool>>? filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        string? includeProperties = null);
+}
