d26ebe5c1a7a48b42be49cb728cd11d0495ea87a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:17:32 +0700

    Upload BOs/BearAccount.cs

---
+++ BOs/BearAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BOs;
+
+public partial class BearAccount
+{
+    public string AccountId { get; set; } = null!;
+
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? FullName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? RoleId { get; set; }
+}
