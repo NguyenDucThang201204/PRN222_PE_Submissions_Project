76d990083c3c626859b94934cd925e842371cb3a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:47:17 +0700

    Upload Repository/ProfileRepository.cs

---
+++ Repository/ProfileRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOs.Models;
+using DAOs;
+
+namespace Repository
+{
+    public class ProfileRepository : IProfileRepository
+    {
+        public async Task<BearProfile> AddProfile(BearProfile profile)
+        {
+            return await ProfileDAO.Instance.AddProfile(profile);
+        }
+
+        public async Task<BearProfile> DeleteProfile(int id)
+        {
+            return await ProfileDAO.Instance.DeleteProfile(id);
+        }
+
+        public async Task<BearProfile> GetProfile(int id)
+        {
+            return await ProfileDAO.Instance.GetProfileById(id);
+        }
+
+        public async Task<List<BearProfile>> GetProfiles()
+        {
+            return await ProfileDAO.Instance.GetProfile();
+        }
+
+        public async Task<List<BearType>> GetTypes()
+        {
+            return await ProfileDAO.Instance.GetTypes();
+        }
+
+        public async Task<BearProfile> UpdateProfile(BearProfile profile)
+        {
+            return await ProfileDAO.Instance.UpdateProfile(profile);
+        }
+        public async Task<List<BearProfile>> SearchProfile(string keyword)
+        {
+            return await ProfileDAO.Instance.SearchProfile(keyword);
+        }
+    }
+}
