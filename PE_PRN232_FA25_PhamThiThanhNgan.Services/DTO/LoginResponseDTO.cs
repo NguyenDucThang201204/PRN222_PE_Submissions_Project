6eaa2cf0a64748d60eb6c4565bff7f9afd8651ac
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:26 +0700

    Upload PE_PRN232_FA25_PhamThiThanhNgan.Services/DTO/LoginResponseDTO.cs

---
+++ PE_PRN232_FA25_PhamThiThanhNgan.Services/DTO/LoginResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_FA25_PE_Services.DTO
+{
+    public class LoginResponseDTO
+    {
+        public string Token { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
