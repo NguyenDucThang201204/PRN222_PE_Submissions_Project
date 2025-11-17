09702ccd6a052e52c789ee0a7e7ddf5b3c3631e5
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:44:40 +0700

    Upload FA25Bear.DataAccess/Models/DTOs/ObjectResponse.cs

---
+++ FA25Bear.DataAccess/Models/DTOs/ObjectResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace FA25Bear.DataAccess.Models.DTOs
+{
+    public class ObjectResponse<T>
+    {
+        public string ErrorCode { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+        public ObjectResponse(string errorCode, string message, T? data)
+        {
+            ErrorCode = "200";
+            Message = "success";
+            Data = data;
+        }
+    }
+}
