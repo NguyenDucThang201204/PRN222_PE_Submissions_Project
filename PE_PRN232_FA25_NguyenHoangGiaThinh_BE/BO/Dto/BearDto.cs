5f6bd3047f5bfdcec35775c985229e6cca10fc6a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:28 +0700

    Upload PE_PRN232_FA25_NguyenHoangGiaThinh_BE/BO/Dto/BearDto.cs

---
+++ PE_PRN232_FA25_NguyenHoangGiaThinh_BE/BO/Dto/BearDto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO.Dto
+{
+    public class BearDto
+    {
+
+        public int? BearTypeId { get; set; }
+
+        public string BearName { get; set; } = null!;
+
+        public double BeartWeight { get; set; }
+
+        public string Characteristics { get; set; } = null!;
+
+        public string CareNeeds { get; set; } = null!;
+
+        public DateTime ModifiedDate { get; set; }
+
+    }
+
+    public class BearDtoResponse
+    {
+
+        public int? BearTypeId { get; set; }
+
+        public string BearName { get; set; } = null!;
+
+        public double BeartWeight { get; set; }
+
+        public string Characteristics { get; set; } = null!;
+
+        public string CareNeeds { get; set; } = null!;
+
+        public DateTime ModifiedDate { get; set; }
+
+
+        public string BearTypeName { get; set; } = string.Empty;
+    }
+}
