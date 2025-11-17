e428f4d58c4fa01617506ebbac31e890f552fdb6
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:39:37 +0700

    Upload Services/DTOs/BearProfileUpdateRequest.cs

---
+++ Services/DTOs/BearProfileUpdateRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTOs
+{
+    public class BearProfileUpdateRequest
+    {
+        [Required]
+        public int BearProfileId { get; set; }
+        [Required]
+        public int BearTypeId { get; set; }
+        [Required]
+        [RegularExpression(@"^([A-Z0-9][a-zA-Z0-9#]*\s)*([A-Z0-9][a-zA-Z0-9#]*)$")]
+        [MinLength(4)]
+        [MaxLength(50)]
+        [DefaultValue("default value")]
+        public string BearName { get; set; }
+        [Required]
+        [Range(201, double.MaxValue)]
+        public double BearWeight { get; set; }
+        [Required]
+        public string Characteristics { get; set; }
+        [Required]
+        public string CareNeeds { get; set; }
+        [Required]
+        public DateTime ModifiedDate { get; set; }
+    }
+}
