a1d74277cb01def5a2818680cb9ace0fffb81a74
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:07 +0700

    Upload Repository/DTO/LoginResponse.cs

---
+++ Repository/DTO/LoginResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DTO
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
