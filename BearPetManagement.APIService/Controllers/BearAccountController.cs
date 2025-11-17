b6919cd22148e6e5bf2240a21c02a6e46146ec88
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:56 +0700

    Upload BearPetManagement.APIService/Controllers/BearAccountController.cs

---
+++ BearPetManagement.APIService/Controllers/BearAccountController.cs
@@ -0,0 +1,61 @@
+﻿using BearPetManagement.Repositories.Models;
+using BearPetManagement.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BearPetManagement.APIService.Controllers
+{
+    [Route("api/Accounts")]
+    [ApiController]
+    public class BearAccountController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly BearAccountService _accountService;
+
+        public BearAccountController(IConfiguration config, BearAccountService AccountService)
+        {
+            _config = config;
+            _accountService = AccountService;
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            var user = _accountService.GetUserAccount(request.UserName, request.Password);
+
+            if (user == null || user.Result == null)
+                return Unauthorized();
+
+            var token = GenerateJSONWebToken(user.Result);
+
+            return Ok(token);
+        }
+
+        private string GenerateJSONWebToken(BearAccount UserAccount)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"]
+                    , _config["Jwt:Audience"]
+                    , new Claim[]
+                    {
+                new(ClaimTypes.Name, UserAccount.UserName),
+                new(ClaimTypes.Role, UserAccount.RoleId.ToString()),
+                    },
+                    expires: DateTime.Now.AddMinutes(120),
+                    signingCredentials: credentials
+                );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenString;
+        }
+
+        public sealed record LoginRequest(string UserName, string Password);
+
+    }
+}
