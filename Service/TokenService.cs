fbcf6b8aeca509726341c728decf663e141c221b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:02:30 +0700

    Upload Service/TokenService.cs

---
+++ Service/TokenService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Repository.Models;
+using Service.Configurations;
+using Service.Enums;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Service
+{
+    public class TokenService
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public TokenService(IOptions<JwtSettings> jwtOptions)
+        {
+            _jwtSettings = jwtOptions.Value;
+        }
+
+        public string GenerateAccessToken(BearAccount account)
+        {
+            // Convert int Role -> Enum name -> string
+            var roleName = Enum.GetName(typeof(Role), account.RoleId) ?? "Unknown";
+
+            var claims = new[]
+            {
+                new Claim("id", account.AccountId.ToString()),
+                new Claim(ClaimTypes.Name, account.FullName),
+                new Claim(ClaimTypes.Role, roleName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.AccessTokenExpireInMinutes),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        
+    }
+}
