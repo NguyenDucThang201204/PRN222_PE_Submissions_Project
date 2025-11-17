53db7ae18eec4af6e09af7ababad2a19d70b6f8f
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:09:16 +0700

    Upload BearPetManagement_Service/Services/UserService.cs

---
+++ BearPetManagement_Service/Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using BearPetManagement_Repository.Models;
+using BearPetManagement_Repository.Repositories;
+
+namespace BearPetManagement_Service.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _repo;
+        public UserService(UserRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<BearAccount> GetUser(string userName, string password)
+        {
+            return await _repo.GetUserByEmailPassword(userName, password);
+        }
+    }
+}
