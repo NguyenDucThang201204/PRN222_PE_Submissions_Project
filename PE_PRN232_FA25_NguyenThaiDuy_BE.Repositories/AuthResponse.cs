4e91ad0ce35a0a59c6d1f8b0dd19d2e54cced8b8
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:55 +0700

    Upload PE_PRN232_FA25_NguyenThaiDuy_BE.Repositories/AuthResponse.cs

---
+++ PE_PRN232_FA25_NguyenThaiDuy_BE.Repositories/AuthResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_NguyenThaiDuy_BE.Repositories
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
