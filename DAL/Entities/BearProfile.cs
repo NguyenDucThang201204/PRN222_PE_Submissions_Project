3817657d8cf55965a6b5d22686a591352627ef7f
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:00:56 +0700

    Upload DAL/Entities/BearProfile.cs

---
+++ DAL/Entities/BearProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Entities;
+
+public partial class BearProfile
+{
+    public int BearProfileId { get; set; }
+
+    public int? BearTypeId { get; set; }
+
+    public string? BearName { get; set; }
+
+    public string? BearWeight { get; set; }
+
+    public int? Characteristics { get; set; }
+
+    public double? CareNeeds { get; set; }
+
+    public DateTime? ModifiedDate { get; set; }
+
+    public virtual BearType? BearType { get; set; }
+}
