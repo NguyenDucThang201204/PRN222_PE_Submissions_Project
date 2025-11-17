e9427f0793aacd5a4313de6934065b717fa19af9
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:41:46 +0700

    Upload Repo/DTO/AuthRequest.cs

---
+++ Repo/DTO/AuthRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo.DTO
+{
+    public class AuthRequest
+    {
+        
+            [Required(ErrorMessage = "Username is required")]
+            public string Username { get; set; }
+
+            [Required(ErrorMessage = "Password is required")]
+            public string Password { get; set; }
+        
+    }
+}
