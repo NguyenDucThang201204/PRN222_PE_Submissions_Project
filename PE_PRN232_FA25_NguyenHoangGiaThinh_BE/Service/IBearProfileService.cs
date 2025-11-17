494aa8667c7ce1271b5ebda81b5d9a2d2d759e30
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:20 +0700

    Upload PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Service/IBearProfileService.cs

---
+++ PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Service/IBearProfileService.cs
@@ -0,0 +1,20 @@
+﻿using BO;
+using BO.Dto;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IBearProfileService
+    {
+        public BearProfile GetBearProfileById(int id);
+        public List<BearProfile> GetBearList();
+        public BearProfile CreateBear(BearDto bear);
+        public void UpdateBear(int id, BearDto bear);
+        public void DeleteBear(int id);
+    }
+}
