7467e12dc79cf14c5ada0a64b5b076bc270ae020
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:35:30 +0700

    Upload Application/Program.cs

---
+++ Application/Program.cs
@@ -0,0 +1,45 @@
+﻿using Business.Interfaces;
+using Business.Services;
+using Data;
+using Data.Interfaces;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register DbContext
+builder.Services.AddDbContext<FA25BearDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings")));
+
+// Register layers
+builder.Services.AddScoped<IBearProfileRepository, BearProfileRepository>();
+builder.Services.AddScoped<IBearProfileService, BearProfileService>();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
