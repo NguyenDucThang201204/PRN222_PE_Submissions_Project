678a017a3c357acf8257d904b2a396489ddcf939
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:40:23 +0700

    Upload Service/DTO/BearDto.cs

---
+++ Service/DTO/BearDto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DTO;
+public class CreateRequest
+{
+    [Required]
+    public int BearTypeId { get; set; }
+
+    [Required]
+    public string BearName { get; set; }
+
+    [Required]
+    [Range(200, double.MaxValue, ErrorMessage = "Weignt must be greater than 200.")]
+    public decimal BearWeight { get; set; }
+
+    [Required]
+    public string Characteristics { get; set; }
+
+    [Required]
+    public string CareNeeds { get; set; }
+}
+
+public class UpdateRequest
+{
+    [Required]
+    public int Id { get; set; }
+    public int BearTypeId { get; set; }
+
+    public string BearName { get; set; }
+
+    public decimal BearWeight { get; set; }
+
+    public string Characteristics { get; set; }
+
+    public string CareNeeds { get; set; }
+}
+
+public class GetResponse
+{
+    public int BearProfileId { get; set; }
+
+    public int BearTypeId { get; set; }
+
+    public string BearTypeName { get; set; }
+
+    public string BearName { get; set; }
+
+    public decimal BearWeight { get; set; }
+
+    public string Characteristics { get; set; }
+
+    public string CareNeeds { get; set; }
+
+}
+
+
+
