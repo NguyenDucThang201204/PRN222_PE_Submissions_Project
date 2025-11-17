cd160fe60ea4ab027105c61339e23cc76ed5f372
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:47:26 +0700

    Upload PE_PRN232_FA25_NguyenThiMaiTrinh_BE/Controllers/AccountController.cs

---
+++ PE_PRN232_FA25_NguyenThiMaiTrinh_BE/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Services;
+
+namespace PE_PRN232_FA25_NguyenThiMaiTrinh_BE.Controllers
+{
+    // [Route("api/[controller]")]
+    [Route("api/auth")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO loginDTO)
+        {
+            var account = await _accountService.Login(loginDTO.Email, loginDTO.Password);
+            if (account == null)
+            {
+                // return Unauthorized("Invalid email or password.");
+                return Unauthorized();
+            }
+            //Generate JWT Token
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true).Build();
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, account.Email),
+                new Claim(ClaimTypes.Role, account.RoleId.ToString()),
+                new Claim("AccountId", account.AccountId.ToString()),
+
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var preparedToken = new JwtSecurityToken(
+                issuer: configuration["JWT:Issuer"],
+                audience: configuration["JWT:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+            var token = new JwtSecurityTokenHandler().WriteToken(preparedToken);
+            var role = account.RoleId.ToString(); //0:Admin 1:Staff 2:Manager
+            var accountId = account.AccountId.ToString();
+            return Ok(new LoginResponseDTO
+            {
+                Role = role,
+                Token = token,
+                // AccountId = accountId
+            });
+        }
+    }
+}
