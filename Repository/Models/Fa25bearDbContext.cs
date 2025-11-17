ed0ff958919b04647e42f3150ae2ba885f679d0a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:10:25 +0700

    Upload Repository/Models/Fa25bearDbContext.cs

---
+++ Repository/Models/Fa25bearDbContext.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace Repository.Models;
+
+public partial class Fa25bearDbContext : DbContext
+{
+    public Fa25bearDbContext()
+    {
+    }
+
+    public Fa25bearDbContext(DbContextOptions<Fa25bearDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<BearAccount> BearAccounts { get; set; }
+
+    public virtual DbSet<BearProfile> BearProfiles { get; set; }
+
+    public virtual DbSet<BearType> BearTypes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(GetConnectionString());
+    private string GetConnectionString()
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true).Build();
+        return configuration["ConnectionStrings:DefaultConnectionString"];
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BearAccount>(entity =>
+        {
+            entity.HasKey(e => e.AccountId);
+
+            entity.ToTable("BearAccount");
+
+            entity.Property(e => e.AccountId).HasColumnName("AccountID");
+            entity.Property(e => e.Email).HasMaxLength(150);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(100);
+            entity.Property(e => e.Phone).HasMaxLength(50);
+            entity.Property(e => e.UserName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<BearProfile>(entity =>
+        {
+            entity.HasKey(e => e.BearProfileId).HasName("PK_LeopardProfile");
+
+            entity.ToTable("BearProfile");
+
+            entity.Property(e => e.BearName).HasMaxLength(150);
+            entity.Property(e => e.CareNeeds).HasMaxLength(1500);
+            entity.Property(e => e.Characteristics).HasMaxLength(2000);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.BearType).WithMany(p => p.BearProfiles)
+                .HasForeignKey(d => d.BearTypeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_BearProfile_BearType");
+        });
+
+        modelBuilder.Entity<BearType>(entity =>
+        {
+            entity.ToTable("BearType");
+
+            entity.Property(e => e.BearTypeName).HasMaxLength(250);
+            entity.Property(e => e.Description).HasMaxLength(1000);
+            entity.Property(e => e.Origin).HasMaxLength(250);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
