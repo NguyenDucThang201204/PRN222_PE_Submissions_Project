a6290e823f31ea06cec883129aeedd0f8eb22900
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:02:26 +0700

    Upload Service/DependencyInjection.cs

---
+++ Service/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Service.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public static class DependencyInjection
+    {
+        public static void AddBusinessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<JwtSettings>(
+            configuration.GetSection("Jwt"));
+
+            services.AddScoped<TokenService>();
+            services.AddScoped<BearAccountService>();
+            services.AddScoped<BearProfileService>();
+            //services.AddScoped<>();
+        }
+    }
+}
