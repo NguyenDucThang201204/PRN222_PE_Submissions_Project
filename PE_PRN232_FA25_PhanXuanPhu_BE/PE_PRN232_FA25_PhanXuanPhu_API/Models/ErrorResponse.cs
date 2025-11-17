534dfa68d029fa7b9c469562503afaa3f6a6b362
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:38:01 +0700

    Upload PE_PRN232_FA25_PhanXuanPhu_BE/PE_PRN232_FA25_PhanXuanPhu_API/Models/ErrorResponse.cs

---
+++ PE_PRN232_FA25_PhanXuanPhu_BE/PE_PRN232_FA25_PhanXuanPhu_API/Models/ErrorResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace PE_PRN232_FA25_PhanXuanPhu_API.Models
+{
+    public class ErrorResponse
+    {
+        public string ErrorCode { get; set; }
+        public string Message { get; set; }
+
+        public ErrorResponse(string code, string message)
+        {
+            ErrorCode = code;
+            Message = message;
+        }
+    }
+}
