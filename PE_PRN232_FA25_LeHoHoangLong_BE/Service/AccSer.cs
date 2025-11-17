98b1f8b0760fa46a34af3863786f21451ac57406
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:18:05 +0700

    Upload PE_PRN232_FA25_LeHoHoangLong_BE/Service/AccSer.cs

---
+++ PE_PRN232_FA25_LeHoHoangLong_BE/Service/AccSer.cs
@@ -0,0 +1,33 @@
+﻿using Repository;
+using Repository.DTOs;
+
+namespace Service
+{
+    public class AccSer : IAccSer
+    {
+        private readonly IAccRepo _accRepo;
+        private readonly IJWTSer _jwtSer;
+        public AccSer(IAccRepo accRepo, IJWTSer jwtSer)
+        {
+            _accRepo = accRepo;
+            _jwtSer = jwtSer;
+        }
+        public async Task<LoginResponse?> LoginAsync(LoginRequest re)
+        {
+            var account = await _accRepo.LoginAsync(re.UserName, re.Password);
+            if (account == null)
+            {
+                return null;
+            }
+
+            string token = _jwtSer.GenerateToken(account.UserName, account.Email, account.RoleId);
+
+            return new LoginResponse
+            {
+                Token = token,
+                Role = account.RoleId,
+            };
+        }
+
+    }
+}
