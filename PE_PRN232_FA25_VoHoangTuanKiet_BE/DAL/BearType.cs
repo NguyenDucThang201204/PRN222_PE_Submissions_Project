71dc34784bb2a84837cb5ead4fa16eb1f92a9846
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:17:06 +0700

    Upload PE_PRN232_FA25_VoHoangTuanKiet_BE/DAL/BearType.cs

---
+++ PE_PRN232_FA25_VoHoangTuanKiet_BE/DAL/BearType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace DAL;
+
+public partial class BearType
+{
+    public int BearTypeId { get; set; }
+
+    public string BearTypeName { get; set; } = null!;
+
+    public string? Origin { get; set; }
+
+    public string? Description { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<BearProfile> BearProfiles { get; set; } = new List<BearProfile>();
+}
