33955e0c0c8d4a30a92eded6231b18b2593ec8bf
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:01:52 +0700

    Upload PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Services/BearAccountService.cs

---
+++ PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Services/BearAccountService.cs
@@ -0,0 +1,34 @@
+﻿using BearPetManagement_Repositories;
+using BearPetManagement_Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement_Services
+{
+    public class BearAccountService
+    {
+        private readonly BearAccountRepository _bearAccountRepository;
+        private readonly JwtService _jwtService;
+
+        public BearAccountService(BearAccountRepository bearAccountRepository, JwtService jwtService)
+        {
+            _bearAccountRepository = bearAccountRepository;
+            _jwtService = jwtService;
+        }
+
+        public async Task<BearAccount> GetAccountAsync(string email, string password)
+        {
+            var account = await _bearAccountRepository.GetAccountAsync(email, password);
+            if (account == null) return null;
+            return account;
+        }
+
+        public async Task<string> GenerateTokenAsync(BearAccount account)
+        {
+            return await Task.Run(() => _jwtService.GenerateToken(account));
+        }
+    }
+}
