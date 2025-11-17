a8b332c23b5b5077906c6ad2ffeeb77605fd88fb
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:18:06 +0700

    Upload PE_PRN232_FA25_LeHoHoangLong_BE/Service/IAccSer.cs

---
+++ PE_PRN232_FA25_LeHoHoangLong_BE/Service/IAccSer.cs
@@ -0,0 +1,14 @@
+﻿using Repository.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IAccSer
+    {
+        Task<LoginResponse?> LoginAsync(LoginRequest re);
+    }
+}
