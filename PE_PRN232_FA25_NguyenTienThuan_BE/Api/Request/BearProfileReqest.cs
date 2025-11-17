3a01f48873e3a631900876ddee7124982f35ac17
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:40 +0700

    Upload PE_PRN232_FA25_NguyenTienThuan_BE/Api/Request/BearProfileReqest.cs

---
+++ PE_PRN232_FA25_NguyenTienThuan_BE/Api/Request/BearProfileReqest.cs
@@ -0,0 +1,17 @@
+﻿namespace Api.Request
+{
+    public class BearProfileReqest
+    {
+        public int BearProfileId { get; set; }
+
+        public int? BearTypeId { get; set; }
+
+        public string BearName { get; set; } = null!;
+
+        public int? BearWeight { get; set; }
+
+        public string? Characteristics { get; set; }
+
+        public string? CareNeeds { get; set; }
+    }
+}
