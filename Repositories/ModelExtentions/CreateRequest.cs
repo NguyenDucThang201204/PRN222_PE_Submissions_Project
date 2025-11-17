211dc7db623c552637745273c8841bc73c90e41c
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:35:18 +0700

    Upload Repositories/ModelExtentions/CreateRequest.cs

---
+++ Repositories/ModelExtentions/CreateRequest.cs
@@ -0,0 +1,27 @@
+﻿using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.ModelExtentions
+{
+    public class CreateRequest
+    {
+        public int LeopardProfileId { get; set; }
+
+        public int LeopardTypeId { get; set; }
+        public string LeopardName { get; set; }
+        [Range(15.01, double.MaxValue, ErrorMessage = "Weight must be greater than 15")]
+        public double Weight { get; set; }
+
+        public string Characteristics { get; set; }
+
+        public string CareNeeds { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}
