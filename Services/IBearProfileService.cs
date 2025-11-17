235ecd54f1a755f072043809b7add7e4f43af22b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:15:14 +0700

    Upload Services/IBearProfileService.cs

---
+++ Services/IBearProfileService.cs
@@ -0,0 +1,26 @@
+﻿using Repositories.ModelExtensions;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IBearProfileService
+    {
+        Task<List<BearProfile>> GetAll();
+
+        Task<BearProfile?> GetById(int id);
+
+        Task<int> Create(BearProfile create);
+
+        Task<int> Update(BearProfile update);
+
+        Task<bool> Delete(int id);
+
+        Task<PaginationResult<List<BearProfile>>> SearchWithPagingAsync(string bearName, int bearWeight, string bearTypeName, int currentPage, int pageSize);
+
+    }
+}
