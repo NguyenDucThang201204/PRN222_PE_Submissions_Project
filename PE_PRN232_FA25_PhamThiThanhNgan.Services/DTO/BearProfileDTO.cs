1094b81c4f7ac937f68501f89f0c9108ec6137d0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:23 +0700

    Upload PE_PRN232_FA25_PhamThiThanhNgan.Services/DTO/BearProfileDTO.cs

---
+++ PE_PRN232_FA25_PhamThiThanhNgan.Services/DTO/BearProfileDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_FA25_PE_Services.DTO
+{
+    public class BearProfileDTO
+    {
+        public int BearProfileId { get; set; }
+        public int BearTypeId { get; set; }
+        public string BearName { get; set; } = null!;
+        public double Weight { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string? BearTypeName { get; set; }
+        public string Characteristics { get; set; } = null!;
+        public string CareNeeds { get; set; } = null!;
+    }
+}
