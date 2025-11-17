756849db00dcd83c034404918d835d00c7226c0b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:40:13 +0700

    Upload API/Controllers/AccountController.cs

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.DTO;
+using Service.Services;
+
+namespace API.Controllers;
+[Route("Accounts")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+
+    private readonly AuthService _authService;
+    public AccountController(AuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    {
+        try
+        {
+            var response = await _authService.Login(request.UserName, request.Password);
+            return Ok(response);
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            return Unauthorized(new { message = ex.Message });
+        }
+    }
+}
