783c7b4b39f0141bff19a5e19682d2dc7c477928
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:47:42 +0700

    Upload PE_PRN232_FA25_LeHoangTrong_BE.api/DTOs/BearProfileDto.cs

---
+++ PE_PRN232_FA25_LeHoangTrong_BE.api/DTOs/BearProfileDto.cs
@@ -0,0 +1,12 @@
+namespace PE_PRN232_FA25_LeHoangTrong_BE_api.DTOs;
+
+public class BearProfileDto
+{
+    public int BearProfileId { get; set; }
+    public int? BearTypeId { get; set; }
+    public string? BearName { get; set; }
+    public decimal? BearWeight { get; set; }
+    public string? Characteristics { get; set; }
+    public string? CareNeeds { get; set; }
+    public string? ModifiedDate { get; set; }
+}
