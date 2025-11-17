5dd275c486cc384f8307f2c6bda8e4938716ef1e
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:55 +0700

    Upload PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_DAL/Models/BearAccount.cs

---
+++ PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_DAL/Models/BearAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PE__PRN232_FA25_NamPK_DAL.Models;
+
+public partial class BearAccount
+{
+    public int AccountId { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int? Role { get; set; }
+
+    public bool? IsActive { get; set; }
+}
