4cc665ab043b71763f18bc9d48ce0a3acf6d1e9b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:25 +0700

    Upload PE_PRN232_FA25_PhamThiThanhNgan.Services/DTO/LoginRequestDTO.cs

---
+++ PE_PRN232_FA25_PhamThiThanhNgan.Services/DTO/LoginRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_FA25_PE_Services.DTO
+{
+    public class LoginRequestDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
