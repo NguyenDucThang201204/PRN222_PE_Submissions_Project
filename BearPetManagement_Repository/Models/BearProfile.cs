455a964f4f6ae07b906a4f00a201fce8db7b1c7f
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:09:10 +0700

    Upload BearPetManagement_Repository/Models/BearProfile.cs

---
+++ BearPetManagement_Repository/Models/BearProfile.cs
@@ -0,0 +1,17 @@
+﻿using BearPetManagement_Repository.Models;
+
+namespace BearPetManagement_Repository.NewFolder
+{
+    public class BearProfile
+    {
+        public int BearProfileId { get; set; }
+        public int BearTypeId { get; set; }
+        public string BearName { get; set; }
+
+        public int BearWeight { get; set; }
+        public string Characteristics { get; set; }
+        public string CareNeeds { get; set; }
+        public DateOnly? ModifiedDate { get; set; }
+        public virtual BearType BearType { get; set; }
+    }
+}
