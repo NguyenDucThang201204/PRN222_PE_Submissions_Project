4d2c3edfc780f8766e40c6e8937ba3a80f1e0bc3
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:57 +0700

    Upload PRN232_FA25_NguyenMinhKhoiNguyen_SE170356/PRN232_SU25_NguyenNMK_DataAccessLayer/Repositories/IUnitOfWork.cs

---
+++ PRN232_FA25_NguyenMinhKhoiNguyen_SE170356/PRN232_SU25_NguyenNMK_DataAccessLayer/Repositories/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using PRN232_SU25_NguyenNMK_DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_SU25_NguyenNMK_DataAccessLayer.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<BearProfile> BearProfileRepository { get; }
+        IRepository<BearType> BearTypeRepository { get; }
+        IRepository<BearAccount> BearAccountRepository { get; }
+        Task<int> SaveChangesAsync();
+    }
+}
