71b8b0d622a454217f151e3f4652f7d1028b16b2
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:41:15 +0700

    Upload PE_PRN232_FA25_DinhLeThinh_BE/Service/IBearService.cs

---
+++ PE_PRN232_FA25_DinhLeThinh_BE/Service/IBearService.cs
@@ -0,0 +1,13 @@
+﻿using Model.DTOs;
+
+namespace Service
+{
+    public interface IBearService
+    {
+        Task<List<BearDto>> GetAllBear();
+
+        Task<BearDto?> GetByIdAsync(int id);
+
+        Task<bool> DeleteByIdAsync(int id);
+    }
+}
