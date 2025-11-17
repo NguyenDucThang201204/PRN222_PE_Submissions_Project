30e0f4c8e7e971def6d1c7d2792dd8910c9a67fa
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:14:56 +0700

    Upload PE_PRN232_FA25_PhamTrungHieu_BE.DAL/ModelExtensions/LoginDTOs.cs

---
+++ PE_PRN232_FA25_PhamTrungHieu_BE.DAL/ModelExtensions/LoginDTOs.cs
@@ -0,0 +1,9 @@
+﻿namespace PE_PRN232_FA25_PhamTrungHieu_BE.DAL.ModelExtensions
+{
+    public record LoginRequest(string Email, string Password);
+    public class LoginResponse
+    {
+        public string Token {  get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;    
+    }
+}
