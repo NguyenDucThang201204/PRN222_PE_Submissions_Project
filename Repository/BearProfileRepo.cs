6a06c7ed3bca6efc820ea9fdd2213be862c9c8dd
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:20:53 +0700

    Upload Repository/BearProfileRepo.cs

---
+++ Repository/BearProfileRepo.cs
@@ -0,0 +1,24 @@
+﻿using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class BearProfileRepo : IBearProfileRepo
+    {
+        private readonly Fa25bearDbContext _context;
+        public BearProfileRepo(Fa25bearDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<BearProfile>> GetAllAsync()
+        {
+            return await _context.BearProfiles.Include(h => h.BearType).ToListAsync();
+        }
+    }
+}
