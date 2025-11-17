fe40eb689d48994ae743622c337a6483aacd4fd5
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:50 +0700

    Upload Repository/Repository/Interfaces/IBearAccountRepository.cs

---
+++ Repository/Repository/Interfaces/IBearAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository.Interfaces
+{
+    public interface IBearAccountRepository
+    {
+        Task<BearAccount> GetByEmailAsync(string email);
+    }
+}
