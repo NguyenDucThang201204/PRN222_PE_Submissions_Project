2088fc2023592d3e010a081700f3348e3c7aa640
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:14:57 +0700

    Upload PE_PRN232_FA25_PhamTrungHieu_BE.DAL/ModelExtensions/PagingResponse.cs

---
+++ PE_PRN232_FA25_PhamTrungHieu_BE.DAL/ModelExtensions/PagingResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace PE_PRN232_FA25_PhamTrungHieu_BE.DAL.ModelExtensions
+{
+    public record PagingResponse<T>(IEnumerable<T>? Items, int TotalCount, int PageNumber, int PageSize) where T : class;  
+}
