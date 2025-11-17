f1f8a82c3acc0948e45474a6c226d3995c1ea77f
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:44:38 +0700

    Upload FA25Bear.DataAccess/Models/DTOs/BearProfileDTO.cs

---
+++ FA25Bear.DataAccess/Models/DTOs/BearProfileDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace FA25Bear.DataAccess.Models.DTOs
+{
+    public class BearProfileDTO
+    {
+        public int BearProfileId { get; set; }
+
+        public int? BearTypeId { get; set; }
+
+        public string BearName { get; set; } = null!;
+
+        public int? BearWeight { get; set; }
+
+        public string? Characteristics { get; set; }
+
+        public string? CareNeeds { get; set; }
+
+        public DateOnly? ModifiedDate { get; set; }
+
+    }
+}
