1a2c41900db30c1cd2f21f7f6762af05d81ff2f3
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:35:46 +0700

    Upload Data/Entities/BearProfile.cs

---
+++ Data/Entities/BearProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace Data.Entities
+{
+    public class BearProfile
+    {
+        public int BearProfileId { get; set; }
+        public int BearTypeId { get; set; }
+        public string BearName { get; set; }
+        public int BearWeight { get; set; }
+        public string? Characteristics { get; set; }
+        public string? CareNeeds { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
