7d1d42888d60fe46861b9859eee2314017b1ce1e
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:48 +0700

    Upload PRN232_FA25_NguyenMinhKhoiNguyen_SE170356/PRN232_SU25_NguyenNMK_DataAccessLayer/DTOs/BearDTO.cs

---
+++ PRN232_FA25_NguyenMinhKhoiNguyen_SE170356/PRN232_SU25_NguyenNMK_DataAccessLayer/DTOs/BearDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_SU25_NguyenNMK_DataAccessLayer.DTOs
+{
+    public class BearDTO
+    {
+        [Key]
+        public int? BearProfileId { get; set; }
+
+        public int? BearTypeId { get; set; }
+
+        public string BearName { get; set; }
+
+        public double BearWeight { get; set; }
+
+        public string Characteristics { get; set; }
+
+        public string CareNeeds { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+        public string BearTypeName { get; set; }
+
+        public string Origin { get; set; }
+
+        public string Description { get; set; }
+    }
+}
