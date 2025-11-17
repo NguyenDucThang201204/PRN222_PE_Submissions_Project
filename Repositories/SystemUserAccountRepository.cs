7396fa50e53d6451070f8a9684f215a8f5132ed7
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:38:21 +0700

    Upload Repositories/SystemUserAccountRepository.cs

---
+++ Repositories/SystemUserAccountRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Basic;
+using Repositories.Models;
+
+namespace Repositories
+
+{
+    public class SystemUserAccountRepository : GenericRepository<BearAccount>
+    {
+        public SystemUserAccountRepository() => _context ??= new FA25BearDBContext();
+
+        public SystemUserAccountRepository(FA25BearDBContext context) => _context = context;
+
+        public async Task<BearAccount> GetUserAccountAsync(string userName, string password)
+        {
+            //return await _context.SystemUserAccounts.FirstOrDefaultAsync(u => u.UserName == userName && u.Password == password && u.IsActive == true);
+
+            return await _context.BearAccounts.FirstOrDefaultAsync(u => u.Email == userName && u.Password == password);
+            //return await _context.SystemUserAccounts.FirstOrDefaultAsync(u => u.Phone == userName && u.Password == password && u.IsActive == true);
+            //return await _context.SystemUserAccounts.FirstOrDefaultAsync(u => u.EmployeeCode == userName && u.Password == password && u.IsActive == true);
+            //return await _context.SystemUserAccounts.FirstOrDefaultAsync(u => u.UserName == userName && u.Password == password);
+        }
+    }
+}
