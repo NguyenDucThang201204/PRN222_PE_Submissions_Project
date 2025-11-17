994fdc51fc8bd0cd79cf1d03dfc5589dc977a4c0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:17:33 +0700

    Upload BOs/BearType.cs

---
+++ BOs/BearType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BOs;
+
+public partial class BearType
+{
+    public string BearTypeId { get; set; } = null!;
+
+    public string? BearTypeName { get; set; }
+
+    public string? Origin { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<BearProfile> BearProfiles { get; set; } = new List<BearProfile>();
+}
