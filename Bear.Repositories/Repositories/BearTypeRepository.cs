00de40b704b62ddad35a6b2c83cff78928ea81f3
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:16 +0700

    Upload Bear.Repositories/Repositories/BearTypeRepository.cs

---
+++ Bear.Repositories/Repositories/BearTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using Bear.Repositories.Basic;
+using Bear.Repositories.Interface;
+using Bear.Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bear.Repositories.Repositories
+{
+    public class BearTypeRepository : GenericRepository<BearType>, IBearTypeRepository
+    {
+        public async Task<BearType> GetItemAsyncById(int? id)
+        {
+            return await _context.BearTypes.FirstOrDefaultAsync(b => b.BearTypeId == id);
+        }
+    }
+}
