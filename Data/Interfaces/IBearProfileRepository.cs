6739a5a6d0a5f911ef221daeb847e09b96468739
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:35:46 +0700

    Upload Data/Interfaces/IBearProfileRepository.cs

---
+++ Data/Interfaces/IBearProfileRepository.cs
@@ -0,0 +1,15 @@
+﻿using Data.Entities;
+using System.Collections.Generic;
+
+namespace Data.Interfaces
+{
+    public interface IBearProfileRepository
+    {
+        IEnumerable<BearProfile> GetAll();
+        BearProfile? GetById(int id);
+        void Add(BearProfile bear);
+        void Update(BearProfile bear);
+        void Delete(BearProfile bear);
+        void Save();
+    }
+}
