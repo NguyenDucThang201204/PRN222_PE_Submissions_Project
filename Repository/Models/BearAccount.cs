be22e365e2562a47c2ab69e4b2f0cf224d78f60e
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:40:17 +0700

    Upload Repository/Models/BearAccount.cs

---
+++ Repository/Models/BearAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repository.Models;
+
+public partial class BearAccount
+{
+    public int AccountId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public int RoleId { get; set; }
+}
