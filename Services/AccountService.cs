df15f4f65a7d809e9cca4bf7533c3606224b1ae0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:37:30 +0700

    Upload Services/AccountService.cs

---
+++ Services/AccountService.cs
@@ -0,0 +1,34 @@
+﻿using Repositories.Basic;
+using Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services
+{
+    public interface IAccountService
+    {
+        Task<BearAccount?> GetAccount(string email, string password);
+    }
+
+    public class AccountService : IAccountService
+    {
+        private readonly GenericRepository<BearAccount> _repository;
+        public AccountService(GenericRepository<BearAccount> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<BearAccount?> GetAccount(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+
+            var userAccount = await _repository
+                .GetSet()
+                .FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+
+            return userAccount;
+        }
+    }
+}
