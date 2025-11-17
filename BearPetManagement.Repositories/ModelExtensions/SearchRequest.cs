50ca758186276c1714e3de0bf3db186462dd3b6d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:22:02 +0700

    Upload BearPetManagement.Repositories/ModelExtensions/SearchRequest.cs

---
+++ BearPetManagement.Repositories/ModelExtensions/SearchRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement.Repositories.ModelExtensions
+{
+    public class SearchRequest
+    {
+        public int? CurrentPage { get; set; }
+        public int? PageSize { get; set; }
+
+    }
+
+    public class SearchTestResult : SearchRequest
+    {
+        public string? Result { get; set; }
+        public decimal? ConfidenceLevel { get; set; }
+        public string? FullName { get; set; }
+
+    }
+}
