92703b0315eb7c1b8a662fc69157d4ff85294e62
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:30 +0700

    Upload PE_PRN232_FA25_PhamThiThanhNgan.Services/Interface/IBearProfileService.cs

---
+++ PE_PRN232_FA25_PhamThiThanhNgan.Services/Interface/IBearProfileService.cs
@@ -0,0 +1,17 @@
+﻿using PE_PRN232_FA25_PhamThiThanhNgan.Repositories.ModelExtensions;
+using PE_PRN232_FA25_PhamThiThanhNgan.Repositories.Models;
+using Practice_FA25_PE_Services.DTO;
+
+namespace Practice_FA25_PE_Services.Interface
+{
+    public interface IBearProfileService
+    {
+        Task<IEnumerable<BearProfileDTO>> GetAllProfilesAsync();
+        Task<BearProfileDTO?> GetProfileByIdAsync(int id);
+        IQueryable<BearProfileDTO> GetQueryableProfiles();
+        Task<BearProfileDTO> CreateProfileAsync(BearProfileRequestDTO profileDto);
+        Task<BearProfileDTO?> UpdateProfileAsync(int id, BearProfileRequestDTO profileDto);
+        Task<bool> DeleteProfileAsync(int id);
+        Task<PaginationResult<List<BearProfile>>> SearchWithPagingAsync(BearSearchRequest searchrequest);
+    }
+}
