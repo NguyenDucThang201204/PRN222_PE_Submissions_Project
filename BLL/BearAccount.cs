ab4f21b8fc1b483583b0055ff62e550aa59c44f6
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:12:15 +0700

    Upload BLL/BearAccount.cs

---
+++ BLL/BearAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BO;
+
+public partial class BearAccount
+{
+    public int AccountId { get; set; }
+
+    public string? Username { get; set; }
+
+    public string Password { get; set; } = null!;
+
+    public string? FullName { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string? PhoneNumber { get; set; }
+
+    public int Role { get; set; }
+}
