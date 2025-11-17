aced2a73069fe577982a80aeaf68b75c9208ee80
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:47:20 +0700

    Upload Services/IProfileService.cs

---
+++ Services/IProfileService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOs.Models;
+
+namespace Services
+{
+    public interface IProfileService
+    {
+        Task<List<BearProfile>> GetProfiles();
+        Task<BearProfile> GetProfile(int id);
+        Task<BearProfile> AddProfile(BearProfile profile);
+        Task<BearProfile> UpdateProfile(BearProfile profile);
+        Task<BearProfile> DeleteProfile(int id);
+        Task<List<BearType>> GetTypes();
+
+        Task<List<BearProfile>> SearchProfile(string keyword);
+
+    }
+}
