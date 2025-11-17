ca10afa5ff136b510b36b408aed434211b814288
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:25 +0700

    Upload PE_PRN232_FA25_Hungnt_BE/Repositories/Models/BearAccount.cs

---
+++ PE_PRN232_FA25_Hungnt_BE/Repositories/Models/BearAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repositories.Models;
+
+public partial class BearAccount
+{
+    public int AccountId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string? FullName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public int? RoleId { get; set; }
+}
