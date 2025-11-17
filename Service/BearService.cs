31243289f26384bf2db6508457172ca372d48710
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:20:57 +0700

    Upload Service/BearService.cs

---
+++ Service/BearService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository;
+using Repository.DTO;
+using Repository.Models;
+
+namespace Service
+{
+    public class BearService
+    {
+        private readonly IBearProfileRepo _repository;
+        public BearService(IBearProfileRepo repository)
+        {
+            _repository = repository;
+        }
+        public Task<IEnumerable<BearProfile>> GetAllAsync() => _repository.GetAllAsync();
+    }
+}
