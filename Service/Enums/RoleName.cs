7cd082d51ff98d305e597c71b9fd3f0e0e2c39d0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:02:48 +0700

    Upload Service/Enums/RoleName.cs

---
+++ Service/Enums/RoleName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Enums
+{
+    public static class RoleName
+    {
+        public const string Admin = "Admin";
+        public const string Manager = "Manager";
+        public const string Staff = "Staff";
+        public const string Member = "Member";
+
+        public const string AllRoles = $"{Manager},{Staff},{Member}";
+        public const string StaffRoles = $"{Admin},{Manager}";
+    }
+
+}
