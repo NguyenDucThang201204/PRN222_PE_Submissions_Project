b28d0efa410634b54bf3de635e441806b629cc7e
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:01:33 +0700

    Upload PE_PRN232_FA25_AnhTHQ_BE/PE_PRN232_FA25_AnhTHQ_BLL/DTOs/Response/LoginResponse.cs

---
+++ PE_PRN232_FA25_AnhTHQ_BE/PE_PRN232_FA25_AnhTHQ_BLL/DTOs/Response/LoginResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_AnhTHQ_BLL.DTOs.Response
+{
+    public class LoginResponse
+    {
+        public String token { get; set; }
+        public int role { get; set; }
+    }
+}
