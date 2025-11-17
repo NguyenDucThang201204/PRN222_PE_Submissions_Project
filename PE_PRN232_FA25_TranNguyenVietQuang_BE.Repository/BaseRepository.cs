a99cb972ba4149ce3f96ea6c056008c2e3bd1571
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:38:42 +0700

    Upload PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository/BaseRepository.cs

---
+++ PE_PRN232_FA25_TranNguyenVietQuang_BE.Repository/BaseRepository.cs
@@ -0,0 +1,60 @@
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PE_PRN232_FA25_TranNguyenVietQuang_BE_Repository;
+
+public abstract class BaseRepository<TEntity, TContext> : IBaseRepository<TEntity>
+    where TEntity : class
+    where TContext : DbContext
+{
+    private readonly IDbFactory<TContext> _dbFactory;
+    private DbSet<TEntity>? _dbSet;
+
+    protected BaseRepository(IDbFactory<TContext> dbFactory)
+    {
+        _dbFactory = dbFactory;
+    }
+
+    protected TContext DbContext => _dbFactory.DbContext;
+
+    protected DbSet<TEntity> DbSet => _dbSet ??= DbContext.Set<TEntity>();
+
+    public virtual DbSet<TEntity> Set() => DbSet;
+
+    public virtual async Task<TEntity?> GetByKeysAsync(object[] keys)
+        => await DbSet.FindAsync(keys);
+
+    public virtual async Task<List<TEntity>> GetAllAsync()
+        => await DbSet.ToListAsync();
+
+    public virtual async Task AddAsync(TEntity entity)
+    {
+        await DbSet.AddAsync(entity);
+        await DbContext.SaveChangesAsync();
+    }
+
+    public virtual async Task AddRangeAsync(IEnumerable<TEntity> entities)
+    {
+        await DbSet.AddRangeAsync(entities);
+        await DbContext.SaveChangesAsync();
+    }
+
+    public virtual async Task UpdateAsync(TEntity entity)
+    {
+        DbSet.Update(entity);
+        await DbContext.SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteAsync(TEntity entity)
+    {
+        DbSet.Remove(entity);
+        await DbContext.SaveChangesAsync();
+    }
+
+    public virtual IQueryable<TEntity> Query(Expression<Func<TEntity, bool>>? predicate = null)
+        => predicate is null ? DbSet : DbSet.Where(predicate);
+}
