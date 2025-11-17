752d9dcdcf33dd64fe9bd7349b0c339e2f61abba
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:45 +0700

    Upload PE_PRN232_FA25_TangPhat_BE/BusinessObjects/DTOs/ErrorCode.cs

---
+++ PE_PRN232_FA25_TangPhat_BE/BusinessObjects/DTOs/ErrorCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.DTOs
+{
+    public enum ErrorCode
+    {
+        HB40001,  //Missinginvalid input
+        HB40101,  //Token missinginvalid
+        HB40301,  //Permission denied
+        HB40401,  //Resource not found
+        HB50001   //Internal server error
+    }
+}
