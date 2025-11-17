12a6b5d711b5a9fef82be07fada87eadae6d85b3
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:14:54 +0700

    Upload PE_PRN232_FA25_PhamTrungHieu_BE.BAL/Services/IBearProfileService.cs

---
+++ PE_PRN232_FA25_PhamTrungHieu_BE.BAL/Services/IBearProfileService.cs
@@ -0,0 +1,14 @@
+﻿using PE_PRN232_FA25_PhamTrungHieu_BE.DAL.ModelExtensions;
+
+namespace PE_PRN232_FA25_PhamTrungHieu_BE.BAL.Services
+{
+    public interface IBearProfileService
+    {
+        Task<GetBearProfileResponse> GetByIdAsync(int id);
+        Task<List<GetBearProfileResponse>> GetAllAsync();
+        Task<string> CreateAsync(CreateBearProfileRequest request);
+        Task<string> UpdateAsync(int id, UpdateBearProfileRequest request);
+        Task<string> DeleteAsync(int id);
+        Task<string> ValidateModelAsync(string bearName, decimal? bearWeight);
+    }
+}
