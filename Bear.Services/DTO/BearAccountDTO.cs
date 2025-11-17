26618cd5a07dbe3d7f39bb621876fab240802bf5
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:17 +0700

    Upload Bear.Services/DTO/BearAccountDTO.cs

---
+++ Bear.Services/DTO/BearAccountDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bear.Services.DTO
+{
+    public class BearAccountDTO
+    {
+        public string token { get; set; }
+        public string role { get; set; }
+    }
+}
