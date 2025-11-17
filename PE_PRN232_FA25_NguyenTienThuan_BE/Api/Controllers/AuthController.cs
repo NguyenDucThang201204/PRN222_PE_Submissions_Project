42d71fd908259ab6c5c3a1428a4061a415163bb0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:37 +0700

    Upload PE_PRN232_FA25_NguyenTienThuan_BE/Api/Controllers/AuthController.cs

---
+++ PE_PRN232_FA25_NguyenTienThuan_BE/Api/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+using Api.Response;
+using Api.Request;
+
+namespace Api.Controllers
+{
+    [Route("Accounts/Login")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private BearAccountService bearAccountService;
+
+        private JwtUtil jwtUtil;
+
+        public AuthController(BearAccountService bearAccountService, JwtUtil jwtUtil)
+        {
+            this.bearAccountService = bearAccountService;
+            this.jwtUtil = jwtUtil;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuthReponse>> Login([FromBody] AuthRequest authRequest)
+        {
+            try
+            {
+                var account = await bearAccountService.GetAccountByEmailAndPassword(authRequest.UserName, authRequest.Password);
+
+                if (account != null)
+                {
+                    string token = jwtUtil.GenerateAccessToken(new JwtPayload(
+                            account.AccountId, account.Email, (int)account.RoleId
+                        ));
+                    var response = new AuthReponse
+                    {
+                        Token = token,
+                        Role = account.RoleId.ToString()
+                    };
+
+                    return Ok(response);
+                }
+                return Unauthorized();
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+    }
+}
