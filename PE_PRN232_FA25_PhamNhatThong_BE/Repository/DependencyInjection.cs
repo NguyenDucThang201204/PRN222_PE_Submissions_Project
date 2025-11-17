61ed579fe0105b816a109cee8fc635b8687d175f
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:08 +0700

    Upload PE_PRN232_FA25_PhamNhatThong_BE/Repository/DependencyInjection.cs

---
+++ PE_PRN232_FA25_PhamNhatThong_BE/Repository/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<Fa25bearDbContext>(options =>
+            options.UseSqlServer(connectionString));
+            services.AddScoped<AccountRepository>();
+            services.AddScoped<BearRepository>();
+            return services;
+        }
+    }
+}
