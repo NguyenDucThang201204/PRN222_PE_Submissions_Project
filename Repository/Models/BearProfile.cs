f5ba034a17d62a2654f2ef4dc7b2fa2f1b7f5bf4
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:07 +0700

    Upload Repository/Models/BearProfile.cs

---
+++ Repository/Models/BearProfile.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Repository.Models
+{
+    public class BearProfile
+    {
+        [Key]
+        public int BearProfileId { get; set; }
+
+        public string? BearName { get; set; } = string.Empty;
+        public double? BearWeight { get; set; }
+        public string? Characteristics { get; set; } = string.Empty;
+        public string? CareNeeds { get; set; } = string.Empty;
+        public DateTime ModifiedDate { get; set; }
+
+        [ForeignKey("BearType")]
+        public int BearTypeId { get; set; }
+        public BearType BearType { get; set; }
+    }
+}
