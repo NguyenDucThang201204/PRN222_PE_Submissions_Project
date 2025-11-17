4efe26336b6dc9289ce8b12b1920d9079f09e988
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:23 +0700

    Upload PE_PRN232_FA25_PhamNhatThong_BE/Service/Dtos/ServiceResult.cs

---
+++ PE_PRN232_FA25_PhamNhatThong_BE/Service/Dtos/ServiceResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Dtos
+{
+    public class ServiceResult<T>
+    {
+        public bool Success { get; set; }
+        public T? Data { get; set; }
+        public string? ErrorCode { get; set; }
+        public string? ErrorMessage { get; set; }
+        public static ServiceResult<T> Ok(T data) => new() { Success = true, Data = data };
+        public static ServiceResult<T> Fail(string code, string message) => new() { Success = false, ErrorCode = code, ErrorMessage = message };
+    }
+}
