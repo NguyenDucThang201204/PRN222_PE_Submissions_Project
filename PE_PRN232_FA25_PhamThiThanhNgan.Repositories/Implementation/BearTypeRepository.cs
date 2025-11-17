8e88828a0824d39d8cb8a3c7445f0ba8036350ad
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:14 +0700

    Upload PE_PRN232_FA25_PhamThiThanhNgan.Repositories/Implementation/BearTypeRepository.cs

---
+++ PE_PRN232_FA25_PhamThiThanhNgan.Repositories/Implementation/BearTypeRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PE_PRN232_FA25_PhamThiThanhNgan.Repositories.Interface;
+using PE_PRN232_FA25_PhamThiThanhNgan.Repositories.Models;
+
+
+
+namespace PE_PRN232_FA25_PhamThiThanhNgan.Repositories.Implementation
+{
+    public class BearTypeRepository : IBearTypeRepository
+    {
+        private readonly Fa25bearDbContext _context;
+
+        public BearTypeRepository(Fa25bearDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<BearType>> GetAllAsync()
+        {
+            return await _context.BearTypes.ToListAsync();
+        }
+        public async Task<BearType?> GetByIdAsync(int id)
+        {
+            return await _context.BearTypes.FindAsync(id);
+        }
+    }
+}
