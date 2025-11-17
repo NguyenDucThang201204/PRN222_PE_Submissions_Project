74804d594f643459ae3682a42dcf0849d54acdaf
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:20 +0700

    Upload PE_PRN232_FA25_PhamNhatThong_BE/Repository/Models/RoleEnum.cs

---
+++ PE_PRN232_FA25_PhamNhatThong_BE/Repository/Models/RoleEnum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Models
+{
+    public enum RoleEnum
+    {
+        Admin = 1,
+        Manager = 2,
+        Staff = 3,
+        Member = 4
+    }
+
+    public static class RoleEnumExtensions
+    {
+        public static string? GetRoleName(this RoleEnum role)
+        {
+            return role switch
+            {
+                RoleEnum.Admin => "admin",
+                RoleEnum.Manager => "manager",
+                RoleEnum.Staff => "staff",
+                RoleEnum.Member => "member",
+                _ => null
+            };
+        }
+    }
+}
