ad28361971ceee1b9cdc9476e965ecf32e9af4c1
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:08:41 +0700

    Upload PE_PRN232_FA25_VuThanhAn_BE/Service/BearAccountService.cs

---
+++ PE_PRN232_FA25_VuThanhAn_BE/Service/BearAccountService.cs
@@ -0,0 +1,29 @@
+﻿using Repository;
+using Repository.Models;
+
+namespace Service
+{
+	public interface IBearAccountService
+	{
+		Task<BearAccount?> GetBearAccountAsync(string userName, string password);
+	}
+	public class BearAccountService : IBearAccountService
+	{
+		private readonly BearAccountRepository _bearAccountRepository;
+		public BearAccountService()
+		{
+			_bearAccountRepository = new BearAccountRepository();
+		}
+
+		public BearAccountService(BearAccountRepository bearAccountRepository)
+		{
+			_bearAccountRepository = bearAccountRepository;
+		}
+
+
+		public async Task<BearAccount?> GetBearAccountAsync(string userName, string password)
+		{
+			return await _bearAccountRepository.GetUserAccountAsync(userName, password);
+		}
+	}
+}
