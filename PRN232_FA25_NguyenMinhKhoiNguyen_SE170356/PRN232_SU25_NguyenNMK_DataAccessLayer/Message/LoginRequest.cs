eec5b9155b95c9a7f57f50b7140ffd563242d1c7
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:50 +0700

    Upload PRN232_FA25_NguyenMinhKhoiNguyen_SE170356/PRN232_SU25_NguyenNMK_DataAccessLayer/Message/LoginRequest.cs

---
+++ PRN232_FA25_NguyenMinhKhoiNguyen_SE170356/PRN232_SU25_NguyenNMK_DataAccessLayer/Message/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_SU25_NguyenNMK_DataAccessLayer.NewFolder
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
