48582c81291d9234e825bd38b8c13002f49b06cc
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:46:43 +0700

    Upload PE_PRN232_FA25_TranSonTrieu_BE/DAO/BearAccount.cs

---
+++ PE_PRN232_FA25_TranSonTrieu_BE/DAO/BearAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAO;
+
+public partial class BearAccount
+{
+    public int AccountId { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? FullName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public int? RoleId { get; set; }
+}
