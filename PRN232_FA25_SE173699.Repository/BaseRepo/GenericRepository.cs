d65e030f90d856429e285e4fbfe5e2ef51e79dbe
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:19 +0700

    Upload PRN232_FA25_SE173699.Repository/BaseRepo/GenericRepository.cs

---
+++ PRN232_FA25_SE173699.Repository/BaseRepo/GenericRepository.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PRN232_FA25_SE173699.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_FA25_SE173699.Repository.BaseRepo
+{
+    public class GenericRepository<T> where T : class
+    {
+        protected FA2025BearDBContext _context;
+
+        public GenericRepository(FA2025BearDBContext context)
+        {
+            _context = context;
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+        public void Create(T entity)
+        {
+            _context.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task<int> CreateAsync(T entity)
+        {
+            _context.Add(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            var tracker = _context.Attach(entity);
+            tracker.State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public async Task<int> UpdateAsync(T entity)
+        {
+            var tracker = _context.Attach(entity);
+            tracker.State = EntityState.Modified;
+            return await _context.SaveChangesAsync();
+        }
+
+        public bool Remove(T entity)
+        {
+            _context.Remove(entity);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public async Task<bool> RemoveAsync(T entity)
+        {
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public T GetById(int id)
+        {
+            var entity = _context.Set<T>().Find(id);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+
+            return entity;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+
+            return entity;
+        }
+
+        public T GetById(string code)
+        {
+            var entity = _context.Set<T>().Find(code);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+
+            return entity;
+        }
+
+        public async Task<T> GetByIdAsync(string code)
+        {
+            var entity = await _context.Set<T>().FindAsync(code);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+
+            return entity;
+        }
+
+        public T GetById(Guid code)
+        {
+            var entity = _context.Set<T>().Find(code);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+
+            return entity;
+        }
+
+        public async Task<T> GetByIdAsync(Guid code)
+        {
+            var entity = await _context.Set<T>().FindAsync(code);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+
+            return entity;
+        }
+    }
+}
