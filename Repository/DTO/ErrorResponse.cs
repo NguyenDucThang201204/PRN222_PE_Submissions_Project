e9cb6765db5bd7fbc21b941a06b1a8154f548074
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:06 +0700

    Upload Repository/DTO/ErrorResponse.cs

---
+++ Repository/DTO/ErrorResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace Repository.DTO
+{
+    public class ErrorResponse
+    {
+        public string ErrorCode { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+
+    public static class ErrorHelper
+    {
+        public static ErrorResponse Create(string errorCode, string message)
+        {
+            return new ErrorResponse
+            {
+                ErrorCode = errorCode,
+                Message = message
+            };
+        }
+    }
+
+
+}   
