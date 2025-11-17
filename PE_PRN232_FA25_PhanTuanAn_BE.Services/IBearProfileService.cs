a4fdce8ff4043a184b5790101f4d08b81cf137ee
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:03:25 +0700

    Upload PE_PRN232_FA25_PhanTuanAn_BE.Services/IBearProfileService.cs

---
+++ PE_PRN232_FA25_PhanTuanAn_BE.Services/IBearProfileService.cs
@@ -0,0 +1,19 @@
+﻿using PE_PRN232_FA25_PhanTuanAn_BE.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_PhanTuanAn_BE.Services
+{
+    public interface IBearProfileService
+    {
+        Task<List<BearProfile>> GetAllAsync();
+        Task<BearProfile> GetByIdAsync(int id);
+
+        Task<int> CreateAsync(BearProfile bear);
+        Task<int> UpdateAsync(BearProfile bear);
+        Task<bool> DeleteAsync(int id);
+    }
+}
