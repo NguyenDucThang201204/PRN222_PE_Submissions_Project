5e28ba500f05a202b59a6de074d031e34e8d5b62
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:13 +0700

    Upload Service/Model/PaginationSearch.cs

---
+++ Service/Model/PaginationSearch.cs
@@ -0,0 +1,11 @@
+﻿namespace Service.Model
+{
+    public class PaginationSearch
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public string BearName { get; set; } = string.Empty;
+        public double BearWeight { get; set; }
+        public int BearTypeName { get; set; }
+    }
+}
