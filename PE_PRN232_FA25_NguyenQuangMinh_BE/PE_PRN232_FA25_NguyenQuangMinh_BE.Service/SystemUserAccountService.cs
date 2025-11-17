80c1566a7400f3f0e76faa049e1620167227ba91
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:22:31 +0700

    Upload PE_PRN232_FA25_NguyenQuangMinh_BE/PE_PRN232_FA25_NguyenQuangMinh_BE.Service/SystemUserAccountService.cs

---
+++ PE_PRN232_FA25_NguyenQuangMinh_BE/PE_PRN232_FA25_NguyenQuangMinh_BE.Service/SystemUserAccountService.cs
@@ -0,0 +1,31 @@
+﻿using PE_PRN232_FA25_NguyenQuangMinh_BE.Repo;
+using PE_PRN232_FA25_NguyenQuangMinh_BE.Repo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_PRN232_FA25_NguyenQuangMinh_BE.Service
+{
+    public class SystemUserAccountService
+    {
+        private readonly SystemUserAccountRepository _repository;
+        public SystemUserAccountService() => _repository = new SystemUserAccountRepository();
+
+        public async Task<BearAccount> GetUserAccount(string username, string password)
+        {
+            try
+            {
+                return await _repository.GetUserAccountAsync(username, password);
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+            return null;
+
+        }
+    }
+}
