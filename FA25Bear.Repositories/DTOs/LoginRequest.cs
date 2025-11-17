d3d42da731f9cb69deccf9de454ba9e551b74035
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:10:02 +0700

    Upload FA25Bear.Repositories/DTOs/LoginRequest.cs

---
+++ FA25Bear.Repositories/DTOs/LoginRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FA25Bear.Repositories.DTOs
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
