43b4088dd2a6421243ebdb11a694464c67278de4
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:44:20 +0700

    Upload FA25Bear.Service/IBearProfileService.cs

---
+++ FA25Bear.Service/IBearProfileService.cs
@@ -0,0 +1,15 @@
+﻿using FA25Bear.DataAccess.Models;
+using FA25Bear.DataAccess.Models.DTOs;
+
+namespace FA25Bear.Service
+{
+    public interface IBearProfileService
+    {
+        List<BearProfile> GetAll();
+        BearProfile GetById(int id);
+        void Create(BearProfileDTO bfdto);
+        void Update(BearProfile bf);
+        void Delete(int id);
+        IQueryable<BearProfile> Search(string? modelName, int? weight);
+    }
+}
