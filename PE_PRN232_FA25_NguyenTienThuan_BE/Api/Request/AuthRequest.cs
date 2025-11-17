c0350f864206df972b69c4a217eeb89b0df4a750
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:45:40 +0700

    Upload PE_PRN232_FA25_NguyenTienThuan_BE/Api/Request/AuthRequest.cs

---
+++ PE_PRN232_FA25_NguyenTienThuan_BE/Api/Request/AuthRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Api.Request
+{
+    public class AuthRequest
+    {
+        [JsonPropertyName("userName")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+    }
+}
