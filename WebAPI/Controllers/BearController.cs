222ce2bb0be06db4ce14fb41061b205328a43550
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:19 +0700

    Upload WebAPI/Controllers/BearController.cs

---
+++ WebAPI/Controllers/BearController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace WebAPI.Controllers
+{
+    public class BearController
+    {
+        //[ApiController]
+        //public class AuthController : ControllerBase
+        //{
+        //    private readonly BearService _service;
+
+        //    public AuthController(BearService bearService)
+        //    {
+        //        _service = bearService;
+        //    }
+
+        //    [HttpGet("BearProfiles")]
+        //    public async Task<IActionResult> Login(LoginRequest request)
+        //    {
+        //        try
+        //        {
+        //            var response = await _service.Authenticate(request);
+        //            if (response == null)
+        //            {
+        //                return Unauthorized();
+        //            }
+        //            return Ok(response);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            throw new Exception(ex.Message);
+        //        }
+        //    }
+        //}
+    }
+}
