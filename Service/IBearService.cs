7d280593af18c4f2c631ce6b1bc90936960fe264
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:20:59 +0700

    Upload Service/IBearService.cs

---
+++ Service/IBearService.cs
@@ -0,0 +1,14 @@
+﻿using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IBearService
+    {
+        Task<IEnumerable<BearProfile>> GetAllAsync();
+    }
+}
