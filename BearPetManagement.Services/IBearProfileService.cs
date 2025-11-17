97e6191d17ea6bf593352e182f3d23c88a7a74c8
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:55 +0700

    Upload BearPetManagement.Services/IBearProfileService.cs

---
+++ BearPetManagement.Services/IBearProfileService.cs
@@ -0,0 +1,19 @@
+﻿using BearPetManagement.Repositories.ModelExtensions;
+using BearPetManagement.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement.Services
+{
+    public interface IBearProfileService
+    {
+        Task<List<BearProfile>> GetAllAsync();
+        Task<BearProfile> GetByIdAsync(int id);
+        Task<int> CreateAsync(BearProfileDTO bearProfile);
+        Task<int> UpdateAsync(BearProfileDTO bearProfile);
+        Task<bool> DeleteAsync(int id);
+    }
+}
