2d116008f26ce1e61eb9578da9722f12e7862c79
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:00:54 +0700

    Upload DAL/DTOs/LoginResponseDTO.cs

---
+++ DAL/DTOs/LoginResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DTOs
+{
+    public class LoginResponseDTO
+    {
+        public string Message { get; set; }
+        public string Token { get; set; }
+
+        public string Role { get; set; }
+    }
+}
