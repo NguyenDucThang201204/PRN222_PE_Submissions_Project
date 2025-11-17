f106c52d8f4da18819de4910cedc4b003bed6092
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:11:10 +0700

    Upload Bear.Repositories/Interface/IBearProfileRepository.cs

---
+++ Bear.Repositories/Interface/IBearProfileRepository.cs
@@ -0,0 +1,18 @@
+﻿using Bear.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bear.Repositories.Interface
+{
+    public interface IBearProfileRepository
+    {
+        Task<int> CreateAsync(BearProfile item);
+        Task<int> UpdateAsync(BearProfile item);
+        Task<bool> RemoveAsync(BearProfile item);
+        Task<BearProfile> GetItemByIdAsync(int id);
+        Task<List<BearProfile>> GetAllItemsAsync();
+    }
+}
