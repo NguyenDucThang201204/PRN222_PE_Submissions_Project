cff18bd4fd71468f464741ab24851952a2e5362a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:08 +0700

    Upload Repository/Models/BearType.cs

---
+++ Repository/Models/BearType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Repository.Models
+{
+    public class BearType
+    {
+        [Key]
+        public int BearTypeId { get; set; }
+        public string BearTypeName { get; set; } = string.Empty;
+        public string Origin { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
