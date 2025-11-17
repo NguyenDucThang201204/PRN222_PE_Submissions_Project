f0f4a9f2236af629bbe47029062c908dcbe61bf0
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:02:28 +0700

    Upload Service/LoginResponse.cs

---
+++ Service/LoginResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; } = default!;
+
+        public string Role { get; set; } = default!;
+    }
+}
