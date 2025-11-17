5c86d0049fcc8be1ba1ef3b81e9c6c2f4170ff5b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:17:45 +0700

    Upload Repository/IJWTRepo.cs

---
+++ Repository/IJWTRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IJwtService
+    {
+        string GenerateToken(string userId, string email, string roleID);
+        ClaimsPrincipal ValidateToken(string token);
+        string GenerateRefreshToken();
+    }
+}
