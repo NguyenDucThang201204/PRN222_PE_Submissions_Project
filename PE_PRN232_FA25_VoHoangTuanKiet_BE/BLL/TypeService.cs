453684a6a93557877de1da459f776797a3da2a32
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:17:03 +0700

    Upload PE_PRN232_FA25_VoHoangTuanKiet_BE/BLL/TypeService.cs

---
+++ PE_PRN232_FA25_VoHoangTuanKiet_BE/BLL/TypeService.cs
@@ -0,0 +1,30 @@
+﻿using DAL;
+using DAL.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class TypeService
+    {
+        private readonly TypeRepo _repo;
+
+        public TypeService(TypeRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public List<BearType> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public BearType? GetById(int id)
+        {
+            return _repo.GetById(id);
+        }
+    }
+}
