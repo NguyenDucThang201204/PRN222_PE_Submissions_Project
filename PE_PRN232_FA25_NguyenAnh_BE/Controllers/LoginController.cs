dae02cf9bd8bbc46226dd2698154182ad8ef36e3
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:01:01 +0700

    Upload PE_PRN232_FA25_NguyenAnh_BE/Controllers/LoginController.cs

---
+++ PE_PRN232_FA25_NguyenAnh_BE/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using BLL.IServices;
+using DAL.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PE_PRN232_FA25_NguyenAnh_BE.Controllers
+{
+    [Route("accounts/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService _service;
+
+        public LoginController(ILoginService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginDTO login)
+        {
+            try
+            {
+                var response = await _service.LoginFunc(login.UserName, login.Password);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(new { error = ex.Message });
+            }
+        }
+
+    }
+}
