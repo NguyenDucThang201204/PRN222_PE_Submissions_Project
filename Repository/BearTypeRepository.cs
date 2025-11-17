f5c1e232ff6ced16294d503f9b1a227152c38819
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:02:17 +0700

    Upload Repository/BearTypeRepository.cs

---
+++ Repository/BearTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Repository.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class BearTypeRepository : GenericRepository<Models.BearType>
+
+    {
+        public BearTypeRepository() { }
+        public BearTypeRepository(DBContext.FA25BearDBContext context) => _context = context;
+    }
+}
