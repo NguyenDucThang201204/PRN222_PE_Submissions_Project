9b5ec160ac4fdca2bb6782e832fed849465e2459
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:42:19 +0700

    Upload PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Service/IBearAccountService.cs

---
+++ PE_PRN232_FA25_NguyenHoangGiaThinh_BE/Service/IBearAccountService.cs
@@ -0,0 +1,14 @@
+﻿using BO.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IBearAccountService
+    {
+        LoginResponse? Login(LoginRequest request);
+    }
+}
