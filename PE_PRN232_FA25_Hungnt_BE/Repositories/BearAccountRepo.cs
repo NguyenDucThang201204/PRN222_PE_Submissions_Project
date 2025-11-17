3c12642ebe17e1577fdbf8162c0ddb41c3d78c96
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:15 +0700

    Upload PE_PRN232_FA25_Hungnt_BE/Repositories/BearAccountRepo.cs

---
+++ PE_PRN232_FA25_Hungnt_BE/Repositories/BearAccountRepo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Basic;
+using Repositories.DBContext;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class BearAccountRepo : GenericRepository<BearAccount>
+    {
+        public BearAccountRepo() { }
+        public BearAccountRepo(Fa25bearDbContext context) => _context = context;
+
+        public async Task<BearAccount> GetUserAccount(string email, string password)
+        {
+            return await _context.BearAccounts.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+
+        }
+    }
+}
