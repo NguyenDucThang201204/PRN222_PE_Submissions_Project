1624f8f16c2019a543f9619149d7b78df4a9aa9a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:02:35 +0700

    Upload PE_PRN232_FA25_NguyenTongThanhAn_BE/Configurations/ODataConfig.cs

---
+++ PE_PRN232_FA25_NguyenTongThanhAn_BE/Configurations/ODataConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+using Repository.Models;
+
+namespace PE_PRN232_FA25_NguyenTongThanhAn_BE.Configurations
+{
+    public static class ODataConfig
+    {
+        public static IEdmModel GetEDModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<BearProfile>("BearProfile");
+            builder.EntitySet<BearType>("BearType");
+             
+
+            return builder.GetEdmModel();
+        }
+    }
+}
