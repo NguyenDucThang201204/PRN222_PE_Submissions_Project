3cbcfef980bd7aed3cc7aa8164c4848d37d39fa8
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:02 +0700

    Upload Services/AccountService.cs

---
+++ Services/AccountService.cs
@@ -0,0 +1,55 @@
+﻿using Repositories.Dto;
+using Repositories.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class AccountService
+    {
+        private readonly AccountRepo _accountRepo;
+        private readonly TokenProvider _tokenProvider;
+
+        public AccountService(AccountRepo accountRepo, TokenProvider tokenProvider)
+        {
+            _accountRepo = accountRepo;
+            _tokenProvider = tokenProvider;
+        }
+
+        public async Task<LoginResponse> Authenticate(LoginRequest request)
+        {
+            var account = await _accountRepo.GetOne(request.userName, request.password);
+            if (account == null)
+            {
+                return null;
+            }
+
+            var roles = new List<string>();
+
+            if (account.RoleId != 0)
+            {
+                roles.Add(account.RoleId.ToString());
+            }
+
+
+            // Generate JWT token using the existing JWT service
+            var token = _tokenProvider.GenerateToken(
+                account.AccountId.ToString(),
+                account.Email ?? string.Empty,
+                roles
+            );
+
+            // Generate refresh token
+            var refreshToken = _tokenProvider.GenerateRefreshToken();
+
+            return new LoginResponse
+            {
+                Token = token,
+                Role = account.RoleId != 0 ? account.RoleId.ToString() : "0"
+            };
+        }
+    }
+}
