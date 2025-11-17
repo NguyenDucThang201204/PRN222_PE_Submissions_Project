fcc7cec552e9637842c4db30ec0ac676db50dd4b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:37:49 +0700

    Upload PE_PRN232_FA25_PhanXuanPhu_BE/PE_PRN232_FA25_PhanXuanPhu_API/Program.cs

---
+++ PE_PRN232_FA25_PhanXuanPhu_BE/PE_PRN232_FA25_PhanXuanPhu_API/Program.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using Microsoft.OpenApi.Models;
+using PE_PRN232_FA25_PhanXuanPhu_API.Models;
+using Repositories.Models;
+using Services;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
+    })
+    .AddOData(opt => opt
+        .Select()
+        .Filter()
+        .OrderBy()
+        .Count()
+        .Expand()
+        .SetMaxTop(100)
+        .AddRouteComponents("api", GetEdmModel()))
+
+    .ConfigureApiBehaviorOptions(options =>
+    {
+
+        options.InvalidModelStateResponseFactory = context =>
+        {
+
+            var firstError = context.ModelState.Values
+                                .SelectMany(v => v.Errors)
+                                .Select(e => e.ErrorMessage)
+                                .FirstOrDefault();
+
+
+            var errorResponse = new ErrorResponse("HB40001", firstError ?? "Missing/invalid input");
+            return new BadRequestObjectResult(errorResponse);
+        };
+    });
+
+
+builder.Services.AddScoped<BearProfileService>();
+
+
+builder.Services.AddScoped<BearAccountService>();
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+
+
+        options.Events = new JwtBearerEvents
+        {
+
+            OnChallenge = context =>
+            {
+                context.HandleResponse();
+                var result = ErrorHelper.Unauthorized() as ObjectResult;
+                context.Response.StatusCode = result.StatusCode.Value;
+                context.Response.ContentType = "application/json";
+                return context.Response.WriteAsync(JsonSerializer.Serialize(result.Value));
+            },
+
+            OnForbidden = context =>
+            {
+
+                var result = ErrorHelper.Forbidden() as ObjectResult;
+                context.Response.StatusCode = result.StatusCode.Value;
+                context.Response.ContentType = "application/json";
+                return context.Response.WriteAsync(JsonSerializer.Serialize(result.Value));
+            }
+        };
+    });
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.DescribeAllParametersInCamelCase();
+    option.ResolveConflictingActions(conf => conf.First());
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+IEdmModel GetEdmModel()
+{
+    var odataBuilder = new ODataConventionModelBuilder();
+    odataBuilder.EntitySet<BearProfile>("BearProfiles");
+    return odataBuilder.GetEdmModel();
+}
