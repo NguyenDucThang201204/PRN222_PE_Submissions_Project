c4004ad328395b1d6295a0d7bcb592024d88bf3a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:16:03 +0700

    Upload Presentation/Controllers/AuthController.cs

---
+++ Presentation/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service;
+using Service.Model;
+
+namespace Presentation.Controllers
+{
+    [Route("Accounts")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
+        {
+            var response = await _authService.Login(loginDTO);
+            if (response == null)
+            {
+                return Unauthorized(new { message = "Missing/invalid input" });
+            }
+            return Ok(response);
+        }
+    }
+}
