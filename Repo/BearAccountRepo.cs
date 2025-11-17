55ea5c74a2cf76d2512d8687534275f68c89bde4
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:12:29 +0700

    Upload Repo/BearAccountRepo.cs

---
+++ Repo/BearAccountRepo.cs
@@ -0,0 +1,18 @@
+﻿using BO;
+using DAL;
+
+namespace Repo
+{
+    public class BearAccountRepo
+    {
+        private readonly BearAccountDAO _dao;
+        public BearAccountRepo(BearAccountDAO dao)
+        {
+            _dao = dao;
+        }
+        public BearAccount Login(string username,string password)
+        {
+            return _dao.Login(username, password);
+        }
+    }
+}
