b146c54593c187d202fc0f5779c006d1df4dcb56
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:40 +0700

    Upload PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_BE/WeatherForecast.cs

---
+++ PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_BE/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace PE__PRN232_FA25_NamPK_BE
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
