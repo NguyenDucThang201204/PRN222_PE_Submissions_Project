82413228173cbd1ad2f5d8c26c153a3c0b37b8b1
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:23:47 +0700

    Upload PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_DAL/IGenericRepository.cs

---
+++ PE__PRN232_FA25_NamPK_BE/PE__PRN232_FA25_NamPK_DAL/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE__PRN232_FA25_NamPK_DAL
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includes);
+        public Task<T?> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        Task<int> SaveChangesAsync();
+    }
+
+}
