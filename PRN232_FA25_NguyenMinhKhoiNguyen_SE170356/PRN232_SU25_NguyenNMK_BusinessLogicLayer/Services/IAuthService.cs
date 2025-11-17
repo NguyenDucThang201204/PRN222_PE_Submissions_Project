75d248f4b54132d215fd1475fe63eaa1f6dcb0d6
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:24:46 +0700

    Upload PRN232_FA25_NguyenMinhKhoiNguyen_SE170356/PRN232_SU25_NguyenNMK_BusinessLogicLayer/Services/IAuthService.cs

---
+++ PRN232_FA25_NguyenMinhKhoiNguyen_SE170356/PRN232_SU25_NguyenNMK_BusinessLogicLayer/Services/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using PRN232_SU25_NguyenNMK_DataAccessLayer.NewFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_SU25_NguyenNMK_BusinessLogicLayer.Services
+{
+    public interface IAuthService
+    {
+        Task<LoginResponse> LoginAsync(LoginRequest request);
+    }
+}
