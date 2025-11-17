824aa6dde7e41e0f5cb3f4c841ceaa90003100ca
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:44:39 +0700

    Upload FA25Bear.DataAccess/Models/DTOs/LoginRequestDTO.cs

---
+++ FA25Bear.DataAccess/Models/DTOs/LoginRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FA25Bear.DataAccess.Models.DTOs
+{
+    public class LoginRequestDTO
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
