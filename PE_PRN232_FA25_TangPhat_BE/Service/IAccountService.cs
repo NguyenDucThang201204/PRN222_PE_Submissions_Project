7567b8e26a0332a447790c393b7b63a2b64fb11b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:25:38 +0700

    Upload PE_PRN232_FA25_TangPhat_BE/Service/IAccountService.cs

---
+++ PE_PRN232_FA25_TangPhat_BE/Service/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObjects.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IAccountService
+    {
+        Task<LoginResponse?> LoginAsync(LoginRequest request);
+    }
+}
