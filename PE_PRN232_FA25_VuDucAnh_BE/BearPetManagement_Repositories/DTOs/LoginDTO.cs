29e1d7b1dd8394d5332184b090f8ed2dd6ccc6c8
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:02:00 +0700

    Upload PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Repositories/DTOs/LoginDTO.cs

---
+++ PE_PRN232_FA25_VuDucAnh_BE/BearPetManagement_Repositories/DTOs/LoginDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearPetManagement_Repositories.DTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(255)]
+        public string userName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Password { get; set; }
+    }
+}
