7ecc7d9f10776aff23ecb4a32f2b3a19535b3ead
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:41:47 +0700

    Upload Repo/DTO/CreateRequest.cs

---
+++ Repo/DTO/CreateRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo.DTO
+{
+    public class CreateRequest
+    {
+        
+       
+        [Required]
+        public int? BearTypeId { get; set; }
+        [Required]
+        [RegularExpression(@"^([A-Z0-9][a-zA-Z0-9#]*\s)*([A-Z0-9][a-zA-Z0-9#]*)$")]
+        
+        public string BearName { get; set; }
+        [Required]
+        [Range(200.00001, double.MaxValue, ErrorMessage = "BearWeight must be greater than 200")]
+        public int BearWeight { get; set; }
+        [Required]
+        public string Characteristics { get; set; }
+        [Required]
+        public string CareNeeds { get; set; }
+        [Required]
+        public DateOnly? ModifiedDate { get; set; }
+      
+    }
+}
