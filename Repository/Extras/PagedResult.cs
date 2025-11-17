68950cc19f1185fcbf20e844104529e378671d19
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:40:17 +0700

    Upload Repository/Extras/PagedResult.cs

---
+++ Repository/Extras/PagedResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Extras;
+public class PagedResult<TEntity>
+{
+    public List<TEntity> Items { get; set; } = new();
+    public int TotalCount { get; set; }
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+}
