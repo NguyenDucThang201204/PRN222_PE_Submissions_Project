cf3fdc4dbfd8f5ff6b7114139b1255093213492d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:21:31 +0700

    Upload Service/BearProfilesService.cs

---
+++ Service/BearProfilesService.cs
@@ -0,0 +1,30 @@
+﻿using Repo;
+using Repo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IBearProfilesService
+    {
+        Task<List<BearProfile>> GetBearProfilesAsync();
+    }
+    public class BearProfilesService : IBearProfilesService
+    {
+        private readonly BearProfilesRepo _repo;
+
+        public BearProfilesService()
+        {
+            _repo = new BearProfilesRepo();
+        }
+
+        public async Task<List<BearProfile>> GetBearProfilesAsync()
+        {
+            return await _repo.GetBearProfilesAsync();
+        }
+
+    }
+}
