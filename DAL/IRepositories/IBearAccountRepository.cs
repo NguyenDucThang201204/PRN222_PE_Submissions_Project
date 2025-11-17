03f0f89bf10cc9ce251b2cc0c92798925a8da580
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:00:58 +0700

    Upload DAL/IRepositories/IBearAccountRepository.cs

---
+++ DAL/IRepositories/IBearAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.IRepositories
+{
+    public interface IBearAccountRepository
+    {
+        Task<BearAccount> LoginAsync(string email, string password);
+    }
+}
