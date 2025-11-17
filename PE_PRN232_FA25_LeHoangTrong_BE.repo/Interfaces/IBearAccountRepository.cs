2680a546d1d07d3c43b643a71bd1a73dd9848edc
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-17 13:47:54 +0700

    Upload PE_PRN232_FA25_LeHoangTrong_BE.repo/Interfaces/IBearAccountRepository.cs

---
+++ PE_PRN232_FA25_LeHoangTrong_BE.repo/Interfaces/IBearAccountRepository.cs
@@ -0,0 +1,8 @@
+using PE_PRN232_FA25_LeHoangTrong_BE.repo.Entities;
+
+namespace PE_PRN232_FA25_LeHoangTrong_BE_repo.Interfaces;
+
+public interface IBearAccountRepository : PE_PRN232_FA25_LeHoangTrong_BE_repo.IBaseRepository<BearAccount>
+{    Task<BearAccount?> GetByEmailAsync(string? email);
+
+}
